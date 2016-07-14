7f4e741fb8d17a06a9e08f03941cbdb83676e9f1
Nick Ponvert <nickponvert@gmail.com>
2016-07-14 11:44:07 -0700

    Bugfixes to two_axis_raster method of ephysinterface

--- nick/database/ephysinterface.py
+++ nick/database/ephysinterface.py
@@ -209,11 +209,10 @@ class EphysInterface(object):
 
         plt.show()
 
-    def plot_two_axis_sorted_raster(self, session, tetrode, behavSuffix, firstSortVar, secondSortVar, cluster = None, replace=0, timeRange = [-0.5, 1], ms = 1):
+    def plot_two_axis_sorted_raster(self, session, tetrode, firstSortArray, secondSortArray, cluster = None, replace=0, timeRange = [-0.5, 1], ms = 1, firstLabels=None, secondLabels=None):
         '''
-        Plot the tuning of a tetrode as a sorted tuning raster
+        Plot rasters sorted by 2 arrays
         '''
-        bdata = self.loader.get_session_behavior(behavSuffix)
         plotTitle = self.loader.get_session_filename(session)
         eventData = self.loader.get_session_events(session)
         spikeData = self.loader.get_session_spikes(session, tetrode)
@@ -221,23 +220,30 @@ class EphysInterface(object):
         eventOnsetTimes = self.loader.get_event_onset_times(eventData)
         spikeTimestamps=spikeData.timestamps
 
-        freqEachTrial = bdata[firstSortVar]
-        intensityEachTrial = bdata[secondSortVar]
+        if not firstLabels:
+            possibleFirst = np.unique(firstSortArray)
+            firstLabels = ['{}'.format(val) for val in possibleFirst]
+            # firstLabels = ['{0:.1f}'.format(val) for val in possibleFirst]
 
-        possibleFreq = np.unique(freqEachTrial)
-        possibleIntensity = np.unique(intensityEachTrial)
-        freqLabels = ['{0:.1f}'.format(freq/1000.0) for freq in possibleFreq]
-        intensityLabels = ['{:.0f} dB'.format(intensity) for intensity in possibleIntensity]
+        if not secondLabels:
+            possibleSecond = np.unique(secondSortArray)
+            secondLabels = ['{}'.format(val) for val in possibleSecond]
+            # secondLabels = ['{:.0f} dB'.format(val) for val in possibleSecond]
+
         xLabel="Time from sound onset (sec)"
 
-        plt.figure()
+        if replace:
+            fig = plt.gcf()
+            plt.clf()
+        else:
+            fig = plt.figure()
 
         dataplotter.two_axis_sorted_raster(spikeTimestamps,
                                            eventOnsetTimes,
-                                           freqEachTrial,
-                                           intensityEachTrial,
-                                           freqLabels,
-                                           intensityLabels,
+                                           firstSortArray,
+                                           secondSortArray,
+                                           firstLabels,
+                                           secondLabels,
                                            xLabel,
                                            plotTitle,
                                            flipFirstAxis=False,
