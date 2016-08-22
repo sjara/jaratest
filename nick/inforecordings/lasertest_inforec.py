3d75a1d2d1df00677be49bd78a40abf6202f26ee
Jara User <jarauser@jararig1>
2016-08-22 16:19:35 -0700

    Laser test inforecording'

---
+++ nick/inforecordings/lasertest_inforec.py
@@ -0,0 +1,43 @@
+laserCal = {
+    '0.5':1.5,
+    '1.0':1.3, #Mistake here??
+    '1.5':1.75,
+    '2.0':2.25,
+    '2.5':2.9,
+    '3.0':3.55
+}
+
+#Impedence between 1.5 and 2.5MOhm for all existing trodes, saved as csv
+
+#Laser setting (on the dial, have to re-calibrate because I didn't go high enough
+#Session
+
+#100msec laser pulses, 0.5+/-0sec isi
+
+#1.5 on dial - 1.6mW
+
+'2016-08-22_16-03-24'
+
+#2.5 on dial - 2.7mW
+'16-05-57'
+
+#3.5 on dial - 3.3mW
+'16-06-50'
+
+#4.5 - 4.1mW
+'16-07-42'
+
+#5.5 - 4.9mW
+'16-08-33'
+
+#6.5 - 5.9mW
+'16-09-24'
+
+#7.5 - 6.5mW
+'16-10-12' #72 trials
+
+#8.5 - 6.9mW
+'16-11-20'
+
+#9.5 - 7.2mW
+'16-12-06'
