45c042f746ad5a93b90abf97c690c62bca49f006
Nick Ponvert <nickponvert@gmail.com>
2016-08-26 12:12:37 -0700

    Fix error in nick's adap024 inforec

--- nick/inforecordings/adap024_inforec.py
+++ nick/inforecordings/adap024_inforec.py
@@ -8,7 +8,7 @@ experiments.append(exp0)
 
 exp0.add_site(0) #3.5 turns
 exp0.add_session('14-40-47', 'a', 'tuning', paradigm='tuning_curve')
-exp0.add_session('14-56-06', 'a', 'behav1', paradigm='2afc')
+exp0.add_session('14-56-09', 'a', 'behav1', paradigm='2afc')
 exp0.add_session('15-29-40', 'a', 'light', paradigm='lightDiscrim')
 exp0.add_session('15-46-41', 'b', 'behav2', paradigm='2afc')
 
