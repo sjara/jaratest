17faa36943fadf5622a70025e84774e7da7b828b
Santiago Jaramillo <sjara@uoregon.edu>
2016-08-28 14:52:54 -0700

    Fixed bugs. It now plots the correct psychometric.

--- santiago/test038_fit_psycurve.py
+++ santiago/test038_fit_psycurve.py
@@ -1,14 +1,17 @@
 '''
 Fitting psychometric curve.
+
+The current version plots the psychometric in log(Hz)
 '''
 
+import numpy as np
 from jaratoolbox import extrastats
 from jaratoolbox import extraplots
 from jaratoolbox import loadbehavior
 from jaratoolbox import behavioranalysis
 import matplotlib.pyplot as plt
 
-subject = 'adap034'
+subject = 'adap031'
 session = '20160824a'
 
 behavFile = loadbehavior.path_to_behavior_data(subject,'2afc',session)
@@ -19,30 +22,42 @@ choice=bdata['choice']
 valid=bdata['valid'] & (choice!=bdata.labels['choice']['none'])
 choiceRight = choice==bdata.labels['choice']['right']
 
-#possibleFreq = np.unique(targetFrequency)
-#nFreq = len(possibleFreq) 
-#trialsEachFreq = behavioranalysis.find_trials_each_type(targetFrequency,possibleFreq)
-
 # -- Calculate and plot psychometric points --
 (possibleValues,fractionHitsEachValue,ciHitsEachValue,nTrialsEachValue,nHitsEachValue)=\
     behavioranalysis.calculate_psychometric(choiceRight,targetFrequency,valid)
 
+logPossibleValues = np.log2(possibleValues)
+
 plt.clf()
-(pline, pcaps, pbars, pdots) = extraplots.plot_psychometric(possibleValues,fractionHitsEachValue,ciHitsEachValue)
-plt.setp(pdots,ms=6,mec='k',mew=2,mfc='k')
-plt.xlabel('Frequency (Hz)')
+#(pline, pcaps, pbars, pdots) = extraplots.plot_psychometric(possibleValues,fractionHitsEachValue,ciHitsEachValue)
+#plt.setp(pdots,ms=6,mec='k',mew=2,mfc='k')
+plt.plot(logPossibleValues,fractionHitsEachValue,'o')
+plt.xlabel('Frequency (log2(Hz))')
 plt.ylabel('Rightward choice (%)')
 
 plt.show()
 
+
 # -- Calculate and plot psychometric fit --
-constraints = None
-constraints = ['flat', 'Uniform(0,0.3)' ,'Uniform(0,0.2)', 'Uniform(0,0.2)', constraints]
-curveParams = extrastats.psychometric_fit(possibleValues,nTrialsEachValue, nHitsEachValue)
+#constraints = None
+constraints = ['Uniform(10,15)', 'Uniform(0,5)' ,'Uniform(0,1)', 'Uniform(0,1)']
+curveParams = extrastats.psychometric_fit(logPossibleValues, nTrialsEachValue,
+                                          nHitsEachValue, constraints)
+
+print 'Psychometric parameters (bias, slope, upper, lower):'
+print curveParams
 
 plt.hold(True)
-(hp,hfit) = extraplots.plot_psychometric_fit(possibleValues,nTrialsEachValue,
-                                           nHitsEachValue,curveParams)
+
+xValues = logPossibleValues
+xRange = xValues[-1]-xValues[1]
+fitxval = np.linspace(xValues[0]-0.1*xRange,xValues[-1]+0.1*xRange,40)
+fityval = extrastats.psychfun(fitxval,*curveParams)
+hfit = plt.plot(fitxval,fityval,'-',linewidth=2,color='k')
+
+#(hp,hfit) = extraplots.plot_psychometric_fit(possibleValues,nTrialsEachValue,
+#                                           nHitsEachValue,curveParams)
+
 plt.hold(False)
 
 plt.draw()
