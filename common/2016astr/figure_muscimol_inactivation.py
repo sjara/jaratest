b3a9fa1ee95b4d59b6fffdfd943462cc8cffba3d
Nick Ponvert <nickponvert@gmail.com>
2017-03-03 17:09:16 -0800

    Put the muscimol stats in the figure

--- common/2016astr/figure_muscimol_inactivation.py
+++ common/2016astr/figure_muscimol_inactivation.py
@@ -204,3 +204,24 @@ plt.show()
 
 if SAVE_FIGURE:
     extraplots.save_figure(figFilename, figFormat, figSize, outputDir)
+
+import numpy as np
+from scipy import stats
+
+##SATS
+dataFn = '/home/nick/data/jarahubdata/figuresdata/2016astr/muscimol_inactivation/muscimol_frac_correct_summary.npz'
+
+dataObj = np.load(dataFn)
+
+data = dataObj['data']
+subjects = dataObj['subjects']
+conditions = dataObj['conditions']
+
+#data(subject, session, condition)
+
+for indSubject in range(5):
+    subDataSal = data[indSubject, :, 0]
+    subDataMus = data[indSubject, :, 1]
+
+    print indSubject
+    print stats.ranksums(subDataSal, subDataMus)
