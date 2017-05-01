d7c6ff141a53e938901adff6f3f67d6f13e6155a
Santiago Jaramillo <sjara@uoregon.edu>
2017-05-01 09:50:11 -0700

    Deleted old file and added clf() to fig.

--- common/2017acnih/camkii_inactivation_behaviour_figure.py
+++ common/2017acnih/camkii_inactivation_behaviour_figure.py
@@ -18,6 +18,8 @@ animal = 'band011'
 sessions = ['20170314a','20170315a','20170324a']
 validPerSNR, rightPerSNR, possibleSNRs, laserTrialTypes = bt.band_SNR_laser_psychometric(animal, sessions)
 colours = ['k','m']
+
+plt.clf()
 for las in range(len(validPerSNR)):
     bt.plot_psychometric(validPerSNR[las,:], rightPerSNR[las,:], possibleSNRs, colour = colours[las])
 plt.ylabel('% rightward choice', fontsize=fontSizeLabels)
