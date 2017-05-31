65fb7e1ed168c804c961805a6732ed2795df2143
Jara User <jarauser@jararig1>
2017-05-31 14:58:25 -0700

    updated adap046 inforec

--- common/inforecordings/adap046_inforec.py
+++ common/inforecordings/adap046_inforec.py
@@ -45,3 +45,27 @@ exp0.add_session('16-04-26', 'g', 'tc', 'am_tuning_curve')
 exp0.add_site(2960, tetrodes=[1,2,3,4,5,6,7,8])
 exp0.add_session('16-23-41', None, 'noiseburst', 'am_tuning_curve') #ref to chan14 in TT5, TT1,3,4,6,8 response 
 exp0.add_session('16-26-17', 'h', 'tc', 'am_tuning_curve') 
+
+exp1 = celldatabase.Experiment(subject,
+                               '2017-05-31',
+                               brainarea='leftAStr',
+                               info=['medialDiI', 'facingPosterior'])
+experiments.append(exp1)
+
+#1800, 2000, 2100, 2200, 2300, 2400, 2500, 2600um not sound responsive
+exp1.add_site(2660, tetrodes=[1,2,4,5,6,7,8])
+exp1.add_session('13-22-11', None, 'noiseburst', 'am_tuning_curve') #ref to chan25 in TT3, TT5-8 offset response 
+exp1.add_session('13-25-04', 'a', 'tc', 'am_tuning_curve') 
+
+exp1.add_site(2780, tetrodes=[1,2,4,5,6,7,8])
+exp1.add_session('13-45-12', None, 'noiseburst', 'am_tuning_curve') #ref to chan32 in TT3, TT1,5-8 offset response 
+exp1.add_session('13-48-23', 'b', 'tc', 'am_tuning_curve') 
+
+#pretty quiet at 2850, 2900um
+exp1.add_site(2950, tetrodes=[1,2,4,5,6,7,8])
+exp1.add_session('14-12-19', None, 'noiseburst', 'am_tuning_curve') #ref to chan32 in TT3, TT5-8 same offset response 
+exp1.add_session('14-13-57', 'c', 'tc', 'am_tuning_curve') 
+
+#pretty quiet at and pass 3000um except the same very late offset response
+#exp1.add_site(3070, tetrodes=[1,2,3,4,5,6,7,8])
+#exp1.add_session('14-40-11', None, 'noiseburst', 'am_tuning_curve') #ref to chan32 in TT3, TT5-8 offset response 
