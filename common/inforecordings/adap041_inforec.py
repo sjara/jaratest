c9d40d62581c8358553e89432d30b7947826de31
Jara User <jarauser@jararig1>
2017-07-09 00:44:31 -0700

    Adap041 inforec

--- common/inforecordings/adap041_inforec.py
+++ common/inforecordings/adap041_inforec.py
@@ -47,3 +47,50 @@ exp0.add_session('17-41-57', 'h', 'tc', 'am_tuning_curve')
 exp0.add_site(2824, tetrodes=range(1, 9))
 exp0.add_session('18-04-36', None, 'noiseburst', 'am_tuning_curve')
 exp0.add_session('18-07-13', 'i', 'tc', 'am_tuning_curve')
+
+exp1 = celldatabase.Experiment(subject,
+                               '2017-07-08',
+                               brainarea='leftAstr',
+                               info=['anteriorDiI', 'facingPosterior'])
+experiments.append(exp1)
+
+#Mouse on the rig at 2012hrs, waiting 10 mins for brain to settle
+#Tetrodes at 1969um - had to remove some membrane from brain surface
+
+exp1.add_site(2055, tetrodes=range(1, 9))
+exp1.add_session('20-33-05', None, 'noiseburst', 'am_tuning_curve')
+exp1.add_session('20-37-11', 'a', 'tc', 'am_tuning_curve')
+
+exp1.add_site(2153, tetrodes=range(1, 9))
+exp1.add_session('21-00-12', None, 'noiseburst', 'am_tuning_curve')
+exp1.add_session('21-02-33', 'b', 'tc', 'am_tuning_curve')
+
+exp1.add_site(2253, tetrodes=range(1, 9))
+exp1.add_session('21-36-58', None, 'noiseburst', 'am_tuning_curve')
+exp1.add_session('21-40-02', 'c', 'tc', 'am_tuning_curve')
+
+exp1.add_site(2403, tetrodes=range(1, 9))
+exp1.add_session('22-14-22', None, 'noiseburst', 'am_tuning_curve')
+exp1.add_session('22-18-02', 'd', 'tc', 'am_tuning_curve')
+
+exp1.add_site(2550, tetrodes=range(1, 9))
+exp1.add_session('22-41-03', None, 'noiseburst', 'am_tuning_curve')
+exp1.add_session('22-43-15', 'e', 'tc', 'am_tuning_curve')
+
+# exp1.add_site(2656, tetrodes=range(1, 9))
+# exp1.add_session('23-08-07', None, 'noiseburst', 'am_tuning_curve')
+#No sound response here
+
+exp1.add_site(2703, tetrodes=range(1, 9))
+exp1.add_session('23-18-45', None, 'noiseburst', 'am_tuning_curve')
+exp1.add_session('23-21-00', 'f', 'tc', 'am_tuning_curve')
+
+site = exp1.add_site(2858, tetrodes=range(1, 9))
+exp1.add_session('23-59-01', None, 'noiseburst', 'am_tuning_curve')
+site.date = '2017-07-09'
+exp1.add_session('00-01-41', 'a', 'tc', 'am_tuning_curve')
+
+# site = exp1.add_site(2979, tetrodes=range(1, 9))
+# site.date = '2017-07-09'
+# exp1.add_session('00-32-27', None, 'noiseburst', 'am_tuning_curve')
+#Nothing here, removing for the day
