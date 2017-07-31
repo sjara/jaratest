7856b538752f558bc2ba03fb3f0f4bb290bf4bc5
LanGuo <lan.guo14@gmail.com>
2017-07-31 16:23:15 -0700

    updated script to plot left vs right shank freq representation

--- lan/analysis_headfixed_astr/plot_responsive_freqs_diff_shanks.py
+++ lan/analysis_headfixed_astr/plot_responsive_freqs_diff_shanks.py
@@ -5,9 +5,9 @@ from jaratoolbox import settings
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import pdb
+import itertools
 
-
-animalLists = ['adap041','adap042', 'adap043','adap044', 'adap046','adap047']
+animalLists = ['adap042', 'adap043','adap044', 'adap046','adap047'] #adap041 did not have enough data
 
 #plotAll = True
 #normalized = False
@@ -68,12 +68,24 @@ def selectFreqs(row):
     print freqs
     return freqs
 
-responsiveFreqsThisAreaThisIntensity = pd.Series(dtype=object)
-responsiveFreqsThisAreaThisIntensity = tuningFreqsThisIntensity.apply(selectFreqs, axis=1) #This is a pd series where each cell is a tuple containing the responsive freqs (if none then it's an empty tuple)
-if logscaled:
-    responsiveFreqsThisAreaThisIntensity = responsiveFreqsThisAreaThisIntensity.apply(lambda x: np.log2(x))
-cellsToPlot['responsiveFreqs'] = responsiveFreqsThisAreaThisIntensity
+def selectZscores(row):
+    zScores = tuple(np.array(row.tuningZscoreEachIntEachFreq)[responseEachFreqThisIntensity.loc[row.name].values])
+    print zScores
+    return zScores
 
+responsiveFreqsThisIntensity = pd.Series(dtype=object)
+responsiveFreqsThisIntensity = tuningFreqsThisIntensity.apply(selectFreqs, axis=1) #This is a pd series where each cell is a tuple containing the responsive freqs (if none then it's an empty tuple)
+zScoresThisIntensity = zScoresThisIntensity.to_frame()
+zScoresEachRespFreq = zScoresThisIntensity.apply(selectZscores, axis=1)
+
+if logscaled:
+    responsiveFreqsThisIntensity = responsiveFreqsThisIntensity.apply(lambda x: np.log2(x))
+cellsToPlot['responsiveFreqs60dB'] = responsiveFreqsThisIntensity
+cellsToPlot['zScores60dBEachRespFreq'] = zScoresEachRespFreq
+cellsToPlot['zThreshold'] = maxZThreshold
+
+outputDf = pd.DataFrame(columns=['animal','brainArea','expInd','shank1_freq','shank2_freq','shank3_freq','shank4_freq','shank1_zScore','shank2_zScore','shank3_zScore','shank4_zScore','shank1_weightedFreq','shank2_weightedFreq','shank3_weightedFreq','shank4_weightedFreq'])
+#outputDf.astype(dtype={})
 for inda, brainArea in enumerate(np.unique(cellsToPlot.brainarea)): 
     thisArea = cellsToPlot.loc[cellsToPlot.brainarea==brainArea]
     for animal in np.unique(thisArea.animalName):
@@ -83,11 +95,15 @@ for inda, brainArea in enumerate(np.unique(cellsToPlot.brainarea)):
         fig, axes = plt.subplots(nrows=1, ncols=numExps, sharex=True, sharey='row')
         for inde,exp in enumerate(experiments):
             thisExp = thisAnimal.loc[thisAnimal.indExperiment == exp]
+            dictThisExp = {'animal':animal, 'brainArea':brainArea, 'expInd':inde}
             for shank in ttsToShank.keys():
                 ttsThisShank = ttsToShank[shank]
                 cellsThisShank = thisExp.loc[thisExp.tetrode.isin(ttsThisShank)]
                 if len(cellsThisShank):
-                    freqsThisShank = np.concatenate(cellsThisShank.responsiveFreqs.values)
+                    freqsThisShank = np.concatenate(cellsThisShank.responsiveFreqs60dB.values)
+                    zScoresThisShank = np.concatenate(cellsThisShank.zScores60dBEachRespFreq.values)
+                    dictThisExp['shank{}_freq'.format(shank)] = freqsThisShank
+                    dictThisExp['shank{}_zScore'.format(shank)] = zScoresThisShank
                     print 'shank {} has these freqs:'.format(shank),len(freqsThisShank)
                     ax = axes[inde]
                     randOffset = 0.2*(np.random.rand(len(freqsThisShank))-0.5)
@@ -97,6 +113,10 @@ for inda, brainArea in enumerate(np.unique(cellsToPlot.brainarea)):
                     ax.set_xticklabels(['shank1', 'shank2', 'shank3', 'shank4'])
                     ax.set_title('Exp {}'.format(inde))
                     ax.set_xlim([0,5])
+                else:
+                    dictThisExp['shank{}_freq'.format(shank)] = np.array([])
+                    dictThisExp['shank{}_zScore'.format(shank)] = np.array([])
+            outputDf = outputDf.append(dictThisExp, ignore_index=True)
             fig.set_tight_layout({'rect': [0.02, 0.02, 1, 0.95], 'w_pad': 0.06, 'h_pad': 0.05})  
             fig.text(0.5, 0.02, 'Experiments', ha='center', va='center')
             fig.text(0.02, 0.5, 'Preferred frequency (logscaled)', ha='center', va='center', rotation='vertical')
@@ -106,3 +126,23 @@ for inda, brainArea in enumerate(np.unique(cellsToPlot.brainarea)):
             figname = 'freqs_by_shanks'+animal+'_'+brainArea
             figFullPath = os.path.join(outputDir, figname)
             plt.savefig(figFullPath, format='png')
+
+
+shanks = ['shank1','shank2','shank3','shank4']
+for indr, row in outputDf.iterrows():
+    for shank in shanks:
+        if np.any(row[shank+'_freq']):
+            row[shank+'_weightedFreq'] = sum(row[shank+'_freq'].values * row[shank+'_zScore'].values)/sum(row[shank+'_zScore'].values)
+        else:
+            row[shank+'_weightedFreq'] = np.NaN
+
+plt.clf()
+for indr, row in outputDf.iterrows():
+    for pairOfShanks in [list(x) for x in itertools.combinations(['shank1_weightedFreq','shank2_weightedFreq','shank3_weightedFreq','shank4_weightedFreq'],2)]:
+        pairOfFreqs = row[pairOfShanks].values.astype(float)
+        if not np.isnan(pairOfFreqs).any():
+            line = plt.plot([1,2],pairOfFreqs, 'o-', mfc='k', mec='none')
+            plt.setp(line, color='k', linewidth=1.5)
+plt.xticks([1,2], ['left shank', 'right shank'])       
+plt.xlim([0.5,2.5]) 
+plt.show()
