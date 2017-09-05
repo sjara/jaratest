b7b7313c83db354a1b3d07c424769676d6449bf4
Jara User <jarauser@jararig1>
2017-09-05 16:59:11 -0700

    pinp025 inforec

--- common/inforecordings/pinp025_inforec.py
+++ common/inforecordings/pinp025_inforec.py
@@ -68,3 +68,37 @@ exp1.add_site(2598, tetrodes=range(1, 9)).remove_tetrodes([1, 2])
 exp1.add_session('16-31-14', None, 'noiseburst', 'am_tuning_curve')
 # exp1.add_session('15-29-10', 'i', 'tc', 'am_tuning_curve')
 # exp1.add_session('16-02-16', 'j', 'am', 'am_tuning_curve')
+
+exp2 = celldatabase.Experiment(subject,
+                               '2017-09-05',
+                               brainarea='rightAstr',
+                               info=['posteriorDiI', 'facingPosterior'])
+experiments.append(exp2)
+
+#Probe in at 10:53am
+#Probe at 2000, waiting 10 mins
+
+exp2.add_site(2000, tetrodes=range(1, 9)).remove_tetrodes([1, 2, 3])
+exp2.add_session('11-21-50', None, 'noiseburst', 'am_tuning_curve')
+exp2.add_session('11-24-39', 'a', 'tc', 'am_tuning_curve')
+exp2.add_session('11-58-01', 'b', 'am', 'am_tuning_curve')
+
+exp2.add_site(2112, tetrodes=range(1, 9)).remove_tetrodes([1, 2, 3])
+exp2.add_session('12-23-22', None, 'noiseburst', 'am_tuning_curve')
+exp2.add_session('12-26-50', 'c', 'tc', 'am_tuning_curve')
+exp2.add_session('13-00-29', 'd', 'am', 'am_tuning_curve')
+
+exp2.add_site(2226, tetrodes=range(1, 9)).remove_tetrodes([1, 2, 3])
+exp2.add_session('13-28-06', None, 'noiseburst', 'am_tuning_curve')
+exp2.add_session('13-33-23', 'e', 'tc', 'am_tuning_curve')
+exp2.add_session('14-06-41', 'f', 'am', 'am_tuning_curve')
+
+exp2.add_site(2352, tetrodes=range(1, 9)).remove_tetrodes([1, 2, 3])
+exp2.add_session('14-34-04', None, 'noiseburst', 'am_tuning_curve')
+exp2.add_session('14-37-34', 'g', 'tc', 'am_tuning_curve')
+exp2.add_session('15-14-10', 'h', 'am', 'am_tuning_curve')
+
+exp2.add_site(2464, tetrodes=range(1, 9)).remove_tetrodes([1, 2, 3])
+exp2.add_session('15-56-18', None, 'noiseburst', 'am_tuning_curve')
+exp2.add_session('16-01-11', 'i', 'tc', 'am_tuning_curve')
+exp2.add_session('16-35-04', 'j', 'am', 'am_tuning_curve')
