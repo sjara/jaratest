93a4b00b623ea3cc1fd99a5d3548cc0afd4f8789
LanGuo <lan.guo14@gmail.com>
2018-01-11 15:00:16 -0800

    finished module for checking duplicates in new celldb

--- lan/analysis_reward_change/new_good_cell_duplication_check_celldb.py
+++ lan/analysis_reward_change/new_good_cell_duplication_check_celldb.py
@@ -16,6 +16,16 @@ import pdb
 wavesize = 160
 
 def get_all_same_session_cells(celldb):
+    '''
+    Find the cells recorded in the same session on the same tetrode.
+    Args: 
+        celldb: a pandas dataframe containing only good quality cells.
+    Returns:
+        sameSessionCellsDict: a dictionary where each key indicates a 
+            unique date/tetrode from the celldb where more than one cell exist; 
+            each value is the rows in celldb that contain cells recorded on the 
+            date and tetrode indicated by the key.
+    '''
     sameSessionCellsDict = {}
     for date in np.unique(celldb.date):
         cellsThisSession = celldb.loc[celldb.date==date]
@@ -29,13 +39,28 @@ def get_all_same_session_cells(celldb):
 
 
 def get_all_consecutive_session_cells(celldb):
+    '''
+    Find the cells recorded in two 'consecutive' session on the same tetrode.
+    Args: 
+        celldb: a pandas dataframe containing only good quality cells.
+    Returns:
+        consecutiveSessCellsDict: a dictionary where each key indicates a 
+            unique pair of dates/tetrode from the celldb where more than one cell exist; 
+            each value is the rows in celldb that contain cells recorded on those 
+            dates and tetrode indicated by the key.
+    NOTE: this function defines 'consecutive' dates as two neighbouring sessions in 
+    the celldb, their actual recording date may or may not be 1 day different from each other.
+    '''
     dateStrs = np.unique(celldb.date)
-    dates = [datetime.datetime.strptime(str, "%Y-%m-%d") for str in dateStrs]
-    possibleConsecutiveDates = []
-    for ind in range(len(dates)-1):
-        possibleConsecutiveDates.append([dates[ind], dates[ind+1]]) 
-    step = datetime.timedelta(days=1)
-    consecutiveDates = [pair for pair in possibleConsecutiveDates if (pair[0]+step==pair[1])]
+    #dates = [datetime.datetime.strptime(str, "%Y-%m-%d") for str in dateStrs]
+    #possibleConsecutiveDates = []
+    #for ind in range(len(dates)-1):
+        #possibleConsecutiveDates.append([dates[ind], dates[ind+1]]) 
+    #step = datetime.timedelta(days=1)
+    #consecutiveDates = [pair for pair in possibleConsecutiveDates if (pair[0]+step==pair[1])]
+    consecutiveDates = []
+    for ind in range(len(dateStrs) -1):
+        consecutiveDates.append([dateStrs[ind], dateStrs[ind+1]]) 
     consecutiveSessCellsDict = {}
     for pairOfDates in consecutiveDates:
         cellsDate1 = celldb.loc[celldb.date==pairOfDates[0]]
@@ -50,7 +75,16 @@ def get_all_consecutive_session_cells(celldb):
     return consecutiveSessCellsDict
         
 
-def get_all_waveforms_behav_session(celldb, wavesize, sessionToUse='behavior'):
+def get_all_waveforms_one_session(celldb, wavesize=160, sessionToUse='behavior'):
+    '''
+    Load waveforms for each row of a celldb for one type of session.
+    Args:
+        celldb: a pandas dataframe containing only good quality cells (containing n cells).
+        wavesize: number of samples in a waveform, usually 160 for all 4 channels of a tetrode.
+        sessionToUse: which type of session we want to load the waveform from.
+    Returns:
+        allWaveforms: a nd-array in the shape of (nCells, mSamples).
+    '''
     numCells = len(celldb)
     allWaveforms = np.zeros((numCells, wavesize))
     for indc, (ind,cell) in enumerate(celldb.iterrows()):
@@ -63,6 +97,7 @@ def get_all_waveforms_behav_session(celldb, wavesize, sessionToUse='behavior'):
             meanWaveforms = np.mean(alignedWaveforms,axis=0)
             allWaveforms[indc,:] = meanWaveforms.flatten()
         except ValueError:
+            print 'Cell {} did not have any spikes in the {} session'.format(ind, sessionToUse)
             continue 
 
     return allWaveforms
@@ -90,16 +125,24 @@ def row_corrcoeff(A,B):
 
 def find_within_session_duplicates(celldb, threshold):
     '''
-    
+    Find within session duplicates and mark them, determine which ones to discard based on sound responsiveness.
+    Args:
+        celldb: a pandas dataframe containing only good quality cells (containing n cells).
+        threshold: the correlation value above which two cells were qualified as 'duplicates'.
+    Returns:
+        excludeDf: a pandas dataframe with same number of rows as celldb,
+        containing a column about whether a cell is a within-session 
+        duplicate ('duplicate_self'), and a column about wheterh a cell
+        is discarded ('duplicate_self_discard') based on sound responsiveness.
     '''
     excludeDf = celldb[['date', 'tetrode','cluster']]
     excludeDf['duplicate_self'] = 0
-    excludeDf['duplicate_self_keep'] = 0
+    excludeDf['duplicate_self_discard'] = 0
     excludeDf['maxSoundRes'] = celldb.behavZscore.apply(lambda x: np.max(np.abs(x))).values
     
     sameSessionCellsDict = get_all_same_session_cells(celldb)
     for (date,cellsThisSess) in sameSessionCellsDict.items():
-        waveformsThisSess = get_all_waveforms_behav_session(cellsThisSess, wavesize=160)
+        waveformsThisSess = get_all_waveforms_one_session(cellsThisSess, wavesize=160)
         corrMat = row_corrcoeff(waveformsThisSess, waveformsThisSess)
         corrMatFiltered = np.tril(corrMat, k=-1) > threshold
         dupInds = np.unique(np.concatenate(np.nonzero(corrMatFiltered)))
@@ -107,22 +150,36 @@ def find_within_session_duplicates(celldb, threshold):
             dupCellsInds = cellsThisSess.index[dupInds]
             dupCells = excludeDf.loc[dupCellsInds] 
             excludeDf.ix[dupCellsInds, 'duplicate_self'] = 1
-            cellToKeepIndex = np.argmax(dupCells.maxSoundRes, axis=1) 
-            excludeDf.ix[cellToKeepIndex, 'duplicate_self_keep'] = 1
+            cellToKeepIndex = np.argmax(dupCells.maxSoundRes, axis=1)
+            cellToDiscardIndex = dupCellsInds[dupCellsInds!= cellToKeepInd]
+            excludeDf.ix[cellToDiscardIndex, 'duplicate_self_discard'] = 1
     return excludeDf
+
 
 def find_cross_session_duplicates(celldb, threshold):
     '''
+    Find cross session duplicates and mark them, determine which ones to discard based on sound responsiveness.
+    Args:
+        celldb: a pandas dataframe containing only good quality cells (containing n cells).
+        threshold: the correlation value above which two cells were qualified as 'duplicates'.
+    Returns:
+        excludeDf: a pandas dataframe with same number of rows as celldb,
+        containing a column about whether a cell is duplicated in two consecutive sessions
+        ('duplicate_cross'), and a column about wheterh a cell is discarded 
+        ('duplicate_cross_discard') based on sound responsiveness.
+    NOTE: this function assumes two cells will likely not be duplicates of one another in
+    two sessions that are not 'consecutive' and do not compare or discard cells that 
+    are duplicated in more than two consecutive sessions.
     '''
     excludeDf = cellDB[['date', 'tetrode','cluster']]
     excludeDf['duplicate_cross'] = 0
-    excludeDf['duplicate_cross_keep'] = 0
+    excludeDf['duplicate_cross_discard'] = 0
     excludeDf['maxSoundRes'] = celldb.behavZscore.apply(lambda x: np.max(np.abs(x))).values
     
     consecutiveSessCellsDict = get_all_consecutive_session_cells(celldb)
     for (pairOfDates,[cellsSess1,cellsSess2]) in consecutiveSessCellsDict.items():
-        waveformsSess1 = get_all_waveforms_behav_session(cellsSess1, wavesize=160)
-        waveformsSess2 = get_all_waveforms_behav_session(cellsSess2, wavesize=160)
+        waveformsSess1 = get_all_waveforms_one_session(cellsSess1, wavesize=160)
+        waveformsSess2 = get_all_waveforms_one_session(cellsSess2, wavesize=160)
         corrMat = row_corrcoeff(waveformsSess1, waveformsSess2)
         corrMatFiltered = corrMat > threshold
         dupIndsSess1 = np.nonzero(corrMatFiltered)[0]
@@ -135,11 +192,7 @@ def find_cross_session_duplicates(celldb, threshold):
             dupCells = excludeDf.loc[dupCellsInds]
             cellToKeepIndex = np.argmax(dupCells.maxSoundRes, axis=1) 
             cellToDiscardInd = dupCellsInds[dupCellsInds!= cellToKeepInd]
-            if excludeDf.ix[cellToKeepIndex, 'duplicate_cross_keep'] == 0:  #cell with biggest sound response has not been compared before
-                excludeDf.ix[cellToKeepInd, 'duplicate_cross_keep'] = 1
-                excludeDf.ix[cellToDiscardInd, 'duplicate_cross_keep'] = -1
-            elif excludeDf.ix[cellToKeepInd, 'duplicate_cross_keep'] in [1,-1]: #cell with bigger sound response previously determined to be kept or to be discarded, do not change its result
-                excludeDf.ix[cellToDiscardInd, 'duplicate_cross_keep'] = -1
+            excludeDf.ix[cellToDiscardInd, 'duplicate_cross_discard'] = 1
     return excludeDf
 
 
