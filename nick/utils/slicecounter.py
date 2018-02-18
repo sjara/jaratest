bf6d6a670d57b3140a0721172ea4441b96ce95ff
Nick Ponvert <nickponvert@gmail.com>
2018-02-17 18:40:23 -0800

    Added util files for nick

---
+++ nick/utils/slicecounter.py
@@ -0,0 +1,15 @@
+rows = ['a', 'b', 'c', 'd']
+cols = ['1', '2', '3', '4', '5', '6']
+chans = ['tl', 'r', 'fr']
+hemis = ['right', 'left']
+
+
+start = [1, 'd', '4']
+
+p1d4tl_left
+p1d4r_left
+p1d4fr_left
+
+p1d4tl_right
+p1d4tl_right
+p1d4tl_right
