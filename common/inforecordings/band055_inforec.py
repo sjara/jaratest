eb1a147ac112666aa5a09af16f97e4cc810449a6
Jara <jarauser@jararig2>
2018-03-19 09:28:57 -0700

    updated band inforec

--- common/inforecordings/band055_inforec.py
+++ common/inforecordings/band055_inforec.py
@@ -159,3 +159,78 @@ exp1.add_session('18-34-54', 'n', 'tuningCurve', 'am_tuning_curve')
 exp1.add_session('18-40-40', 'o', 'AM', 'am_tuning_curve')
 exp1.add_session('18-48-05', 'p', 'laserBandwidth', 'bandwidth_am')
 exp1.add_session('19-13-41', 'q', 'noiseAmps', 'am_tuning_curve')
+
+
+exp2 = celldatabase.Experiment(subject, '2018-03-16', 'right_AC', info=['lateralDiI','TT1ant','soundleft'])
+experiments.append(exp2)
+
+exp2.laserCalibration = {
+    '1.0':3.3,
+    '2.0':3.8,
+    '3.0':4.3,
+    '4.0':4.85,
+    '5.0':5.4,
+    '7.5':6.7,
+    '10.0':8.05
+}
+
+exp2.add_site(1000, tetrodes=[1,2,4,6,8])
+exp2.add_session('10-19-15', None, 'noisebursts', 'am_tuning_curve')
+exp2.add_session('10-20-40', None, 'lasernoisebursts', 'bandwidth_am')
+exp2.add_session('10-22-52', 'a', 'tuningCurve', 'am_tuning_curve')
+exp2.add_session('10-27-52', 'b', 'AM', 'am_tuning_curve')
+exp2.add_session('10-34-14', 'c', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('10-59-25', 'd', 'noiseAmps', 'am_tuning_curve')
+
+exp2.add_site(1100, tetrodes=[2,4,6,8])
+exp2.add_session('11-23-17', None, 'noisebursts', 'am_tuning_curve')
+exp2.add_session('11-24-29', None, 'lasernoisebursts', 'bandwidth_am')
+exp2.add_session('11-26-03', 'e', 'tuningCurve', 'am_tuning_curve')
+exp2.add_session('11-31-25', 'f', 'AM', 'am_tuning_curve')
+exp2.add_session('11-38-54', 'g', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('12-03-01', 'h', 'noiseAmps', 'am_tuning_curve')
+
+exp2.add_site(1200, tetrodes=[1,2,4,6,8])
+exp2.add_session('12-31-55', None, 'noisebursts', 'am_tuning_curve')
+exp2.add_session('12-33-40', None, 'lasernoisebursts', 'bandwidth_am')
+exp2.add_session('12-35-51', 'i', 'tuningCurve', 'am_tuning_curve')
+exp2.add_session('12-40-33', 'j', 'AM', 'am_tuning_curve')
+exp2.add_session('12-48-42', 'k', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('13-13-51', 'l', 'noiseAmps', 'am_tuning_curve')
+
+exp2.add_site(1300, tetrodes=[1,2,3,4,6,8])
+exp2.add_session('13-34-01', None, 'noisebursts', 'am_tuning_curve')
+exp2.add_session('13-35-20', None, 'lasernoisebursts', 'bandwidth_am')
+exp2.add_session('13-36-41', 'm', 'tuningCurve', 'am_tuning_curve')
+exp2.add_session('13-41-33', 'n', 'AM', 'am_tuning_curve')
+exp2.add_session('13-50-26', 'o', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('14-14-51', 'p', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('14-41-25', 'q', 'noiseAmps', 'am_tuning_curve')
+
+exp2.add_site(1400, tetrodes=[1,2,3,4,6,8])
+exp2.add_session('14-56-45', None, 'noisebursts', 'am_tuning_curve')
+exp2.add_session('14-58-03', None, 'lasernoisebursts', 'bandwidth_am')
+exp2.add_session('14-59-20', 'r', 'tuningCurve', 'am_tuning_curve')
+exp2.add_session('15-06-24', 's', 'tuningCurve', 'am_tuning_curve') #accidentally started moving probe cause I forgot what I was doing, moved back and rerecording
+exp2.add_session('15-10-53', 't', 'AM', 'am_tuning_curve')
+exp2.add_session('15-19-16', 'u', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('15-43-51', 'v', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('16-08-04', 'w', 'noiseAmps','am_tuning_curve')
+
+exp2.add_site(1500, tetrodes=[1,2,3,4,5,6,8])
+exp2.add_session('16-25-53', None, 'noisebursts', 'am_tuning_curve')
+exp2.add_session('16-27-21', None, 'lasernoisebursts', 'bandwidth_am')
+exp2.add_session('16-29-15', 'x', 'tuningCurve', 'am_tuning_curve')
+exp2.add_session('16-33-52', 'y', 'AM', 'am_tuning_curve')
+exp2.add_session('16-41-51', 'z', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('17-05-51', 'aa', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('17-30-53', 'ab', 'noiseAmps', 'am_tuning_curve')
+
+exp2.add_site(1600, tetrodes=[1,2,3,4,5,6,8])
+exp2.add_session('17-47-15', None, 'noisebursts', 'am_tuning_curve')
+exp2.add_session('17-48-37', None, 'lasernoisebursts', 'bandwidth_am')
+exp2.add_session('17-50-00', 'ac', 'tuningCurve', 'am_tuning_curve')
+exp2.add_session('17-55-53', 'ad', 'AM', 'am_tuning_curve')
+exp2.add_session('18-03-58', 'ae', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('18-28-24', 'af', 'laserBandwidth', 'bandwidth_am')
+exp2.add_session('18-52-21', 'ag', 'noiseAmps', 'am_tuning_curve')
