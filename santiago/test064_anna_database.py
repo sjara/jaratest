f7468730ec1a943da2a9fac57edc8d5a98c63109
Santiago Jaramillo <sjara@uoregon.edu>
2018-05-17 17:31:26 -0700

    Test save_hdf and load_hdf.

--- santiago/test064_anna_database.py
+++ santiago/test064_anna_database.py
@@ -13,10 +13,12 @@ import pandas as pd
 import numpy as np
 
 from jaratoolbox import celldatabase
+reload(celldatabase)
 from jaratoolbox import ephyscore
 from jaratoolbox import spikesorting
 from jaratoolbox import spikesanalysis
 from jaratoolbox import settings
+reload(settings)
 
 from scipy import stats
 
@@ -25,7 +27,7 @@ from scipy import stats
 
 if len(sys.argv)==2:
     subject = sys.argv[1]
-else:    
+else:
     raise ValueError('Please specify what subject to process (as an argument to this script).')
 
 #inforec = '/home/jarauser/src/jaratest/common/inforecordings/{0}_inforec.py'.format(subject)
@@ -35,9 +37,9 @@ db = celldatabase.generate_cell_database(inforec)
 
 """
 db = db.reindex(columns=np.concatenate((db.columns.values,['gaussFreqFit','bestFreqTuningWindow'])))
-db['gaussFreqFit'] = db['gaussFreqFit'].astype(object) 
+db['gaussFreqFit'] = db['gaussFreqFit'].astype(object)
 db['bestFreqTuningWindow'] = db['bestFreqTuningWindow'].astype(object)
-        
+
 db = db.drop(db[(db['isiViolations'] > 0.05) | (db['spikeShapeQuality'] < 2)].index).reset_index(drop=True)
 
 # --- determine laser responsiveness of each cell (using laser pulse) ---
@@ -46,9 +48,9 @@ laserTestStatistic = np.empty(len(db))
 laserPVal = np.empty(len(db))
 
 for indRow, dbRow in db.iterrows():
-    
+
     cellObj = ephyscore.Cell(dbRow)
-    
+
     try:
         laserEphysData, noBehav = cellObj.load('laserPulse')
     except IndexError:
@@ -62,15 +64,15 @@ for indRow, dbRow in db.iterrows():
         binTime = baseRange[1]-baseRange[0]         # Time-bin size
         responseRange = [0, 0+binTime]
         fullTimeRange = [baseRange[0], responseRange[1]]
-        spikeTimesFromEventOnset, trialIndexForEachSpike, indexLimitsEachTrial = spikesanalysis.eventlocked_spiketimes(laserSpikeTimestamps, 
-                                                                                                                   laserEventOnsetTimes, 
+        spikeTimesFromEventOnset, trialIndexForEachSpike, indexLimitsEachTrial = spikesanalysis.eventlocked_spiketimes(laserSpikeTimestamps,
+                                                                                                                   laserEventOnsetTimes,
                                                                                                                    fullTimeRange)
         baseSpikeCountMat = spikesanalysis.spiketimes_to_spikecounts(spikeTimesFromEventOnset, indexLimitsEachTrial, baseRange)
         laserSpikeCountMat = spikesanalysis.spiketimes_to_spikecounts(spikeTimesFromEventOnset, indexLimitsEachTrial, responseRange)
         [testStatistic, pVal] = stats.ranksums(laserSpikeCountMat, baseSpikeCountMat)
     laserTestStatistic[indRow] = testStatistic
     laserPVal[indRow] = pVal
-    
+
 db['laserPVal'] = laserPVal
 db['laserUStat'] = laserTestStatistic
 
@@ -81,9 +83,9 @@ laserTrainTestStatistic = np.empty(len(db))
 laserTrainPVal = np.empty(len(db))
 
 for indRow, dbRow in db.iterrows():
-    
+
     cellObj = ephyscore.Cell(dbRow)
-    
+
     try:
         laserTrainEphysData, noBehav = cellObj.load('laserTrain')
     except IndexError:
@@ -98,24 +100,53 @@ for indRow, dbRow in db.iterrows():
         binTime = baseRange[1]-baseRange[0]         # Time-bin size
         responseRange = [0, 0+binTime]
         fullTimeRange = [baseRange[0], responseRange[1]]
-        spikeTimesFromEventOnset, trialIndexForEachSpike, indexLimitsEachTrial = spikesanalysis.eventlocked_spiketimes(laserTrainSpikeTimestamps, 
-                                                                                                                   laserTrainEventOnsetTimes, 
+        spikeTimesFromEventOnset, trialIndexForEachSpike, indexLimitsEachTrial = spikesanalysis.eventlocked_spiketimes(laserTrainSpikeTimestamps,
+                                                                                                                   laserTrainEventOnsetTimes,
                                                                                                                    fullTimeRange)
         baseSpikeCountMat = spikesanalysis.spiketimes_to_spikecounts(spikeTimesFromEventOnset, indexLimitsEachTrial, baseRange)
         laserSpikeCountMat = spikesanalysis.spiketimes_to_spikecounts(spikeTimesFromEventOnset, indexLimitsEachTrial, responseRange)
         [testStatistic, pVal] = stats.ranksums(laserSpikeCountMat, baseSpikeCountMat)
     laserTrainTestStatistic[indRow] = testStatistic
     laserTrainPVal[indRow] = pVal
-    
+
 db['laserTrainPVal'] = laserTrainPVal
 db['laserTrainUStat'] = laserTrainTestStatistic
 """
 # --- determine sound frequency tuning (gaussian fit for high amplitude trials) ---
 
 # save db as h5 and csv
+
+'''
 dbFilename = os.path.join(settings.DATABASE_PATH,'{0}_test.h5'.format(subject))
 db.to_hdf(dbFilename, 'database',mode='w')
-#db.to_csv(dbFilename+'.csv')
 print('Saved database to: {}'.format(dbFilename))
+
+db.to_csv(dbFilename+'.csv')
+print('Saved database to: {}'.format(dbFilename+'.csv'))
+'''
+
+dbFilenameNew = os.path.join(settings.DATABASE_PATH,'{0}_new.h5'.format(subject))
+celldatabase.save_hdf(db, dbFilenameNew)
+print('Saved database to: {}'.format(dbFilenameNew))
+
+# -- To load the HDF5 --
+# df = celldatabase.load_hdf('/tmp/band045_new.h5')
+
+
+#db.to_csv(dbFilename+'.csv')
 #db.to_hdf('/home/jarauser/data/database/{0}_test.h5'.format(subject), 'database')
-#db.to_csv('/home/jarauser/data/database/{0}_test.csv'.format(subject))        
+#db.to_csv('/home/jarauser/data/database/{0}_test.csv'.format(subject))
+
+'''
+### TEST CODE ###
+
+for col in db.columns: print('{} \t {}'.format(col,type(db[col][0])));
+
+for col in db.columns: print('{} \t {}'.format(col, db[col][0]) );
+
+dd = celldatabase.load_hdf('/tmp/band045_new.h5')
+df=pd.DataFrame({'tetrode':dd['tetrode'], 'depth':dd['depth']})
+df=pd.DataFrame({'tetrode':dd['tetrode'], 'date':dd['date']})
+df=pd.DataFrame({'tetrode':dd['tetrode'], 'spikeShape':dd['spikeShape']}) # FAILS
+df=pd.DataFrame({'tetrode':dd['tetrode'], 'ephysTime':dd['ephysTime']})
+'''
