39a90bdb71c87d19fa7bf42d4f0ca453427bf14d
LanGuo <lan.guo14@gmail.com>
2018-07-03 15:09:15 -0700

    updating analysis scripts

--- lan/test_argparse.py
+++ lan/test_argparse.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--CASE', type=str, required=True, help="CASE can be 'calculate' or 'merge'")
-parser.add_argument('-MICE', '--MOUSENAMELIST', type=str, nargs='*', help="Enter mouse names separated by space")
+parser.add_argument('-MICE', '--MOUSENAMELIST', type=str, required=True, help="Enter mouse names separated by space")
 args = parser.parse_args()
 CASE = args.CASE
 mouseNameList = args.MOUSENAMELIST
