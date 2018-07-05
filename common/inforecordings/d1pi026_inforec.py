9acd5d039bdf1b0839a593f1a80392db6571cf7a
JaraLab User <jarauser@jararigtest>
2018-07-05 11:39:22 -0700

    d1pi026 inforec

--- common/inforecordings/d1pi026_inforec.py
+++ common/inforecordings/d1pi026_inforec.py
@@ -6,25 +6,24 @@ experiments=[]
 
 exp0 = celldatabase.Experiment(subject, '2018-07-03', 'right_AudStr', info=['FacingPosterior', 'Anterior'])
 experiments.append(exp0)
-#Used left speaker; 2.5 mW for laser; probe DAF8
-#
-# exp0.laserCalibration = {
-#     '0.5':1.45,
-#     '1.0':2.75,
-#     '1.5':4.4,
-#     '2.0':6.1,
-#     '2.5':7.5,
-#     '3.0':10.05,
-#     #'3.5':3.0,
-#     #'4.0':3.6
-# }
+#Used left speaker; 2.5 mW for laser; probe DAF8; Rig 1
 
-#Tetrode 4 has reference; threshold set to 55mV
+exp0.laserCalibration = {
+    '0.51':1.50,
+    '1.0':2.70,
+    '1.5':4.2,
+    '1.99':5.98,
+    '2.5':7.28,
+    '3.01':9.28,
+}
+
+#Tetrode 5 has reference; threshold set to 55mV
 exp0.add_site(2073, tetrodes=[3,4,6,7,8])
 exp0.add_session('12-10-39', None, 'noisebursts', 'am_tuning_curve')
 
 exp0.add_site(2180, tetrodes=[3,8])
 exp0.add_session('12-19-13', None, 'noisebursts', 'am_tuning_curve')
+#Threshold was set to 44mV
 
 exp0.add_site(2502, tetrodes=[3,8])
 exp0.add_session('12-19-13', None, 'noisebursts', 'am_tuning_curve')
