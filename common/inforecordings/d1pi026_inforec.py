778727a8d42bcd9d8016678f972b3dd34195ce69
JaraLab User <jarauser@jararigtest>
2018-07-30 12:27:08 -0700

    Conflicting commit of some unsaved d1pi026 changes

--- common/inforecordings/d1pi026_inforec.py
+++ common/inforecordings/d1pi026_inforec.py
@@ -18,15 +18,15 @@ exp0.laserCalibration = {
 }
 
 #Tetrode 5 has reference; threshold set to 55mV
-exp0.add_site(2073, tetrodes=[3,4,6,7,8])
-exp0.add_session('12-10-39', None, 'noiseburst', 'am_tuning_curve')
+# exp0.add_site(2073, tetrodes=[3,4,6,7,8])
+# exp0.add_session('12-10-39', None, 'noiseburst', 'am_tuning_curve')
 
-exp0.add_site(2180, tetrodes=[3,8])
-exp0.add_session('12-19-13', None, 'noiseburst', 'am_tuning_curve')
+# exp0.add_site(2180, tetrodes=[3,8])
+# exp0.add_session('12-19-13', None, 'noiseburst', 'am_tuning_curve')
 #Threshold was set to 44mV
 
-exp0.add_site(2502, tetrodes=[3,8])
-exp0.add_session('12-19-13', None, 'noiseburst', 'am_tuning_curve')
+# exp0.add_site(2502, tetrodes=[3,8])
+# exp0.add_session('12-19-13', None, 'noiseburst', 'am_tuning_curve')
 
 exp0.maxDepth = 2502
 
@@ -57,8 +57,8 @@ exp1.add_session('13-37-48', None, 'noiseburst', 'am_tuning_curve')
 exp1.add_session('13-39-29', None, 'lasertrain', 'am_tuning_curve')
 exp1.add_session('13-46-07', None, 'laserpulse', 'am_tuning_curve')
 
-exp1.add_site(3000, tetrodes=[1,2,3,4,6,8]) #While lowering, the noise completely faded out. Cause unknown
-exp1.add_session('13-56-13', None, 'noiseburst', 'am_tuning_curve')
+# exp1.add_site(3000, tetrodes=[1,2,3,4,6,8]) #While lowering, the noise completely faded out. Cause unknown
+# exp1.add_session('13-56-13', None, 'noiseburst', 'am_tuning_curve')
 #No response in ephys whatsoever. Experiment was ended for the day as the cause was unknown.Raising the probe caused no response to return either
 
 exp1.maxDepth = 3000
@@ -98,63 +98,64 @@ exp3.laserCalibration = {
     '4.0':6.9
 }
 
-#When the probewas first inserted and then removed, some tissue dame out with it and stuck. This was washed away y sooking in saline and removng with the needle, but could jave an affect on the impedance
+#When the probe was first inserted and then removed, some tissue came out with it and stuck. This was washed away y sooking in saline and removng with the needle, but could jave an affect on the impedance
 #Tetrode 5 has reference; threshold set to 55mV
 exp3.add_site(2900, tetrodes=[1,2,3,4,6,7,8])
 exp3.add_session('13-53-22', None, 'noiseburst', 'am_tuning_curve')
 exp3.add_session('13-58-35', None, 'laserPulse', 'am_tuning_curve')# initial burst on raster, but no second line so likey not caused by photoelectric effect
 exp3.add_session('14-10-46', None, 'laserTrain', 'am_tuning_curve')#two possible light sensitive neurons, both on tetrode 3
 
-exp3.add_site(2950, tetrodes=[1,2,3,4,6,7,8])
-exp3.add_session('14-25-33', None, 'noiseburst', 'am_tuning_curve')
-#In an effort to find a sound responsie cell, I'm moving on without testing light sensitivity
+# exp3.add_site(2950, tetrodes=[1,2,3,4,6,7,8])
+# exp3.add_session('14-25-33', None, 'noiseburst', 'am_tuning_curve')
+#In an effort to find a sound responsive cell, I'm moving on without testing light sensitivity
 
-exp3.add_site(3000, tetrodes=[1,2,3,4,6,7,8])
-exp3.add_session('14-30-57', None, 'noiseburst', 'am_tuning_curve')
-exp3.add_session('14-34-26', None, 'tc', 'am_tuning_curve')
+# exp3.add_site(3000, tetrodes=[1,2,3,4,6,7,8])
+# exp3.add_session('14-30-57', None, 'noiseburst', 'am_tuning_curve')
+# exp3.add_session('14-34-26', None, 'tc', 'am_tuning_curve')
 
 exp3.add_site(3050, tetrodes=[1,2,3,4,6,7,8])
 exp3.add_session('14-30-57', None, 'noiseburst', 'am_tuning_curve')
 exp3.add_session('14-43-53', None, 'laserPulse', 'am_tuning_curve')
 
-exp3.add_site(3100, tetrodes=[1,2,3,4,6,7,8])
-exp3.add_session('14-50-09', None, 'noiseburst', 'am_tuning_curve')
-exp3.add_session('14-53-27', None, 'am', 'am_tuning_curve')
+# exp3.add_site(3100, tetrodes=[1,2,3,4,6,7,8])
+# exp3.add_session('14-50-09', None, 'noiseburst', 'am_tuning_curve')
+# exp3.add_session('14-53-27', None, 'am', 'am_tuning_curve')
 
-exp3.add_site(3150, tetrodes=[1,2,3,4,6,7,8])
+exp3.add_site(3150, tetrodes=[1,2,3,4,8])
 exp3.add_session('15-01-30', None, 'noiseburst', 'am_tuning_curve')
 exp3.add_session('15-08-36', None, 'noiseburst', 'am_tuning_curve')#6 now has reference Had spike on reference, so it's beng retested
 
 #7 has the reference
-exp3.add_site(3200, tetrodes=[1,2,3,4,5,8])
-exp3.add_session('15-15-22', None, 'noiseburst', 'am_tuning_curve')
-exp3.add_session('15-19-13', None, 'am', 'am_tuning_curve')
+# exp3.add_site(3200, tetrodes=[1,2,3,4,5,8])
+# exp3.add_session('15-15-22', None, 'noiseburst', 'am_tuning_curve')
+# exp3.add_session('15-19-13', None, 'am', 'am_tuning_curve')
 
 exp3.add_site(3250, tetrodes=[1,2,3,4,5,8])
 exp3.add_session('15-38-05', None, 'noiseburst', 'am_tuning_curve')
 
 #5 has the reference
-exp3.add_site(3300, tetrodes=[1,2,3,7,8])
-exp3.add_session('15-45-06', None, 'noiseburst', 'am_tuning_curve')
-exp3.add_session('15-47-28', None, 'tc', 'am_tuning_curve')
-exp3.add_session('15-47-28', None, 'am', 'am_tuning_curve')
+# exp3.add_site(3300, tetrodes=[1,2,3,7,8])
+# exp3.add_session('15-45-06', None, 'noiseburst', 'am_tuning_curve')
+# exp3.add_session('15-47-28', None, 'tc', 'am_tuning_curve')
+# exp3.add_session('15-47-28', None, 'am', 'am_tuning_curve')
 
 exp3.maxDepth = 3300
 
-exp4 = celldatabase.Experiment(subject, '2018-07-12', 'right_AudStr', info=['FacingPosterior', 'PosteriorMid'])
-experiments.append(exp4)
+# exp4 = celldatabase.Experiment(subject, '2018-07-12', 'right_AudStr', info=['FacingPosterior', 'PosteriorMid'])
+# experiments.append(exp4)
+#
+# #Used left speaker; 2.5 mW for laser; probe DAF9; Rig 1
+#
+# exp4.laserCalibration = {
+#     '0.5':1.0,
+#     '1.0':1.6,
+#     '1.5':2.3,
+#     '2.0':3.0,
+#     '2.5':3.8,
+#     '3.0':4.8,
+#     '3.5':5.85,
+#     '4.0':6.65
+# }
 
-#Used left speaker; 2.5 mW for laser; probe DAF9; Rig 1
-
-exp4.laserCalibration = {
-    '0.5':1.0,
-    '1.0':1.6,
-    '1.5':2.3,
-    '2.0':3.0,
-    '2.5':3.8,
-    '3.0':4.8,
-    '3.5':5.85,
-    '4.0':6.65
-}
-
-#Aborted due to hemorrhaging after cleaning the craniotomy. Gel foam was needed to reduce the bleeding and was left under the silguard
+#exp4.maxDepth = 0
+#Aborted due to hemorrhaging after cleaning the craniotomy. Gel-foam was needed to reduce the bleeding and was left under the silguard
