c0d867f3ad331724b1d169bf8324d911d7eb065a
Anna Lakunina <alakunina@gmail.com>
2019-03-12 15:47:14 -0700

    BRAND NEW HOT PLACE TO PUT OUR HISTOLOGY INFORMATIONgit add infohistology/git add infohistology/git add infohistology/git add infohistology/git add infohistology/git add infohistology/git add infohistology/

---
+++ common/infohistology/band055_tracks.py
@@ -0,0 +1,72 @@
+'''
+TODO: 
+- add reference to type of recording device used? e.g. tetrode vs silicon probe, probe geometry, etc.
+- 'shank' is very specific to silicon probes, replace with more generic info key?
+'''
+
+subject = 'band055'
+
+tracks = [
+    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1-D6-03',
+     'recordingTrack':'middleDiD', 'shank':1, 'atlasZ':196},
+          
+    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1-D3-03',
+     'recordingTrack':'middleDiD', 'shank':2, 'atlasZ':190},
+          
+    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1-C6-03',
+     'recordingTrack':'middleDiD', 'shank':3, 'atlasZ':182}#,
+          
+#     {'subject':subject, 'brainArea':'LeftAC',
+#      'recordingTrack':'p1-C2-03_middleDiD_shank4', 'atlasZ':176},
+#           
+#     {'subject':subject, 'brainArea':'LeftAC',
+#      'recordingTrack':'p1-C6-03_medialDiO_shank3', 'atlasZ':182},
+#           
+#     {'subject':subject, 'brainArea':'LeftAC',
+#      'recordingTrack':'p1-D2-03_medialDiO_shank2', 'atlasZ':188},
+#           
+#     {'subject':subject, 'brainArea':'LeftAC',
+#      'recordingTrack':'p1-D5-03_medialDiO_shank1', 'atlasZ':194},
+# 
+#     {'subject':subject, 'brainArea':'LeftAC',
+#      'recordingTrack':'p1-D6-03_lateralDiO_shank1', 'atlasZ':196},
+# 
+#     {'subject':subject, 'brainArea':'LeftAC',
+#      'recordingTrack':'p1-D3-03_lateralDiO_shank2', 'atlasZ':190},
+# 
+#     {'subject':subject, 'brainArea':'LeftAC',
+#      'recordingTrack':'p1-C6-03_lateralDiO_shank3', 'atlasZ':182},    
+# 
+#     {'subject':subject, 'brainArea':'LeftAC',
+#      'recordingTrack':'p1-C2-03_lateralDiO_shank4', 'atlasZ':176},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p2-A4-02_lateralDiI_shank1', 'atlasZ':207},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p1-D4-02_lateralDiI_shank3', 'atlasZ':191},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p1-D2-02_lateralDiI_shank4', 'atlasZ':188},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p2-A6-02_medialDiI_shank1', 'atlasZ':210},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p2-A4-02_medialDiI_shank2', 'atlasZ':207},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p1-D6-02_medialDiI_shank4', 'atlasZ':197},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p2-A4-02_middleDiD_shank1', 'atlasZ':207},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p2-A2-02_middleDiD_shank2', 'atlasZ':203},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p1-D6-02_middleDiD_shank3', 'atlasZ':197},
+# 
+#     {'subject':subject, 'brainArea':'RightAC',
+#      'recordingTrack':'p1-D5-02_middleDiD_shank4', 'atlasZ':193}, 
+]
