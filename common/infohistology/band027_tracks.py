f333e4e1e86b066bc902d623666fc8dfa2dbf59f
JaraLab User <jarauser@jararigtest>
2019-04-04 13:47:28 -0700

    band027infohistology

--- common/infohistology/band027_tracks.py
+++ common/infohistology/band027_tracks.py
@@ -7,20 +7,36 @@ TODO:
 subject = 'band015'
 
 tracks = [
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p2-A2-02',
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p2-A2-02',
      'recordingTrack':'posteriormedialDiD', 'shank':4, 'atlasZ':214},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p2-B1-02',
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p2-B1-02',
      'recordingTrack':'medialDiI', 'shank':4, 'atlasZ':228},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p2-B2-02',
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p2-B2-02',
      'recordingTrack':'middleDiD', 'shank':4, 'atlasZ':231},
 
-    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-B2-02',
+    {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p2-B2-02',
      'recordingTrack': 'posteriormedialDiD', 'shank': 2, 'atlasZ': 231},
 
-    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-B4-02',
+    {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p2-B4-02',
      'recordingTrack': 'medialDiI', 'shank': 3, 'atlasZ': 234},
 
-    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-B6-02',
+    {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p2-B6-02',
      'recordingTrack': 'middleDiD', 'shank': 3, 'atlasZ': 238},
+
+     {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p2-B6-02',
+      'recordingTrack': 'posteriormedialDiD', 'shank': 1, 'atlasZ': 238},
+
+     {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p2-C3-02',
+      'recordingTrack': 'medialDiI', 'shank': 2, 'atlasZ': 240},
+
+     {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p2-C3-02',
+      'recordingTrack': 'middleDiD', 'shank': 2, 'atlasZ': 240},
+
+     {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p2-C5-02',
+      'recordingTrack': 'middleDiD', 'shank': 1, 'atlasZ': 244},
+
+     {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p2-C5-02',
+      'recordingTrack': 'medialDiI', 'shank': 1, 'atlasZ': 246},
+]
