fd6d3e696912a817281bc71ceac6f92431bf89c0
JaraLab User <jarauser@jararigtest>
2019-04-05 13:40:42 -0700

    Band 29, 31, 32, and 33s infohistology files

---
+++ common/infohistology/band029_tracks.py
@@ -0,0 +1,42 @@
+'''
+TODO:
+- add reference to type of recording device used? e.g. tetrode vs silicon probe, probe geometry, etc.
+- 'shank' is very specific to silicon probes, replace with more generic info key?
+'''
+
+subject = 'band029'
+
+tracks = [
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-A3-02',
+     'recordingTrack':'medialDiD', 'shank':4, 'atlasZ':197},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-A4-02',
+     'recordingTrack':'medialDiD', 'shank':2, 'atlasZ':199},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-A6-02',
+     'recordingTrack':'lateralDiI', 'shank':4, 'atlasZ':203},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-A6-02',
+     'recordingTrack':'medialDiD', 'shank':1, 'atlasZ':203},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-B2-02',
+     'recordingTrack':'middleDiD', 'shank':4, 'atlasZ':206},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-B3-02',
+     'recordingTrack':'lateralDiI', 'shank':3, 'atlasZ':206},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-B6-02',
+     'recordingTrack':'lateralDiI', 'shank':2, 'atlasZ':209},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-B6-02',
+     'recordingTrack':'middleDiD', 'shank':3, 'atlasZ':209},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-C3-02',
+     'recordingTrack':'lateralDiI', 'shank':1, 'atlasZ':218},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-C3-02',
+     'recordingTrack':'middleDiD', 'shank':2, 'atlasZ':218},
+
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-C6-02',
+     'recordingTrack':'middleDiD', 'shank':1, 'atlasZ':225},
+]
