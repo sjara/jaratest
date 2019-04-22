01bf6b51b8cf17681d4e0c0046262cd95aa5d765
JaraLab User <jarauser@jararigtest>
2019-04-22 12:40:57 -0700

    Updated band059 tracks file

--- common/infohistology/band059_tracks.py
+++ common/infohistology/band059_tracks.py
@@ -7,18 +7,18 @@ TODO:
 subject = 'band059'
 
 tracks = [
-    {'subject':subject, 'brainArea':'RightAC', 'histImage':'p1-D4-02',
+    {'subject':subject, 'brainArea':'RightAC', 'histImage':'P1-D4-02',
      'recordingTrack':'lateralDiD', 'shank':4, 'atlasZ':206},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1-D5-03',
+    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'P1-D5-03',
      'recordingTrack':'lateralDiD', 'shank':4, 'atlasZ':196},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p2-A3-03',
+    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'P2-A3-03',
      'recordingTrack':'lateralDiD', 'shank':3, 'atlasZ':208},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p2-A6-03',
+    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'P2-A6-03',
      'recordingTrack':'lateralDiD', 'shank':2, 'atlasZ':212},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p2-A4-03',
+    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'P2-A4-03',
      'recordingTrack':'lateralDiD', 'shank':1, 'atlasZ':218},
 ]
