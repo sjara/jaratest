17e3d1df27e1a0d5b0836b4c8cf7834f2dd1040b
Anna Lakunina <alakunina@gmail.com>
2019-04-23 14:44:48 -0700

    left side tracks for band004

--- common/infohistology/band004_tracks.py
+++ common/infohistology/band004_tracks.py
@@ -30,4 +30,28 @@ tracks = [
 
     {'subject': subject, 'brainArea': 'RightAC', 'histImage': 'p1-B6-02',
      'recordingTrack': 'lateralDiI', 'shank': 4, 'atlasZ': 187},
+          
+    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-A3-03',
+     'recordingTrack': 'middleDiI', 'shank': 4, 'atlasZ': 188},
+          
+    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-A5-03',
+     'recordingTrack': 'medialDiO', 'shank': 4, 'atlasZ': 195},
+          
+    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-A5-03',
+     'recordingTrack': 'middleDiI', 'shank': 3, 'atlasZ': 195},
+          
+    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-B2-03',
+     'recordingTrack': 'medialDiO', 'shank': 3, 'atlasZ': 206},
+          
+    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-B2-03',
+     'recordingTrack': 'middleDiI', 'shank': 2, 'atlasZ': 206},
+          
+    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-B4-03',
+     'recordingTrack': 'medialDiO', 'shank': 2, 'atlasZ': 212},
+          
+    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-B5-03',
+     'recordingTrack': 'medialDiO', 'shank': 1, 'atlasZ': 216},
+          
+    {'subject': subject, 'brainArea': 'LeftAC', 'histImage': 'p2-B5-03',
+     'recordingTrack': 'middleDiI', 'shank': 1, 'atlasZ': 216},
 ]
