81ecbcef6640be822744c7777334743568094302
Beth <bmccarry@uoregon.edu>
2019-08-09 12:43:39 -0700

    Added study name for 3_tone sequence

--- beth/ssa/studyparams.py
+++ beth/ssa/studyparams.py
@@ -3,6 +3,7 @@ This file contains the default names and queries used in experiment and analysis
 '''
 
 STUDY_NAME = 'ssa'
+#STUDY_NAME = '3_tone'
 
 # List of animals used in this study
-MICE_LIST = ['chad013']
+MICE_LIST = ['chad013','chad015']
