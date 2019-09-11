e16e52b3f977eb2fdb1011e8fe554f861366bd69
Jara User <jarauser@jararig1>
2019-09-10 17:17:04 -0700

    Updated inforec

--- common/inforecordings/chad018_inforec.py
+++ common/inforecordings/chad018_inforec.py
@@ -3,7 +3,7 @@ from jaratoolbox import celldatabase
 subject = 'chad018'
 experiments=[]
 
-exp0 = celldatabase.Experiment(subject, '2019-09-09', 'left AC', info=['midLateral' 'facingLeft' 'soundRight'])
+exp0 = celldatabase.Experiment(subject, '2019-09-09', 'left AC', info=['Lateral' 'facingLeft' 'soundRight'])
 experiments.append(exp0)
 # Using probe C39C
 
@@ -25,3 +25,53 @@ exp0.add_session('17-10-30', 'd', 'descending', 'threetones_sequence')
 # Frequencies chosen based on tetrode 2.
 
 exp0.maxDepth = 1150
+
+
+exp1 = celldatabase.Experiment(subject, '2019-09-10', 'left AC', info=['Lateral' 'facingLeft' 'soundRight'])
+experiments.append(exp1)
+# Using probe C39C
+
+exp1.add_site(1050, tetrodes=[1,2,3,4,5,6,7,8])
+
+exp1.add_session('15-40-52', None, 'noiseburst', 'am_tuning_curve')
+# Behavior suffix 'a'
+# Reference set to 18 (Tetrode 1, channel 1)
+
+
+exp1.add_site(1127, tetrodes=[1,2,3,4,5,6,7,8])
+
+exp1.add_session('16-00-24', None, 'noiseburst', 'am_tuning_curve')
+# Behavior suffix 'b'
+# Reference set to 18 (Tetrode 1, channel 1)
+
+exp1.add_site(1274, tetrodes=[1,2,3,4,5,6,7,8])
+
+exp1.add_session('16-24-00', None, 'noiseburst', 'am_tuning_curve')
+# Behavior suffix 'c'
+# Reference set to 18 (Tetrode 1, channel 1)
+
+
+exp1 = celldatabase.Experiment(subject, '2019-09-10', 'left AC', info=['Medial' 'facingLeft' 'soundRight'])
+experiments.append(exp1)
+# Using probe C39C
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
