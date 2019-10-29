4ede505c7133b314ecaa42aa3c38e33bec135f11
Matt Nardoci <mattnardoci@gmail.com>
2019-10-29 15:30:31 -0700

    d1pi040 tracks file

---
+++ common/infohistology/d1pi040_tracks.py
@@ -0,0 +1,55 @@
+'''
+TODO:
+- add reference to type of recording device used? e.g. tetrode vs silicon probe, probe geometry, etc.
+- 'shank' is very specific to silicon probes, replace with more generic info key?
+'''
+
+subject = 'd1pi040'
+
+tracks = [
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'P1-B4-03',
+     'recordingTrack': 'middleDiD', 'shank': 1, 'atlasZ': 258},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'P1-B4-03',
+     'recordingTrack': 'middleDiD', 'shank': 2, 'atlasZ': 258},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'P1-B4-03',
+     'recordingTrack': 'middleDiD', 'shank': 3, 'atlasZ': 258},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'P1-B4-03',
+     'recordingTrack': 'middleDiD', 'shank': 4, 'atlasZ': 258},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'P1-C1-03',
+     'recordingTrack': 'anteriourDiI', 'shank': 1, 'atlasZ': 268},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'P1-C1-03',
+     'recordingTrack': 'anteriourDiI', 'shank': 2, 'atlasZ': 268},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'P1-C1-03',
+     'recordingTrack': 'anteriourDiI', 'shank': 3, 'atlasZ': 268},
+
+    {'subject': subject, 'brainArea': 'RightAstr', 'histImage': 'P1-C2-02',
+     'recordingTrack': 'middleDiD', 'shank': 1, 'atlasZ': 260},
+
+    {'subject': subject, 'brainArea': 'RightAstr', 'histImage': 'P1-C2-02',
+     'recordingTrack': 'middleDiD', 'shank': 2, 'atlasZ': 260},
+
+    {'subject': subject, 'brainArea': 'RightAstr', 'histImage': 'P1-C2-02',
+     'recordingTrack': 'middleDiD', 'shank': 3, 'atlasZ': 260},
+
+    {'subject': subject, 'brainArea': 'RightAstr', 'histImage': 'P1-C2-02',
+     'recordingTrack': 'middleDiD', 'shank': 4, 'atlasZ': 260},
+
+    {'subject': subject, 'brainArea': 'RightAstr', 'histImage': 'P1-C5-02',
+     'recordingTrack': 'anteriourDiI', 'shank': 1, 'atlasZ': 275},
+
+    {'subject': subject, 'brainArea': 'RightAstr', 'histImage': 'P1-C5-02',
+     'recordingTrack': 'anteriourDiI', 'shank': 2, 'atlasZ': 275},
+
+    {'subject': subject, 'brainArea': 'RightAstr', 'histImage': 'P1-C5-02',
+     'recordingTrack': 'anteriourDiI', 'shank': 3, 'atlasZ': 275},
+
+    {'subject': subject, 'brainArea': 'RightAstr', 'histImage': 'P1-C5-02',
+     'recordingTrack': 'anteriourDiI', 'shank': 4, 'atlasZ': 275},
+
+]
