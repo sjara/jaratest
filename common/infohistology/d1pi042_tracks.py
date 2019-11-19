521a27981d9e46300caea1c99e5002a137142fcb
Matt Nardoci <mattnardoci@gmail.com>
2019-11-19 10:43:51 -0800

    d1pi042 infohistology

---
+++ common/infohistology/d1pi042_tracks.py
@@ -0,0 +1,33 @@
+'''
+TODO:
+- add reference to type of recording device used? e.g. tetrode vs silicon probe, probe geometry, etc.
+- 'shank' is very specific to silicon probes, replace with more generic info key?
+'''
+
+subject = 'd1pi042'
+
+tracks = [
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'p2-b5-03',
+     'recordingTrack': 'posteriorDiI', 'shank': 1, 'atlasZ': 234},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'p2-b5-03',
+     'recordingTrack': 'posteriorDiI', 'shank': 2, 'atlasZ': 234},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'p2-b5-03',
+     'recordingTrack': 'posteriorDiI', 'shank': 3, 'atlasZ': 234},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'p2-b5-03',
+     'recordingTrack': 'posteriorDiI', 'shank': 4, 'atlasZ': 234},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'p2-c4-03',
+     'recordingTrack': 'middleDiD', 'shank': 1, 'atlasZ': 242},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'p2-c4-03',
+     'recordingTrack': 'middleDiD', 'shank': 2, 'atlasZ': 242},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'p2-c4-03',
+     'recordingTrack': 'middleDiD', 'shank': 3, 'atlasZ': 242},
+
+    {'subject': subject, 'brainArea': 'LeftAstr', 'histImage': 'p2-c4-03',
+     'recordingTrack': 'middleDiD', 'shank': 4, 'atlasZ': 242},
+    ]
