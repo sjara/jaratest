c193f209d4cf6538b0684ce1ea4bbbbe2e3ed8f9
Santiago Jaramillo <sjara@uoregon.edu>
2019-11-22 09:57:56 -0800

    Added scripts to generate figure about model.

---
+++ common/2018acsup/figure_model.py
@@ -0,0 +1,167 @@
+''' 
+Show results of the model.
+
+Parameters taken from Li et al (2014)
+https://dx.doi.org/10.1523%2FJNEUROSCI.1516-14.2014
+
+IPSC Amplitude in Exc from PV:
+0 um : 157.4 pA
+80 um: 11.56 pA
+100um: 0 pA
+NOTE: findsigma(80,11.56,0,157.4) results in a sigma of 35.0
+
+IPSC Amplitude in Exc from SOM:
+0 um : 50.8 pA
+80 um: 30.9 pA
+240um: 7.62 pA
+400um: 0 pA
+NOTE: findsigma(80,30.9,0,50.8) results in a sigma of 80.23
+      findsigma(240,7.62,0,50.8) results in a sigma of 123.21
+
+So Amp is 3x higher for PV
+and StDev is 3x higher for SOM
+
+Total IPSC Amplitude in Exc:
+Activating PV : 442 pA 
+Activating SOM: 128 pA
+
+
+'''
+import os
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.colors
+#import matplotlib.patches as patches
+#from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+#from scipy import ndimage
+
+from jaratoolbox import settings
+from jaratoolbox import extraplots
+
+import figparams
+import studyparams
+import model_suppression as suppmodel
+reload(suppmodel)
+
+
+FIGNAME = 'figure_model'
+dataDir = os.path.join(settings.FIGURES_DATA_PATH, figparams.STUDY_NAME, FIGNAME)
+
+
+SAVE_FIGURE = 1
+outputDir = '/tmp/'
+figFilename = 'Fig8_model' # Do not include extension
+figFormat = 'pdf' # 'pdf' or 'svg'
+#figFormat = 'svg'
+#figSize = [6.6, 6] # In inches
+figSize = [10,6] # In inches
+
+
+fontSizeLabels = figparams.fontSizeLabels
+fontSizeTicks = figparams.fontSizeTicks
+fontSizePanel = figparams.fontSizePanel
+fontSizeLegend = figparams.fontSizeLegend
+
+labelPosX = [0.01, 0.3, 0.64]   # Horiz position for panel labels
+labelPosY = [0.96, 0.48, 0.48]    # Vert position for panel labels
+
+ExcColor = figparams.colp['excitatoryCell']
+Exclight = matplotlib.colors.colorConverter.to_rgba(ExcColor, alpha=0.5)
+PVcolor = figparams.colp['PVcell']
+PVlight = matplotlib.colors.colorConverter.to_rgba(PVcolor, alpha=0.5)
+SOMcolor = figparams.colp['SOMcell']
+SOMlight = matplotlib.colors.colorConverter.to_rgba(SOMcolor, alpha=0.5)
+
+# -- Simulate model --
+nCells = 101
+wParams = {'ampPV':-20, 'stdPV':10,
+           'ampSOM':-20, 'stdSOM':30,
+           'ampThal':100, 'stdThal':6}
+net = suppmodel.Network(nCells, wParams)
+centerCellOutput,  bandwidths, condLabels = net.simulate_inactivation()
+maxFiringRate = np.max(centerCellOutput[0,:])
+bandwidthsNormed = bandwidths/float(wParams['stdSOM'])
+
+# -- Plot resutls --
+fig = plt.gcf()
+fig.clf()
+fig.set_facecolor('w')
+
+gs = gridspec.GridSpec(2, 3, width_ratios=[0.8,0.8,1.1], height_ratios=[1,1])
+gs.update(top=0.96, left=0.07, right=0.95, bottom=0.11, wspace=0.5, hspace=0.3)
+
+# -- Panel Cartoon --
+axCartoon = plt.subplot(gs[0, 0:2])
+axCartoon.set_axis_off()
+axCartoon.annotate('A', xy=(labelPosX[0],labelPosY[0]), xycoords='figure fraction',
+                   fontsize=fontSizePanel, fontweight='bold')
+
+# -- Panel No PV --
+lineWidth = 3
+#xLims = [16,100]/stdSOM
+xLims = [0.6,3.4]
+#rangeToPlot = [10,100]
+axNoPV = plt.subplot(gs[1, 0])
+axNoPV.annotate('B', xy=(labelPosX[0],labelPosY[1]), xycoords='figure fraction',
+                fontsize=fontSizePanel, fontweight='bold')
+plt.plot(bandwidthsNormed, centerCellOutput[0,:]/maxFiringRate, '-', lw=lineWidth, color=ExcColor)
+plt.plot(bandwidthsNormed, centerCellOutput[1,:]/maxFiringRate, '--', lw=lineWidth, color=PVcolor)
+plt.xlim(xLims)
+plt.ylabel('Firing rate (normalized)')
+plt.xlabel('Bandwidth ($\sigma_{SOM}$)')
+plt.legend(['Control','No PV'], frameon=False, fontsize=fontSizeLegend)
+extraplots.boxoff(axNoPV)
+
+# -- Panel No PV --
+axNoSOM = plt.subplot(gs[1, 1])
+axNoSOM.annotate('C', xy=(labelPosX[1],labelPosY[1]), xycoords='figure fraction',
+                fontsize=fontSizePanel, fontweight='bold')
+plt.plot(bandwidthsNormed, centerCellOutput[0,:]/maxFiringRate, '-', lw=lineWidth, color=ExcColor)
+plt.plot(bandwidthsNormed, centerCellOutput[2,:]/maxFiringRate, '--', lw=lineWidth, color=SOMcolor)
+plt.xlim(xLims)
+plt.ylabel('Firing rate (normalized)')
+plt.xlabel('Bandwidth ($\sigma_{SOM}$)')
+plt.legend(['Control','No SOM'], frameon=False, fontsize=fontSizeLegend)
+extraplots.boxoff(axNoSOM)
+
+
+# -- Supp index and response change summaries --
+responseChangeFile = os.path.join(dataDir,'response_change_summary.npz')
+responseChangeData = np.load(responseChangeFile)
+suppIndexVec = responseChangeData['suppIndexVec']
+changeAtPeakVec = responseChangeData['changeAtPeakVec']
+changeAtWNVec = responseChangeData['changeAtWNVec']
+condLabels = responseChangeData['condLabels']
+
+markerSize = 3
+
+# -- Plot supp index --
+axSuppIndex = plt.subplot(gs[0, 2:])
+plt.plot(suppIndexVec[0],suppIndexVec[1],'s', color=PVcolor, mfc='none', ms=markerSize)
+plt.plot(suppIndexVec[0],suppIndexVec[2],'o', color=SOMcolor, mfc='none', ms=markerSize)
+xLims = [-0.1,1.1]
+plt.xlim(xLims)
+plt.ylim(xLims)
+plt.plot(xLims,xLims,'--',color='0.5')
+plt.xlabel('Suppression Index (control)')
+plt.ylabel('Suppression Index (inactivation)')
+plt.axis('square')
+extraplots.boxoff(axSuppIndex)
+
+# -- Plot change in response --
+axSuppIndex = plt.subplot(gs[1, 2:])
+plt.plot(changeAtPeakVec[0,:],changeAtWNVec[0,:],'s', color=PVcolor, mfc='none', ms=markerSize)
+plt.plot(changeAtPeakVec[1,:],changeAtWNVec[1,:],'o', color=SOMcolor, mfc='none', ms=markerSize)
+xLims = [-50,2200]
+plt.xlim(xLims)
+plt.ylim(xLims)
+plt.plot(xLims,xLims,'--',color='0.5')
+plt.xlabel('Change in response to preferred bandwidth')
+plt.ylabel('Change in response to WN')
+plt.axis('square')
+extraplots.boxoff(axSuppIndex)
+
+plt.show()
+
---
+++ common/2018acsup/generate_model.py
@@ -0,0 +1,123 @@
+"""
+Simulate model with many parameters.
+"""
+
+import os
+import numpy as np
+from jaratoolbox import settings
+import studyparams
+import figparams
+import model_suppression as suppmodel
+reload(suppmodel)
+
+figName = 'figure_model'
+dataDir = os.path.join(settings.FIGURES_DATA_PATH, figparams.STUDY_NAME, figName)
+
+# -- Simulate model --
+nCells = 101
+
+RANDOMIZED = 1
+np.random.seed(1)
+
+def random_in_range(low,high,shape):
+    """Return equally distributed random numbers in specified range"""
+    width = high-low
+    randVec = width*np.random.rand(shape) + low
+    return randVec
+
+if RANDOMIZED:
+    nSamples = 200
+    ampPVvec = random_in_range(-2, -44, nSamples)
+    ampSOMvec = random_in_range(-2, -44, nSamples)
+    stdThalvec = random_in_range(5, 10, nSamples)
+    suppIndexVec = np.empty((3,nSamples))     # 3:Control, PV, SOM
+    changeAtPeakVec = np.empty((2,nSamples))  # 2:PV-Control, SOM-Control
+    changeAtWNVec = np.empty((2,nSamples))    # 2:PV-Control, SOM-Control
+
+    for inds in range(nSamples):
+        wParams = {'ampPV':ampPVvec[inds], 'stdPV':10,
+                   'ampSOM':ampSOMvec[inds], 'stdSOM':30,
+                   'ampThal':100, 'stdThal':stdThalvec[inds]}
+        net = suppmodel.Network(nCells, wParams)
+        centerCellOutput,  bandwidths, condLabels = net.simulate_inactivation()
+        suppIndex = suppmodel.suppression_index(centerCellOutput)
+        changeAtPeak, changeAtWN = suppmodel.change_in_response(centerCellOutput)
+        suppIndexVec[:,inds] = suppIndex
+        changeAtPeakVec[:,inds] = changeAtPeak
+        changeAtWNVec[:,inds] = changeAtWN
+else:
+    ampPVvec = np.arange(-2,-44, -4)  # -20
+    ampSOMvec = np.arange(-2,-44, -4) # -20
+    stdThalvec = [5,7,9]#np.arange(3, 15, 2)  # 6
+
+    suppIndexAll = np.empty((3,len(ampPVvec),len(ampSOMvec),len(stdThalvec)))     # 3:Control, PV, SOM
+    changeAtPeakAll = np.empty((2,len(ampPVvec),len(ampSOMvec),len(stdThalvec)))  # 2:PV-Control, SOM-Control
+    changeAtWNAll = np.empty((2,len(ampPVvec),len(ampSOMvec),len(stdThalvec)))    # 2:PV-Control, SOM-Control
+
+    for indPV,ampPV in enumerate(ampPVvec):
+        for indSOM,ampSOM in enumerate(ampSOMvec):
+            for indThal,stdThal in enumerate(stdThalvec):
+                wParams = {'ampPV':ampPV, 'stdPV':10,
+                           'ampSOM':ampSOM, 'stdSOM':30,
+                           'ampThal':100, 'stdThal':stdThal}
+                net = suppmodel.Network(nCells, wParams)
+                centerCellOutput,  bandwidths, condLabels = net.simulate_inactivation()
+                suppIndex = suppmodel.suppression_index(centerCellOutput)
+                changeAtPeak, changeAtWN = suppmodel.change_in_response(centerCellOutput)
+                suppIndexAll[:,indPV,indSOM,indThal] = suppIndex
+                changeAtPeakAll[:,indPV,indSOM,indThal] = changeAtPeak
+                changeAtWNAll[:,indPV,indSOM,indThal] = changeAtWN
+    nConds = len(ampPVvec)*len(ampSOMvec)*len(stdThalvec)     
+    suppIndexVec = suppIndexAll.reshape([3,nConds])
+    changeAtPeakVec = changeAtPeakAll.reshape([2,nConds])
+    changeAtWNVec = changeAtWNAll.reshape([2,nConds])
+
+      
+import matplotlib.pyplot as plt
+
+plt.clf()
+
+markerSize = 3
+
+# -- Plot supp index --
+plt.subplot(2,2,1)
+plt.plot(suppIndexVec[0],suppIndexVec[1],'sb', mfc='none', ms=markerSize)
+plt.plot(suppIndexVec[0],suppIndexVec[2],'or', mfc='none', ms=markerSize)
+xLims = [-0.1,1.1]
+plt.xlim(xLims)
+plt.ylim(xLims)
+plt.plot(xLims,xLims,'--',color='0.5')
+plt.xlabel('Suppression Index (control)')
+plt.ylabel('Suppression Index (inactivation)')
+plt.axis('square')
+
+plt.subplot(2,2,2)
+avgSIchangePV = np.median(suppIndexVec[1]-suppIndexVec[0])
+avgSIchangeSOM = np.median(suppIndexVec[2]-suppIndexVec[0])
+plt.bar(1,avgSIchangePV, fc='w', ec='b', lw=2)
+plt.bar(2,avgSIchangeSOM, fc='w', ec='r', lw=2)
+
+
+# -- Plot change in response --
+plt.subplot(2,2,3)
+plt.plot(changeAtPeakVec[0,:],changeAtWNVec[0,:],'sb', mfc='none', ms=markerSize)
+plt.plot(changeAtPeakVec[1,:],changeAtWNVec[1,:],'or', mfc='none', ms=markerSize)
+xLims = [-50,1500]
+plt.xlim(xLims)
+plt.ylim(xLims)
+plt.plot(xLims,xLims,'--',color='0.5')
+plt.xlabel('Change in response to preferred bandwidth')
+plt.ylabel('Change in response to WN')
+plt.axis('square')
+
+plt.subplot(2,2,2)
+
+
+# -- Save data --
+outputFile = 'response_change_summary.npz'
+outputFullPath = os.path.join(dataDir,outputFile)
+
+np.savez(outputFullPath, suppIndexVec=suppIndexVec,
+         changeAtPeakVec=changeAtPeakVec, changeAtWNVec=changeAtWNVec,
+         condLabels=condLabels)
+print("Saved {}".format(outputFullPath))
