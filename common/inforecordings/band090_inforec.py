b359bda98344eec31a0db2b14b7312f1de7748a1
Anna Lakunina <alakunina@gmail.com>
2020-01-14 13:56:08 -0800

    fixed typo

--- common/inforecordings/band090_inforec.py
+++ common/inforecordings/band090_inforec.py
@@ -10,7 +10,7 @@ experiment = celldatabase.Experiment(subject,
 
 experiments.append(experiment)
 
-experiment.add_site(1000, data='2020-01-13', tetrodes=range(1, 9))
+experiment.add_site(1000, date='2020-01-13', tetrodes=range(1, 9))
 experiment.add_session('14-13-31', None, 'behavior', '2afc')
 experiment.add_session('14-16-15', None, 'noisebursts', 'am_tuning_curve')
 
