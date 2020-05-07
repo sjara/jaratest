4e0764e4d29a766aca7478a2febbcfbd79c4ce1d
bethmccarry <53198526+bethmccarry@users.noreply.github.com>
2020-05-07 11:57:03 -0700

    Add files via upload

--- beth/threetone/studyparams.py
+++ beth/threetone/studyparams.py
@@ -5,4 +5,8 @@ This file contains the default names and queries used for this study.
 STUDY_NAME = 'three_tone'
 
 # List of animals used in this study
-MICE_LIST = ['chad015']
+#MICE_LIST = ['chad013']
+MICE_LIST = 'chad015'
+#MICE_LIST = ['chad016']
+#MICE_LIST = ['chad018']
+#MICE_LIST = ['chad013', 'chad015', 'chad016']
