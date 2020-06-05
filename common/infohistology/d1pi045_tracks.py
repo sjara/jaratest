65a78ebac7498abe138a24a1842ab0a9b88bc238
Matt Nardoci <mattnardoci@gmail.com>
2020-06-05 11:42:27 -0700

    Tracks files Jewls made

---
+++ common/infohistology/d1pi045_tracks.py
@@ -0,0 +1,51 @@
+subject = 'd1pi045'
+tracks = [
+    {'subject':subject, 'brainArea':'RightAstr', 'histImage':'p1-b4-02',
+     'recordingTrack':'posteriorDiI', 'shank':1, 'atlasZ':259},
+
+    {'subject':subject, 'brainArea':'RightAstr', 'histImage':'p1-b6-02',
+     'recordingTrack':'posteriorDiI', 'shank':2, 'atlasZ':262},
+
+     {'subject':subject, 'brainArea':'RightAstr', 'histImage':'p1-c1-02',
+      'recordingTrack':'posteriorDiI', 'shank':3, 'atlasZ':272},
+
+     {'subject':subject, 'brainArea':'RightAstr', 'histImage':'p1-c2-02',
+      'recordingTrack':'posteriorDiI', 'shank':4, 'atlasZ':274},
+
+     {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-a4-03',
+       'recordingTrack':'posteriorDiD', 'shank':1, 'atlasZ':247},
+
+     {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-a5-03',
+         'recordingTrack':'posteriorDiD', 'shank':2, 'atlasZ':244},
+
+     {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-a5-03',
+     'recordingTrack':'posteriorDiD', 'shank':3, 'atlasZ':244},
+
+     {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-a5-03',
+         'recordingTrack':'posteriorDiD', 'shank':4, 'atlasZ':244},
+
+     {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-a5-03',
+     'recordingTrack':'posteriorDiI', 'shank':1, 'atlasZ':244},
+
+     {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-a5-03',
+     'recordingTrack':'posteriorDiI', 'shank':2, 'atlasZ':244},
+
+    {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-a5-03',
+      'recordingTrack':'posteriorDiI', 'shank':3, 'atlasZ':244},
+
+    {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-a5-03',
+       'recordingTrack':'posteriorDiI', 'shank':4, 'atlasZ':244},
+
+    {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-b6-03',
+        'recordingTrack':'anteriorDiD', 'shank':1, 'atlasZ':265},
+
+    {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-b5-03',
+    'recordingTrack':'anteriorDiD', 'shank':2, 'atlasZ':267},
+
+    {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-b5-03',
+    'recordingTrack':'anteriorDiD', 'shank':3, 'atlasZ':267},
+
+    {'subject':subject, 'brainArea':'LeftAstr', 'histImage':'p1-b6-03',
+    'recordingTrack':'anteriorDiD', 'shank':4, 'atlasZ':265},
+
+]
