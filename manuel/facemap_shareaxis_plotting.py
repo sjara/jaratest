f213985c13279bf6e427bf6f537bfe7522f1b17b
Manuel <manuelo@uoregon.edu>
2021-07-29 08:31:24 -0700

    code update for share x axis and plot

--- manuel/facemap_shareaxis_plotting.py
+++ manuel/facemap_shareaxis_plotting.py
@@ -8,9 +8,11 @@ proc = np.load('your_video_proc.npy', allow_pickle = True).item()
 
 pupil = proc['pupil'][0] # dtype = Dic
 pArea = pupil['area']    # dtype = numpy.array
+pArea1 = pupil[:-5]    # Eliminates last 5 elements within arrays: fixes infite line length in plot (X axis)
 blink = proc['blink'][0] # dtype = numpy.array
 blink1 = proc['blink']   # dtype = List
 blink2 = np.array(blink).T # Creates transpose matrix of blink. Necessary for plotting
+blink2_2 = blink2[:-5]  # Eliminates last 5 elements within arrays: fixes infite line length in plot (Y axis)
 
 fig, (pupil, blink) = plt.subplots(2, 1, sharex = True, sharey = False, constrained_layout = True) #Tried changing it to fig, (pArea, blink1). Didn't work
 
