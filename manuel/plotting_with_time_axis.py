898e882343dbed0de3698cc518d203ed5a654b61
Manuel <manuelo@uoregon.edu>
2021-08-02 08:25:18 -0700

    Updated w/out hardcoding

--- manuel/plotting_with_time_axis.py
+++ manuel/plotting_with_time_axis.py
@@ -17,16 +17,18 @@ blink = proc['blink'][0] # numpy.array
 blink1 = proc['blink']   # List
 blink2 = np.array(blink).T # Creates transpose matrix of blink. Necessary for plotting
 blink2_a = blink[:-5] # Elimination of last 5 elements of array. Fixes infinite line length (Y axis)
-print(pAreaa.shape)
-print(blink2_a.shape)
+#print(pAreaa.shape)
+#print(blink2_a.shape)
 
 framerate = 30 # frame rate of video
-total_frames = 993 # In this particular case, you can use the total frames of the video (998) or to be more precise, the number of frames contained within the plotting values (pAreaa & blink2_a).
-time = (total_frames * 1)/framerate # Time to reproduce 998 frames
+total_frames = pAreaa # In this particular case, you can use the total frames to plot, or to be more precise, the number of frames contained within the plotting values (pAreaa & blink2_a)
+time = (total_frames * 1)/framerate # Time to reproduce the pAreaa & blink2_a frames. This apply only in this case, because they have the same number of frames.
 #print(time)
-step_number = time/993 # Time required to reproduce each of those 993 frames.
-time_array = np.arange(0, time, step_number)
+step_number_array = time/pAreaa # Time required to reproduce each of those 993 frames from pAreaa.
+step_number = step_number_array[0]
+time_array = np.arange(0, time, step_number) # Time used to plot in the x axis
 #print(time_array)
+#print(step_number)
 
 fig, (pupil, blink) = plt.subplots(2, 1, sharex = True, sharey = False, constrained_layout = True)
 pupil.set(title = 'Animal name and session date', ylabel = 'Pupil Area', xlabel = 'Time(s)')
