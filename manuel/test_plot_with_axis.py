6ec0008f76e6925fa7840b5ed3bd32fd0d2a7b1a
Manuel <manuelo@uoregon.edu>
2021-08-05 13:06:54 -0700

    Testing changes in the plot_with_axis.py

--- manuel/test_plot_with_axis.py
+++ manuel/test_plot_with_axis.py
@@ -5,13 +5,35 @@ Calculating the time(s) for the x axis for FaceMap plots
 #Python
 import numpy as np
 import matplotlib.pyplot as plt
-
+#from jaratoolbox import pupilanalysis
 
 proc = np.load('./to_compare/chad045_030_1_proc.npy', allow_pickle = True).item()
 #Note: the proc.npy is the output file generated from facemap
 
-pupil = proc['pupil'][0] # Dic                                                                                                                                                                             
+pupil = proc['pupil'][0] # Dic
 pArea = pupil['area']    # numpy.array
+nframes = len(pArea)     # Contains length of pArea variable. It has the calculation of the pupil area for each frame
+frameVec = np.arange(0, nframes, 1) #Vector of the total frames from the video
+framerate = 30 # frame rate of video
+timeVec = (frameVec * 1)/framerate # Time Vector to calculate the length of the video 
+print(timeVec)
+print(frameVec)
+fig, (pupil_axis, blink_axis) = plt.subplots(2, 1, sharex = True, sharey = False, constrained_layout = True)
+pupil_axis.plot(timeVec, pArea)
+pupil_axis.set_xlim([0, nframes])
+#pupil_axis.set_ylim([0, 1500])
+blink_axis.set_xlabel('Time (s)')
+plt.show()
+
+'''
+pupil_axis.plot(frameVec, pArea)
+pupil_axis.set_xlim([0, nframes-20])
+pupil_axis.set_ylim([0, 1500])
+blink_axis.set_xlabel('Frame')
+plt.show()
+'''
+
+"""
 pAreaa = pArea[:-5] # Elimination of last 5 elements of array. Fixes infinite line length (X axis)
 blink = proc['blink'][0] # numpy.array
 blink1 = proc['blink']   # List
@@ -40,3 +62,4 @@ blink.set(title = 'Animal name and session date', ylabel = 'on/off', xlabel = 'T
 blink.grid(b = True)
 blink.plot(time_array, blink2_a)
 plt.show()
+"""
