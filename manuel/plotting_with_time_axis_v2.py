384bb0bc0a791afdaf1bfa4154779935bf67b58a
Manuel <manuelo@uoregon.edu>
2021-08-06 11:10:56 -0700

    enhanced: plotting with time axis plot

--- manuel/plotting_with_time_axis_v2.py
+++ manuel/plotting_with_time_axis_v2.py
@@ -47,7 +47,7 @@ superior_limit_y_axis = pArea[nframes_pArea - 6] # Defines max value for the y a
 
 
 fig, (pupil_axis, blink_axis) = plt.subplots(2, 1, sharex = True, sharey = False, constrained_layout = True)
-pupil_axis.set(title = 'Animal name and session date', ylabel = 'Pupil Area')
+pupil_axis.set(title = 'Chad045, 2020-10-30', ylabel = 'Pupil Area')
 plt.setp((pupil_axis, blink_axis), xticks = (np.arange(0, 35, 1))) #Sets up number of ticks in both plots
 pupil_axis.plot(timeVec_pArea, pArea)
 pupil_axis.set_xlim([min_value_timeVec_pArea, max_value_timeVec_pArea])
@@ -58,5 +58,7 @@ blink_axis.plot(timeVec_blink2, blink2)
 blink_axis.set_xlim([min_value_timeVec_blink2, max_value_timeVec_blink2])
 blink_axis.grid(b = True)
 plt.show()
+
+
 
 #Important for user: the frameVec_pArea and frameVec_blink2 are the same, as well as timeVec_pArea and timeVec_blink2 and so on with the other variables with the pArea and blink2 suffixes in this exercise, but it's good to keep in mind that the frames in the pupil area might not be the same as the ones in the sync signal in other videos, so play safe and create a variable for each one of them. In the particular case of the timeVec, one can use either the pArea or blink2 variables to calculate it (therefore, it can be called just "timeVec"). But again, it was done like this to raise awareness regarding the chance of the variables to have different values. Same logic applies to the rest of the variables with the pArea and blink2 suffixes.
