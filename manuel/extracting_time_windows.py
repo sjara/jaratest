38d4cf0175ed21d45dfcc290d17fd9c1a5fcaaf0
Manuel <manuelo@uoregon.edu>
2021-08-19 08:58:51 -0700

    Updated the extracting time windows code

--- manuel/extracting_time_windows.py
+++ manuel/extracting_time_windows.py
@@ -6,7 +6,7 @@ Calculating the time(s) for the x axis for FaceMap plots
 #Python
 import numpy as np
 import matplotlib.pyplot as plt
-#from jaratoolbox import pupilanalysis
+#from jaratoolbox import pupilanalysis.
 
 
 
@@ -28,20 +28,22 @@ blink2 = np.array(blink).T # Creates transpose matrix of blink. Necessary for pl
 nframes = len(pArea) # Contains length of pArea variable (equivalent to the blink variable).
 frameVec = np.arange(0, nframes, 1) # Vector of the total frames from the video.
 framerate = 30 # frame rate of video
-timeVec = (frameVec * 1)/framerate # Time Vector to calculate the length of the video 
+timeVec = (frameVec * 1)/framerate # Time Vector to calculate the length of the video .
 
 
 
-minValueTimeVec = np.amin(timeVec) # Minimum value of the variable timeVec_pArea. Added for precision
-maxValueTimeVec = np.amax(timeVec) # Maximum value of the variable timeVec_pArea. Made to prevent the plotting of space without data in the plot
-#range_time = np.array([min_value_timeVec, max_value_timeVec]) # Shows starting and ending seconds of the video
+minValueTimeVec = np.amin(timeVec) # Minimum value of the variable timeVec_pArea. Added for precision.
+maxValueTimeVec = np.amax(timeVec) # Maximum value of the variable timeVec_pArea. Made to prevent the plotting of space without data in the plot.
+#range_time = np.array([min_value_timeVec, max_value_timeVec]) # Shows starting and ending seconds of the video.
 
 
-blink2Bool = np.logical_and(blink2>1000, blink2<2000) # Boolean values from the blink2 variable where True values will be within the established range
+blink2Bool = np.logical_and(blink2>1000, blink2<2000) # Boolean values from the blink2 variable where True values will be within the established range.
 blink2RangeValues = np.diff(blink2Bool) # Determines the start and ending values (as the boolean value True) where the sync signal is on. 
-startValueSyncSignal = np.flatnonzero(blink2RangeValues) # Provides all the indices of numbers assigned as 'True' from the blink2_binary variable
-timeOfBlink2Event = timeVec[startValueSyncSignal] # Provides the time windows in which the sync signal is on
-rangeTime = np.array([-1, 2]) # Range of time window
+indicesValueSyncSignal = np.flatnonzero(blink2RangeValues) # Provides all the indices of numbers assigned as 'True' from the blink2_binary variable.
+indicesSyncSignal = [0, 2, 4, 6, 8, 10] # Obtains all indices corresponding to the start values from the sync signal.
+startValuesSyncSignalVec = np.take(indicesValueSyncSignal, indicesSyncSignal) # Takes the established indices in indicesSyncSignal and looks for the corresponding numbers within the variable indicesValueSyncSignal
+timeOfBlink2Event = timeVec[startValuesSyncSignalVec] # Provides the time windows in which the sync signal is on.
+rangeTime = np.array([-1, 2]) # Range of time window, one second before the sync signal is on and two seconds after is on.
 
 
 def eventlocked_signal(timeVec, signal, eventOnsetTimes, windowTimeRange):
@@ -71,15 +73,13 @@ def eventlocked_signal(timeVec, signal, eventOnsetTimes, windowTimeRange):
 
 windowTimeVec, windowed_signal = eventlocked_signal(timeVec, pArea, timeOfBlink2Event, rangeTime)
 
-samplingRate = 1/(timeVec[1]-timeVec[0])
-windowSampleRange = samplingRate*np.array(rangeTime) 
-windowSampleVec = np.arange(*windowSampleRange, dtype=int)
-windowTimeVec = windowSampleVec/samplingRate
-nSamples = len(windowTimeVec)
-nTrials = len(timeOfBlink2Event)
-lockedSignal = np.empty((nSamples,nTrials))
+plt.plot(windowTimeVec, windowed_signal)
+plt.title('Trials Vs Time window (s)')
+plt.xlabel('Time window (s)')
+plt.ylabel('Trials')
+plt.show()
 
-
+'''
 fig, (signalWindow, comparePlot) = plt.subplots(2, 1, sharex = False, sharey = False, constrained_layout = True)
 signalWindow.plot(windowTimeVec, windowed_signal)
 signalWindow.set(title = 'Trials Vs Time window (s)' , xlabel = 'Time window (s)', ylabel = 'Trials')
@@ -88,3 +88,4 @@ comparePlot.plot(timeVec, blink2)
 comparePlot.set(title = 'Sync signal Vs Time (s)', xlabel = 'Time (s)', ylabel = 'on/off')
 comparePlot.grid(b = True)
 plt.show()
+'''
