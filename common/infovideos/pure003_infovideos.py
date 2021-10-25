c2e4dfefa99d39588ae46b359c98b03c9089b7f7
Manuel <manuelo@uoregon.edu>
2021-10-25 09:11:30 -0700

    infovideo file for pure003

---
+++ common/infovideos/pure003_infovideos.py
@@ -0,0 +1,25 @@
+'''
+Information about videos.
+'''
+
+from jaratoolbox import videoinfo
+
+        
+subject = 'pure003'
+videos = videoinfo.Videos(subject)
+cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
+
+videos.add_session('2021-09-28', 'positiveControl',
+                   'pure003_20210928_syncVisibleNoSound_01.mkv',
+                   'pure003_detectsound_20210928_syncVisibleNoSound_01.h5',
+                   cameraParams) 
+                   
+videos.add_session('2021-09-28', 'negativeControl',
+                   'pure003_20210928_NoSound_01.mkv',
+                   'pure003_detectsound_20210928_syncNoSound_01.h5',
+                   cameraParams)
+
+videos.add_session('2021-09-28', 'experimental',
+                   'pure003_20210928_syncSound_01.mkv',
+                   'pure003_detectsound_20210928_syncSound_01.mkv.h5',
+                   cameraParams)
