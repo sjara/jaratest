5345f868fb5eea58a7780177bff6f2d5ca2d30b8
temeritylab <51007421+temeritylab@users.noreply.github.com>
2021-10-27 02:44:42 -0700

    Create chad047_infovideos.py

---
+++ common/infovideos/chad047_infovideos.py
@@ -0,0 +1,28 @@
+'''
+Information about videos.
+'''
+
+from jaratoolbox import videoinfo
+
+        
+subject = 'chad045'
+videos = videoinfo.Videos(subject)
+cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
+
+videos.add_session('2020-10-30', 'positiveControl',
+                   'chad047_detect_sound_pos_20201030_config0.mkv',
+                   cameraParams) 
+videos.add_session('2020-10-22', 'positiveControl',
+                   'chad047_detect_sound_pos_20201022_config0.mkv',
+                    cameraParams) 
+               
+videos.add_session('2020-11-02', 'negativeControl',
+                   'chad047_detect_sound_neg_20201102_config0.mkv',
+                   cameraParams)
+videos.add_session('2020-12-10', 'negativeControl',
+                   'chad047_detect_sound_neg_20201210_config0.mkv',
+                   cameraParams)
+                   
+videos.add_session('2020-12-10', 'experimental',
+                   'chad047_detect_sound_sound_20201210_config0.mkv',
+                   cameraParams)
