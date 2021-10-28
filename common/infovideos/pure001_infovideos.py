e7ce80572c563ed3ad3342ca209aca90f81421c8
Manuel <manuelo@uoregon.edu>
2021-10-28 08:16:25 -0700

    infovideo file for pure001

--- common/infovideos/pure001_infovideos.py
+++ common/infovideos/pure001_infovideos.py
@@ -19,7 +19,7 @@ videos.add_session('2021-09-28', 'negativeControl',
                    'pure001_detectsound_20210928_syncNoSound_01.h5',
                    cameraParams)
 
-videos.add_session('2021-09-28', 'experimental',
+videos.add_session('2021-09-28', 'chordTrain',
                    'pure001_20210928_syncSound_01.mkv',
                    'pure001_detectsound_20210928_syncSound_01.h5',
                    cameraParams)
