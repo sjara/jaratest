760e77b9c18b86647bf04134ea59b717af45c406
temeritylab <51007421+temeritylab@users.noreply.github.com>
2021-11-10 14:29:03 -0800

    Update chad048_infovideos.py

--- common/infovideos/chad048_infovideos.py
+++ common/infovideos/chad048_infovideos.py
@@ -11,15 +11,19 @@ cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
 
 videos.add_session('2020-10-22', 'positiveControl',
                    'chad048_detect_sound_pos_20201022_config0.mvk',
+                   '',
                    cameraParams) 
 videos.add_session('2020-11-09', 'positiveControl',
                    'chad048_detect_sound_pos_20201109_config0.mkv',
+                   '',
                     cameraParams) 
                
 videos.add_session('2020-12-10', 'negativeControl',
                    'chad048_detect_sound_neg_20201210_config0.mkv',
+                   '',
                    cameraParams)
                    
 videos.add_session('2020-12-10', 'experimental',
                    'chad048_detect_sound_sound_20201210_config0.mkv',
+                   '',
                    cameraParams)
