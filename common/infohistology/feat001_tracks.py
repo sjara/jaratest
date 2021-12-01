0ca82507fdb79f2b840dfa5079e04c943dfcdb01
JaraLab User <jlmohn@ucdavis.edu>
2021-12-01 14:26:49 -0800

    fixed typos in feat001_tracks.py

--- common/infohistology/feat001_tracks.py
+++ common/infohistology/feat001_tracks.py
@@ -1,16 +1,16 @@
 subject = 'feat001'
 tracks = [
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1_C5',
+    {'subject':subject, 'brainArea':'left_AC', 'histImage':'p1_C5',
      'recordingTrack':'anterolateral_DiI', 'shank':1, 'atlasZ':218},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1_C3',
+    {'subject':subject, 'brainArea':'left_AC', 'histImage':'p1_C3',
      'recordingTrack':'anteromedial_DiD', 'shank':1, 'atlasZ':209},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1_C1',
+    {'subject':subject, 'brainArea':'left_AC', 'histImage':'p1_C1',
      'recordingTrack':'middlelateral_DiD', 'shank':1, 'atlasZ':203},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1_B4',
+    {'subject':subject, 'brainArea':'left_AC', 'histImage':'p1_B4',
      'recordingTrack':'caudomedial_DiI', 'shank':1, 'atlasZ':193},
 
-    {'subject':subject, 'brainArea':'LeftAC', 'histImage':'p1_B3',
+    {'subject':subject, 'brainArea':'left_AC', 'histImage':'p1_B3',
      'recordingTrack':'caudolateral_DiI', 'shank':1, 'atlasZ':187},
