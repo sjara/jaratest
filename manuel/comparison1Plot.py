4731615ab2766f3b1d08faddf4cc36a270cd9113
Manuel <manuelo@uoregon.edu>
2022-01-11 14:15:36 -0800

    Code for plotting different amount of plots of pupil behavior to sound stimulus

---
+++ manuel/comparison1Plot.py
@@ -0,0 +1,178 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+def onset_values(signalArray): 
+
+     '''
+     Helps to find onset start values of the sync singal in any given array: 
+     Args: 
+     SignalArray (np.array) = array that contains data of the sync signal
+     Returns:
+     onsetStartValues (np.array)  = an array of the indices containing the start onset values of the sync signal.
+    ''' 
+     firstIndexValue = 0 
+     lastIndexValue = len(signalArray)-1 
+     stepNumber = 2
+     startIndicesValues = range(firstIndexValue, lastIndexValue, stepNumber)
+     startIndicesVec = np.array(startIndicesValues)
+     onsetStartValues = np.take(signalArray, startIndicesVec)
+     return (onsetStartValues)
+     
+def eventlocked_signal(timeVec, signal, eventOnsetTimes, windowTimeRange):
+    '''
+    Make array of signal traces locked to an event.
+    Args:
+        timeVec (np.array): time of each sample in the signal.
+        signal (np.array): samples of the signal to process.
+        eventOnsetTimes (np.array): time of each event.
+        windowTimeRange (list or np.array): 2-element array defining range of window to extract.
+    Returns: 
+        windowTimeVec (np.array): time of each sample in the window w.r.t. the event.
+        lockedSignal (np.array): extracted windows of signal aligned to event. Size (nSamples,nEvents)
+    '''
+    samplingRate = 1/(timeVec[1]-timeVec[0])
+    windowSampleRange = samplingRate*np.array(windowTimeRange) 
+    windowSampleVec = np.arange(*windowSampleRange, dtype=int)
+    windowTimeVec = windowSampleVec/samplingRate
+    nSamples = len(windowTimeVec)
+    nTrials = len(eventOnsetTimes)
+    lockedSignal = np.empty((nSamples,nTrials))
+    for inde,eventTime in enumerate(eventOnsetTimes):
+       eventSample = np.searchsorted(timeVec, eventTime)
+       #print('eventS:',eventSample)
+       thiswin = windowSampleVec + eventSample
+       #print('thiswin:',thiswin)
+       #print(thiswin.shape, eventSample.shape)
+       lockedSignal[:,inde] = signal[thiswin]
+    return (windowTimeVec, lockedSignal)
+    
+def find_prepost_values(timeArray, dataArray, preLimDown, preLimUp, postLimDown, postLimUp): 
+  
+      '''  
+      Obtain pupil data before and after stimulus  
+      Args:  
+      timeArray (np.array): array of the time window to evaluate pupil area obtained from even  t_locked  
+      dataArray (np.array): array of the pupil data obtained from event_locked function  
+      preLimDown (int or float): first number of the time interval to evaluate before stimulus onset  
+      preLimUp (int or float): second number of the time interval to evaluate before stimulus onset
+      postLimDown (int or float): first number of the time interval to evaluate after stimulus onset  
+      postLimUp (int or float): second number of the time interval to evaluate after stimulus onset 
+      Returns: 
+      preData (np.array): array with the pupil data before stimulus 
+      postData (np.array): array with the pupil data after stimulus    
+      '''   
+      preBool = np.logical_and(preLimDown <= timeArray, timeArray < preLimUp) 
+      postBool = np.logical_and(postLimDown <= timeArray, timeArray < postLimUp) 
+      preValuesIndices = np.argwhere(preBool == True)  
+      postValuesIndices = np.argwhere(postBool == True)  
+      preProcessedPreValues = dataArray[preValuesIndices]  
+      preProcessedPostValues = dataArray[postValuesIndices]  
+      preData = preProcessedPreValues.reshape(preValuesIndices.shape[0], dataArray.shape[1]) 
+      postData = preProcessedPostValues.reshape(postValuesIndices.shape[0], dataArray.shape[1])  
+      return(preData, postData)
+      
+def comparison_plot(time, valuesData1, pVal): 
+     ''' 
+     Creates 1 figure with 3 plots 
+     Args: 
+     time = vector values for x axis 
+     valuesData1 (np.array) = vector values for y axis of the first plot 
+     valuesData2 (np.array)= vector values for y axis of the second plot
+     valuesData3 (np.array)= vector values for y axis of the third plot
+     returns: 
+     plt.show() = 1 figure with 3 plots using the input data 
+     ''' 
+     labelsSize = 16
+     fig, subplt = plt.subplots(1,1)
+     fig.set_size_inches(9.5, 7.5, forward = True)
+     sp = np.round(pVal, decimals=6)
+     label1 = filesDict['config1'],'pval:',sp
+     
+
+     subplt.plot(time, valuesData1, color = 'g', label = label1, linewidth = 4)
+
+     subplt.set_xlabel('Time (s)', fontsize = labelsSize)
+     subplt.set_ylabel('Pupil Area', fontsize = labelsSize)
+     subplt.set_title('Pupil behavior in different conditions: pure006', fontsize = labelsSize)
+     plt.grid(b = True)
+     #plt.ylim([550, 650])
+     plt.xticks(fontsize = labelsSize)
+     plt.yticks(fontsize = labelsSize)
+     plt.legend()
+     #plt.legend(prop ={"size":10}, bbox_to_anchor=(1.0, 0.8))
+     #plt.savefig('comparisonPure004Plot', format = 'pdf', dpi = 50)
+     plt.show() 
+     return(plt.show())
+      
+def pupilDilation_time(timeData1, plotData1, pvalue):
+     shortPval = np.round(pvalue, decimals = 6)
+     lab = 'p-value', shortPval 
+     plt.plot(timeData1,plotData1, label = lab)
+     plt.title('pure004_20220110_2Sounds: average pupil behavior') 
+     plt.ylabel('Pupil Area', fontsize = 13)
+     plt.xlabel('Time(s)', fontsize = 13)
+     plt.legend()
+     plt.show() 
+     return(plt.show())
+
+filesDict = {'file1':'pure005_20220111_2sounds_51_2Sconfig1_proc.npy', 
+	'loadFile1':np.load('./project_videos/mp4Files/mp4Outputs/pure005_20220111_2sounds_51_2Sconfig1_proc.npy', allow_pickle = True).item(), 
+	'config1':'2Sconfig1', 'sessionFile1':'46', 
+	'condition':'2Sounds', 'sound':'ChordTrain', 'mouse':'pure006'}
+
+proc = filesDict['loadFile1']
+
+#---obtain pupil data---
+pupil = proc['pupil'][0] # Dic.
+pArea = pupil['area']    # numpy.array. Contains calculation of the pupil area in each frame of the video.
+blink = proc['blink'][0] # numpy.array. Contains calculation of the sync signal in each frame of the video.
+blink1 = proc['blink']   # List.
+blink2 = np.array(blink).T # Creates transpose matrix of blink. Necessary for plotting.
+
+
+#---obtain values where sync signal is on---
+minBlink = np.amin(blink2)
+maxBlink = np.amax(blink2) - np.amin(blink2)//2
+blink2Bool = np.logical_and(blink2 > minBlink, blink2 < maxBlink) # Boolean values from the blink2 variable where True values will be within the established range.
+blink2RangeValues = np.diff(blink2Bool) # Determines the start and ending values (as the boolean value True) where the sync signal is on. 
+indicesValueSyncSignal = np.flatnonzero(blink2RangeValues) # Provides all the indices of numbers assigned as 'True' from the blink2_binary variable.
+
+
+#---calculate number of frames, frame rate, and time vector---
+nframes = len(pArea) # Contains length of pArea variable (equivalent to the blink variable).
+frameVec = np.arange(0, nframes, 1) # Vector of the total frames from the video.
+#newFrame = frameVec[blink2Bool]
+framerate = 30 # frame rate of video
+timeVec = (frameVec * 1)/framerate # Time Vector to calculate the length of the video.
+
+
+#--- obtaining onset sync signal values ---
+syncOnsetValues = onset_values(indicesValueSyncSignal) #--> if the terminal complains around here, check the blink2Bool variable.
+timeOfBlink2Event = timeVec[syncOnsetValues] # Provides the time values in which the sync signal is on.
+timeOfBlink2Event = timeOfBlink2Event[1:-1]
+
+#--- Align trials to the event ---
+timeRange = np.array([-0.5, 2.0]) # Range of time window, one second before the sync signal is on and one second after is on. For syncSound [-0.95,0.95] and for controls [-0.6,0.6]
+windowTimeVec, windowed_signal = eventlocked_signal(timeVec, pArea, timeOfBlink2Event, timeRange)
+
+#--- Obtain pupil pre and post stimulus values, and average size ---
+preSignal, postSignal = find_prepost_values(windowTimeVec, windowed_signal, -0.5, 0, 1.4, 2.0)
+averagePreSignal = preSignal.mean(axis = 0)
+averagePostSignal = postSignal.mean(axis = 0)
+dataToPlot = [averagePreSignal, averagePostSignal]
+xlabels = ['Pre signal', 'Post signal']
+
+
+#--- Defining the correct time range for pupil's relaxation (dilation) ---
+timeRangeForPupilDilation = np.array([-15, 15])
+pupilDilationTimeWindowVec, pAreaDilated = eventlocked_signal(timeVec, pArea, timeOfBlink2Event, timeRangeForPupilDilation)
+pAreaDilatedMean = pAreaDilated.mean(axis = 1)
+
+#--- Wilcoxon test to obtain statistics ---
+wstat, pval = stats.wilcoxon(averagePreSignal, averagePostSignal)
+print('Wilcoxon value config14_1', wstat,',',  'P-value config14_1', pval)
+
+OverLapPlots = comparison_plot(pupilDilationTimeWindowVec, pAreaDilatedMean, pval)
+wstat, pval = stats.wilcoxon(averagePreSignal, averagePostSignal)
+print('Wilcoxon value 2Sconfig1', wstat,',',  'P-value 2Sconfig1', pval)
