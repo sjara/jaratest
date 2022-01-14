d89237ecac70775d4b00c89c8b96ed3a99d658c4
JaraLab User <jlmohn@ucdavis.edu>
2022-01-14 12:45:32 -0800

    updated daily behavior script

--- jenny/run_daily_behav.py
+++ jenny/run_daily_behav.py
@@ -23,27 +23,9 @@ from jaratoolbox import behavioranalysis
 
 # Add the subject(s)
 ## VOT animals
-#subject = 'bili034'
-#subject = 'bili035'
-#subject = 'bili036'
-#subject = 'bili037'
-#subject = 'bili038'
-#subject = 'bili039'
-#subject = 'bili040'
-#subject = 'bili041'
-#subject = 'bili042'
 #subject = ['bili034', 'bili035', 'bili036', 'bili037', 'bili038', 'bili039', 'bili040', 'bili041', 'bili042']
 
 ## FT animals
-#subject = 'bili043'
-#subject = 'bili044'
-#subject = 'bili045'
-#subject = 'bili046'
-#subject = 'bili047'
-#subject = 'bili048'
-#subject = 'bili049'
-#subject = 'bili050'
-#subject = 'bili051'
 #subject = ['bili043', 'bili044', 'bili045', 'bili046', 'bili047', 'bili048', 'bili049', 'bili050', 'bili051']
 
 print('Enter which subjects you want to look at: 1 = VOT, 2 = FT, 3 = all, or enter a specific animal name')
@@ -64,27 +46,6 @@ paradigm = '2afc_speech'
 print('input the session name (e.g. 20220113a):')
 session = input()
 
-'''
-## Single session/subject
-bdata = behavioranalysis.load_many_sessions(subject, sessions, paradigm)
-sessionID = bdata['sessionID']
-uniqueSessionID = np.unique(sessionID)
-outcomeMode = bdata['outcomeMode']
-sidesDirect = bdata['outcomeMode'] == bdata.labels['outcomeMode']['sides_direct']
-automationMode = np.any(bdata['automationMode'])
-direct = bdata['outcomeMode'] == bdata.labels['outcomeMode']['direct']
-
-if automationMode == 1:
-    maxDelay = np.max(bdata['delayToTarget'])
-    print(maxDelay)
-else:
-
-print(subject)
-numTrls = len(bdata['sessionID'])
-print(numTrls)
-print(np.unique(outcomeMode))
-
-'''
 
 ## Multiple subjects, single day
 for nSub in range(len(subject)):
@@ -100,12 +61,10 @@ for nSub in range(len(subject)):
     print(mode)
     print('numTrials')
     print(numTrials)
-    print()
-
-
+    
     if automationMode == 1:
         maxDelay = np.max(bdata['delayToTarget'])
+        print('maxDelay')
         print(maxDelay)
+    print()
 
-
-# testing
