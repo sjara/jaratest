98d1da37f59c5a1e823cd74043de3056aa320b08
jmohnn <jlmohn@ucdavis.edu>
2022-02-21 13:26:59 -0800

    added feat006 inforec/vid files

---
+++ common/inforecordings/feat006_inforec.py
@@ -0,0 +1,27 @@
+from jaratoolbox import celldatabase
+
+subject = 'feat006'
+experiments = []
+
+# Experiment parameters: subject, date, brainArea, recordingTrack (penetrationLocationAndDye),
+#    info (which contains [probeOrientation, soundSource, probeConfiguration]).
+# Session parameters: sessionTime, behaviorFileSuffix, sessionType paradigmName.
+
+
+exp0 = celldatabase.Experiment(subject, '2022-02-21', brainArea='AC_right', probe = 'NPv1-8131', recordingTrack='caudomedialDiI', info=['caudomedialDiI', 'soundLeft']) #reference = external
+experiments.append(exp0)
+# 11:15 in booth
+# 11:30 couldn't penetrate brain. Retracted and tried cleaning craniotomy
+# 11:45 tried lowering electrodes again. In brain
+# 11:53 reached max depth
+# 12:20 started recording
+# 1:25 done
+
+exp0.add_site(2934)
+exp0.maxDepth = 2934
+exp0.add_session('12-20-35','a','AM','am_tuning_curve') 
+exp0.add_session('12-32-36','b','pureTones','am_tuning_curve')
+exp0.add_session('12-49-24','a','FTVOTBorders','2afc_speech')
+
+
+
---
+++ common/infovideos/feat006_infovideos.py
@@ -0,0 +1,26 @@
+from jaratoolbox import videoinfo
+
+subject = 'feat006'
+videos = videoinfo.Videos(subject)
+cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
+
+# Session parameters: date, sessionType, videoFile, behavFile, cameraParams
+
+# 2022-02-21
+
+videos.add_session('2022-02-21', 'AM',
+                   'feat006_am_tuning_curve_20220221_01.mkv',
+                   'feat006_am_tuning_curve_20220221a.h5',
+                   cameraParams) 
+
+videos.add_session('2022-02-21', 'pureTones',
+                   'feat006_am_tuning_curve_20220221_02.mkv',
+                   'feat006_am_tuning_curve_20220221b.h5',
+                   cameraParams) 
+                   
+videos.add_session('2022-02-21', 'FTVOTBorders',
+                   'feat006_2afc_speech_20220221_01.mkv',
+                   'feat006_2afc_speech_20220221a.h5',
+                   cameraParams) 
+                   
+ 
