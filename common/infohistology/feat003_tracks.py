75cea109f7d649686059e5aaf618c4ff40ab765f
JaraLab User <jlmohn@ucdavis.edu>
2022-02-23 11:05:17 -0800

    added feat003 tracks file

---
+++ common/infohistology/feat003_tracks.py
@@ -0,0 +1,13 @@
+subject = 'feat003'
+tracks = [
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'',
+     'recordingTrack':'anterolateralDiI', 'shank':0, 'atlasZ':},
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'',
+      'recordingTrack':'anterolateralDiI', 'shank':1, 'atlasZ':},
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'',
+     'recordingTrack':'anteromedialDiD', 'shank':0, 'atlasZ':},
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'',
+      'recordingTrack':'anteromedialDiD', 'shank':1, 'atlasZ':}]
