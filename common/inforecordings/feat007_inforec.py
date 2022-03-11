a476d472f1c44d9050adb099aef65b1626b0f320
jmohnn <jlmohn@ucdavis.edu>
2022-03-11 15:54:55 -0800

    updated feat007 inforec/vid

--- common/inforecordings/feat007_inforec.py
+++ common/inforecordings/feat007_inforec.py
@@ -8,13 +8,13 @@ experiments = []
 # Session parameters: sessionTime, behaviorFileSuffix, sessionType paradigmName.
 
 
-exp0 = celldatabase.Experiment(subject, '2022-03-10', brainArea='AC_right', probe = 'NPv1-8131', recordingTrack='anteromedial_DiI', info=['anteromedial_DiI', 'soundLeft']) #reference = external
+exp0 = celldatabase.Experiment(subject, '2022-03-10', brainArea='AC_right', probe = 'NPv1-8131', recordingTrack='anteromedial_DiI', info=['anteromedial_DiI', 'soundLeft']) #reference = tip
 experiments.append(exp0)
 # 10:53 in booth
 # 10:55 in brain
 # 11:00 reached max depth
 # 11:19 started recording
-# 1:25 done
+# 12:35 done
 
 exp0.add_site(2967)
 exp0.maxDepth = 2967
@@ -22,3 +22,18 @@ exp0.add_session('11-19-31','a','AM','am_tuning_curve')
 exp0.add_session('11-27-37','b','pureTones','am_tuning_curve')
 exp0.add_session('11-44-02','a','FTVOTBorders','2afc_speech')
 
+
+exp1 = celldatabase.Experiment(subject, '2022-03-11', brainArea='AC_right', probe = 'NPv1-8131', recordingTrack='middlemedialDiD', info=['middlemedial_DiD', 'soundLeft']) #reference = tip
+experiments.append(exp1)
+# 13:53 in booth
+# 14:05 lowered electrodes, couldn't penetrate brain. Retracted and cleaned craniotomy.
+# 14:20 lowered electrodes, in brain
+# 14:25 reached max depth
+# 14:45 started recording
+# 15:45 done
+
+exp1.add_site(2942)
+exp1.maxDepth = 2942
+exp1.add_session('14-47-12','a','AM','am_tuning_curve') 
+exp1.add_session('14-55-00','b','pureTones','am_tuning_curve') #accidentally hit the start button after I finished recording so there are ~2 extra trials in the behavior data at the end that weren't recorded.
+exp1.add_session('15-15-21','a','FTVOTBorders','2afc_speech')
--- common/infovideos/feat007_infovideos.py
+++ common/infovideos/feat007_infovideos.py
@@ -9,15 +9,31 @@ cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
 # 2022-03-10
 
 videos.add_session('2022-03-10', 'AM',
-                   'feat007_am_tuning_curve_20220308_01.mkv',
-                   'feat007_am_tuning_curve_20220308a.h5', cameraParams) 
+                   'feat007_am_tuning_curve_20220310_01.mkv',
+                   'feat007_am_tuning_curve_20220310a.h5', cameraParams) 
 
 
 videos.add_session('2022-03-10', 'pureTones',
-                   'feat007_am_tuning_curve_20220308_02.mkv',
-                   'feat007_am_tuning_curve_20220308b.h5', cameraParams) 
+                   'feat007_am_tuning_curve_20220310_02.mkv',
+                   'feat007_am_tuning_curve_20220310b.h5', cameraParams) 
 
 videos.add_session('2022-03-10', 'FTVOTBorders',
-                   'feat007_2afc_speech_20220308_01.mkv',
-                   'feat007_2afc_speech_20220308a.h5', cameraParams) 
+                   'feat007_2afc_speech_20220310_01.mkv',
+                   'feat007_2afc_speech_20220310a.h5', cameraParams) 
+
+# 2022-03-11
+
+videos.add_session('2022-03-11', 'AM',
+                   'feat007_am_tuning_curve_20220311_01.mkv',
+                   'feat007_am_tuning_curve_20220311a.h5', cameraParams) 
+
+
+videos.add_session('2022-03-11', 'pureTones',
+                   'feat007_am_tuning_curve_20220311_02.mkv',
+                   'feat007_am_tuning_curve_20220311b.h5', cameraParams) 
+
+videos.add_session('2022-03-11', 'FTVOTBorders',
+                   'feat007_2afc_speech_20220311_01.mkv',
+                   'feat007_2afc_speech_20220311a.h5', cameraParams) 
+
 
