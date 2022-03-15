fb095cf13c44c22cba06c7a460b8c4238e4e89dc
JaraLab User <jlmohn@ucdavis.edu>
2022-03-15 11:22:40 -0700

    pushed feat005 infohist file

--- common/infohistology/feat005_tracks.py
+++ common/infohistology/feat005_tracks.py
@@ -1,16 +1,19 @@
 subject = 'feat005'
 tracks = [
-    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C5',
-     'recordingTrack':'anterolateral_DiI', 'shank':0, 'atlasZ':218},
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C1_L',
+     'recordingTrack':'anterolateralDiI', 'shank':1, 'atlasZ':199},
 
-    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C3',
-     'recordingTrack':'anteromedial_DiD', 'shank':0, 'atlasZ':209},
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C1_L',
+     'recordingTrack':'anteromedialDiD', 'shank':1, 'atlasZ':199},
 
-    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C1',
-     'recordingTrack':'middlelateral_DiD', 'shank':0, 'atlasZ':203},
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B2_R',
+     'recordingTrack':'anterolateralDiI', 'shank':1, 'atlasZ':181},
 
-    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_B4',
-     'recordingTrack':'caudolateral_DiI', 'shank':0, 'atlasZ':193},
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B2_R',
+     'recordingTrack':'anterolateralDiD', 'shank':1, 'atlasZ':181},
 
-    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_B3',
-     'recordingTrack':'caudomedial_DiI', 'shank':0, 'atlasZ':187}]
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_A4_R',
+     'recordingTrack':'caudomedialDiI', 'shank':1, 'atlasZ':170},
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_A4_R',
+     'recordingTrack':'caudomedialDiD', 'shank':1, 'atlasZ':170}]
