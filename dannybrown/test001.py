8ad7394dfd9b7e1ee10cce4fbc88cbf5f69354ec
jarauser@jarastation4 <jarauser@jarastation4>
2022-03-18 15:50:44 -0700

    Added test001 file

---
+++ dannybrown/test001.py
@@ -0,0 +1,12 @@
+"""
+Test script to show Danny how to run things in IPython
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+random_array = np.random.rand(100,100)
+plt.imshow(random_array)
+plt.show()
+
+print('Test text')
