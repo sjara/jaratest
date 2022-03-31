7e087a91f2312dfb41c1cf81d313d8419f092749
Santiago Jaramillo <sjara@uoregon.edu>
2022-03-31 13:38:32 -0700

    Various changes to templates for code associated with papers.

--- common/templates/generate_template.py
+++ common/templates/generate_template.py
@@ -1,19 +1,25 @@
+"""
+DESCRIBE HERE WHAT THE SCRIPT GENERATES.
+"""
+
 import os
 import sys
 import numpy as np
-import pandas as pd
 from jaratoolbox import settings
-import figparams
+import studyparams
 
 FIGNAME = 'figure_name'
 figDataFile = 'file_containing_data_for_this_fig.npz'
-figDataDir = os.path.join(settings.FIGURES_DATA_PATH, figparams.STUDY_NAME, FIGNAME)
+figDataDir = os.path.join(settings.FIGURES_DATA_PATH, studyparams.STUDY_NAME, FIGNAME)
+
+# -- Optional --
 if not os.path.exists(figDataDir):
     os.mkdir(figDataDir)
     #print('Please create folder: {}'.format(figDataDir)); sys.exit()
+
 figDataFullPath = os.path.join(figDataDir,figDataFile)
 scriptFullPath = os.path.realpath(__file__)
 
 databaseName = 'THIS_STUDY_database.h5'
-databaseFullPath = os.path.join(settings.DATABASE_PATH, figparams.STUDY_NAME, databaseName)
+databaseFullPath = os.path.join(settings.DATABASE_PATH, studyparams.STUDY_NAME, databaseName)
 celldb = celldatabase.load_hdf(databaseFullPath)
