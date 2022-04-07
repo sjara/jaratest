d909aed9d7567a61517ddd717aa1a73dac78aee0
Manuel <manuelo@uoregon.edu>
2022-04-07 10:23:06 -0700

    infovideo file for pure011

--- common/infovideos/pure011_infovideos.py
+++ common/infovideos/pure011_infovideos.py
@@ -93,18 +93,40 @@ videos.add_session('2022-03-23', '2chords',
                    'pure011_20220323_2Sounds_169_2Sconfig13.mp4',
                    'pure011_detectiongonogo_20220323_2Sounds_169_2Sconfig13.h5',
                    cameraParams)
-
-videos.add_session('2022-03-23', '2chords',
-                   'pure011_20220323_2Sounds_170_2Sconfig13.mp4',
-                   'pure011_detectiongonogo_20220323_2Sounds_170_2Sconfig13.h5',
+                   
+videos.add_session('2022-03-25', '2chords',
+                   'pure011_20220325_2Sounds_170_2Sconfig13.mp4',
+                   'pure011_detectiongonogo_20220325_2Sounds_170_2Sconfig13.h5',
                    cameraParams)
 
+videos.add_session('2022-03-24', '2chords',
+                   'pure011_20220324_2Sounds_174_2Sconfig14.mp4',
+                   'pure011_detectiongonogo_20220324_2Sounds_174_2Sconfig14.h5',
+                   cameraParams)
 
+videos.add_session('2022-03-25', '2chords',
+                   'pure011_20220325_2Sounds_175_2Sconfig14.mp4',
+                   'pure011_detectiongonogo_20220325_2Sounds_175_2Sconfig14.h5',
+                   cameraParams)
 
+videos.add_session('2022-03-25', '2chords',
+                   'pure011_20220325_2Sounds_176_2Sconfig14.mp4',
+                   'pure011_detectiongonogo_20220325_2Sounds_176_2Sconfig14.h5',
+                   cameraParams)
 
+videos.add_session('2022-04-06', 'multipleFreqs',
+                   'pure011_20220406_mfq_189_mconfig1.mp4',
+                   'pure011_am_tuning_curve_20220406_mfq_189_mconfig1.h5',
+                   cameraParams)
 
+videos.add_session('2022-04-06', 'multipleFreqs',
+                   'pure011_20220406_mfq_190_mconfig1.mp4',
+                   'pure011_am_tuning_curve_20220406_mfq_190_mconfig1.h5',
+                   cameraParams)
 
-
-
+videos.add_session('2022-04-06', 'multipleFreqs',
+                   'pure011_20220406_mfq_191_mconfig1.mp4',
+                   'pure011_am_tuning_curve_20220406_mfq_191_mconfig1.h5',
+                   cameraParams)
 
 
