7d4f2a6ac8e1f629518583d6b14018347e51df7d
Max Horrocks <max.horrocks@uoregon.edu>
2023-01-30 14:47:02 -0800

    added cell report WIP for acid mice

--- maxh/acid001/acid001_cell_reports.py
+++ maxh/acid001/acid001_cell_reports.py
@@ -18,85 +18,148 @@ inforecFile = os.path.join(settings.INFOREC_PATH, f'{subject}_inforec.py')
 dbPath = os.path.join(settings.DATABASE_PATH ,f'celldb_{subject}.h5')
 
 celldb = celldatabase.generate_cell_database(inforecFile)
+figureCount = 1
+figureTotal = 0
+
+for indRow, dbRow in celldb.iterrows():
+    oneCell = ephyscore.Cell(dbRow)
+    if oneCell.get_session_inds('preLowFreq') != [] and oneCell.get_session_inds('highFreq') != []:
+        figureTotal += 1
 
 for indRow, dbRow in celldb.iterrows():
     plt.clf()
 
     oneCell = ephyscore.Cell(dbRow)
-    gsMain = gs.GridSpec(1, 3)
+    gsMain = gs.GridSpec(4, 3)
     gsMain.update(left=0.075, right=0.98, top=0.9, bottom=0.1, wspace=0.4, hspace=0.4) #Change spacing of things
-    plt.suptitle(oneCell, fontsize=16, fontweight='bold', y = 0.99)
+    
 
     # Plot Waveform
-    ax0 = plt.subplot(gsMain[0, 0])
+    ax0 = plt.subplot(gsMain[0])
     plt.plot(dbRow.spikeShape, linewidth = 3)
     plt.text(30, -0.1, f"bestChannel = {dbRow.bestChannel}")
 
-    # lowFreq = lowfrequency is the oddball
-    # Lock to event
-    if oneCell.get_session_inds('lowFreq') != []: #and oneCell.get_session_inds('lowFreq') != []:
-        ephysData, bdata = oneCell.load('lowFreq')
+    # lowFreq = the low frequency is the oddball
+    # Lock spikes during prelowFreq paradigm to events.
+    if oneCell.get_session_inds('preLowFreq') != []: #and oneCell.get_session_inds('lowFreq') != []:
+        ephysData, bdataLow = oneCell.load('preLowFreq')
         spikeTimesLow = ephysData['spikeTimes']
         eventOnsetTimesLow = ephysData['events']['stimOn']
         eventOnsetTimesLow = eventOnsetTimesLow[:-1]
         timeRange = [-0.3, 0.45]  # In seconds
+        (spikeTimesFromEventOnsetLowPre, trialIndexForEachSpikeLowPre, indexLimitsEachTrialLowPre) = spikesanalysis.eventlocked_spiketimes(spikeTimesLow, eventOnsetTimesLow, timeRange)
 
-        (spikeTimesFromEventOnsetLow, trialIndexForEachSpikeLow, indexLimitsEachTrialLow) = spikesanalysis.eventlocked_spiketimes(spikeTimesLow, eventOnsetTimesLow, timeRange)
+        # Lock spikes during prehighFreq paradigm to events.
+        ephysData, bdataHigh = oneCell.load('preHighFreq')
+        spikeTimesHigh = ephysData['spikeTimes']
+        eventOnsetTimesHigh = ephysData['events']['stimOn']
+        eventOnsetTimesHigh= eventOnsetTimesHigh[:-1]
+        timeRange = [-0.3, 0.45]  # In seconds
+        (spikeTimesFromEventOnsetHighPre, trialIndexForEachSpikeHighPre, indexLimitsEachTrialHighPre) = spikesanalysis.eventlocked_spiketimes(spikeTimesHigh, eventOnsetTimesHigh, timeRange)
+
+        # Sort by frequencies for LowFreqOddball
+        frequencies_each_trial_low = bdataLow['currentStartFreq']
+        array_of_frequencies_low = np.unique(frequencies_each_trial_low)
+        trialsEachCondLowPre = behavioranalysis.find_trials_each_type(frequencies_each_trial_low, array_of_frequencies_low)
+
+        #Sort by frequencies for HighFreqOddball
+        frequencies_each_trial_high= bdataHigh['currentStartFreq']
+        array_of_frequencies_high = np.unique(frequencies_each_trial_high)
+        trialsEachCondHighPre = behavioranalysis.find_trials_each_type(frequencies_each_trial_high, array_of_frequencies_high)
 
 
-        # raster Plot
-        frequencies_each_trial = bdata['currentStartFreq']
+        # Extract only high freqency column and reshape array for raster_plot function
+        trialsEachHighFreqLowPre = trialsEachCondLowPre[:,1].reshape(-1,1)
+        trialsEachHighFreqHighPre = trialsEachCondHighPre[:,1].reshape(-1,1)
 
-        oddballIndex = []
-        for i in range(0, len(frequencies_each_trial)): 
-            if frequencies_each_trial[i] == 8000 : 
-                oddballIndex.append(i)
+        # Extract only low freqency column and reshape array for raster_plot function
+        trialsEachLowFreqLowPre = trialsEachCondLowPre[:,0].reshape(-1,1) # when low is the oddball, get the low freqs
+        trialsEachLowFreqHighPre = trialsEachCondHighPre[:,0].reshape(-1,1)
 
-        array_of_frequencies = np.unique(frequencies_each_trial)
-        trialsEachCond = behavioranalysis.find_trials_each_type(frequencies_each_trial, array_of_frequencies)
+        # Get the index of trials before oddball for LowFreqOddball
+        trialIndexBeforeOddball = np.array(np.flatnonzero(trialsEachLowFreqLowPre[:,0])) -1
+        nTrials = len(frequencies_each_trial_low)
+        trialBeforeOddballPreLow = np.zeros(nTrials, dtype=bool)
+        trialBeforeOddballPreLow[trialIndexBeforeOddball]=True
+        trialBeforeOddballPreLow = trialBeforeOddballPreLow.reshape(-1,1) 
+        
+        # Get the index of trials before oddball for HighFreqOddball
+        trialIndexBeforeOddball = np.array(np.flatnonzero(trialsEachHighFreqHighPre[:,0])) -1
+        nTrials = len(frequencies_each_trial_high)
+        trialBeforeOddballPreHigh = np.zeros(nTrials, dtype=bool)
+        trialBeforeOddballPreHigh[trialIndexBeforeOddball]=True
+        trialBeforeOddballPreHigh = trialBeforeOddballPreHigh.reshape(-1,1) 
 
-        #sorted, sortedtrialsindex = spikesanalysis.sort_by_trial_type(trialIndexForEachSpikeLow, trialsEachCond)
 
+        # Raster Plot of high frequency when it is standard
+        # ax1 = plt.subplot(gsMain[1])
+        # fRaster = extraplots.raster_plot(spikeTimesFromEventOnsetLowPre, indexLimitsEachTrialLowPre, timeRange, trialsEachHighFreqLowPre)
+        # plt.xlabel('Time (s)')
+        # plt.ylabel('Trials')
+        # plt.title('13kHz (preLowFreq)')
 
-        #trialsEachHighFreq = trialsEachCond[:,1]
+        # Raster Plot of high frequency when it is oddball
+        # ax3 = plt.subplot(gsMain[2])
+        # fRaster = extraplots.raster_plot(spikeTimesFromEventOnsetHighPre, indexLimitsEachTrialHighPre, timeRange, trialsEachHighFreqHighPre)
+        # plt.xlabel('Time (s)')
+        # plt.ylabel('Trials')
+        # plt.title('13kHz (preHighFreq)')
+
+        # New Raster Plot
+        colorsEachCond = ['#39b5c4', '#f04158']
+
+        stfeo = np.concatenate((spikeTimesFromEventOnsetHighPre, spikeTimesFromEventOnsetLowPre))
+        ilet = np.hstack((indexLimitsEachTrialHighPre, indexLimitsEachTrialLowPre +len(spikeTimesFromEventOnsetHighPre)))
+
+        tec1 = np.vstack((trialBeforeOddballPreHigh, np.zeros(trialsEachHighFreqHighPre.shape)))
+        tec2 = np.vstack((np.zeros(trialBeforeOddballPreHigh.shape), trialsEachHighFreqHighPre))
+        tec = np.hstack((tec1, tec2))
+
+        highFreqLabels = ('Standard', "Oddball")
+        ax1 = plt.subplot(gsMain[1])
+        plt.xlabel('Time (s)')
+        plt.ylabel('Firing Rate')
+        plt.title('13kHz Tone (pre')
+        pRaster, hcond, zline = extraplots.raster_plot(stfeo, ilet, timeRange, tec, colorsEachCond, labels= highFreqLabels)
+        
+
+        
+        # PSTH of high frequency 
+        binWidth = 0.010
+        timeVec = np.arange(timeRange[0],timeRange[-1],binWidth)
+        smoothWinSizePsth = 2 
+        lwPsth = 2
+        downsampleFactorPsth = 1
+
+        ax2 = plt.subplot(gsMain[4])    
+        spikeCountMatLow = spikesanalysis.spiketimes_to_spikecounts(spikeTimesFromEventOnsetLowPre, indexLimitsEachTrialLowPre, timeVec)
+        spikeCountMatHigh = spikesanalysis.spiketimes_to_spikecounts(spikeTimesFromEventOnsetHighPre, indexLimitsEachTrialHighPre, timeVec)
+
+        colorsEachCond = ['#39b5c4', '#f04158'] #add to PSTH when multiple conditions present.
+        colorsEachCond2 = ['#f04158'] #add to PSTH when multiple conditions present.
+
+        extraplots.plot_psth(spikeCountMatLow/binWidth, smoothWinSizePsth, timeVec, trialBeforeOddballPreHigh, colorsEachCond, linestyle=None, linewidth=lwPsth, downsamplefactor=downsampleFactorPsth)
+        extraplots.plot_psth(spikeCountMatHigh/binWidth, smoothWinSizePsth, timeVec, trialsEachHighFreqHighPre, colorsEachCond2, linestyle=None, linewidth=lwPsth, downsamplefactor=downsampleFactorPsth)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Firing Rate')
+        plt.title('13kHz (lowFreq)')
+        plt.legend(("Standard Tone", "Oddball Tone"), loc='below')
+
+        # Title of image
+        plt.suptitle(f'{oneCell} {figureCount}/{figureTotal}', fontsize=16, fontweight='bold', y = 0.99)
+        figureCount+=1
 
        
-        fRaster = extraplots.raster_plot(spikeTimesFromEventOnsetLow, indexLimitsEachTrialLow, timeRange)
-
-
-        ax1 = plt.subplot(gsMain[0, 1])
-        #lowRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnsetLow, indexLimitsEachTrialLow,timeRange)
-        #plt.setp(fRaster, ms=2)
-        plt.xlabel('Time (s)')
-        plt.ylabel('Trials')
-        plt.title('13kHz (lowFreq)')
+        # Change shape of entire figure
+        plt.gcf().set_size_inches([12.8, 9.6])
         plt.show()
 
+        
+
+        # Save Figure
+        # figPath = os.path.join(settings.FIGURES_DATA_PATH, 'cell_reports', f'{subject}_{dbRow.date}_{dbRow.maxDepth}um_c{dbRow.cluster}_report.png')
+        # plt.savefig(figPath, format='png')
 
 
-        # binWidth = 0.010
-        # timeRange = [-0.3, 0.45]
-        # timeVec = np.arange(timeRange[0],timeRange[-1],binWidth)
-        # smoothWinSizePsth = 2 
-        # lwPsth = 2
-        # downsampleFactorPsth = 1
-        # ax2 = plt.subplot(gsMain[0,2 ])    
-        # spikeCountMat = spikesanalysis.spiketimes_to_spikecounts(spikeTimesFromEventOnsetLow, indexLimitsEachTrialLow, timeVec)
-        # frequenciesEachTrialStd = bdata['highFreq']
-        # numberOfTrialsStd = len(frequenciesEachTrialStd)
-        # arrayOfFrequenciesStd = np.unique(bdata['currentStartFreq'])
 
-        # trialsEachCondStd = behavioranalysis.find_trials_each_type(frequenciesEachTrialStd,
-        # arrayOfFrequenciesStd)
-
-        # HighFreqStdInStdPara = indexLimitsEachTrialLow[:,trialsEachCondStd[:,1]]
-        # spikeCountMatHighFreqStdInStdPara = spikesanalysis.spiketimes_to_spikecounts(spikeTimesFromEventOnsetLow,
-        #         HighFreqStdInStdPara,timeVec)
-
-
-        # #labelsForYaxis = ['%.0f' % f for f in arrayOfFrequenciesStd]
-
-
-        # highPSTH = extraplots.plot_psth(spikeCountMatHighFreqStdInStdPara/binWidth, smoothWinSizePsth, timeVec, trialsEachCond=[], linestyle=None, linewidth=lwPsth, downsamplefactor=downsampleFactorPsth)
-
-        plt.show()
+        plt.close()
