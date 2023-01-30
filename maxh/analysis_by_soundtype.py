37bed4c4836f4eb38da9e87638426a16091f83b6
Max Horrocks <max.horrocks@uoregon.edu>
2023-01-30 14:47:53 -0800

    updated maxh

--- maxh/analysis_by_soundtype.py
+++ maxh/analysis_by_soundtype.py
@@ -12,45 +12,51 @@ import matplotlib.pyplot as plt
 inforecFile = os.path.join(settings.INFOREC_PATH,'febe008_inforec.py')
 celldb = celldatabase.generate_cell_database(inforecFile)
 
-cellDict = {'subject' : 'febe008',
-            'date' : '2022-07-22',
-            'pdepth' : 2961,
-            'cluster' : 127,
-            'egroup' : 0
-            }
+#Find a specific cell with a cellDict
+
+# cellDict = {'subject' : 'febe008',
+#             'date' : '2022-07-22',
+#             'pdepth' : 2961,
+#             'cluster' : 127,
+#             'egroup' : 0
+#             }
 #cellInd = 0
 #dbRow = celldb.iloc[cellInd]
 
-cellInd, dbRow = celldatabase.find_cell(celldb, **cellDict)
+#cellInd, dbRow = celldatabase.find_cell(celldb, **cellDict)
+#oneCell = ephyscore.Cell(dbRow)
 
-oneCell = ephyscore.Cell(dbRow)
-ephysData, bdata = oneCell.load('pureTones')
+for indRow, dbRow in celldb.iterrows():
+    oneCell = ephyscore.Cell(dbRow)
+    plt.clf()
 
-
-spikeTimes = ephysData['spikeTimes']
-eventOnsetTimes = ephysData['events']['stimOn']
-timeRange = [-0.5, 1]  # In seconds
-
-(spikeTimesFromEventOnset,trialIndexForEachSpike,indexLimitsEachTrial) = spikesanalysis.eventlocked_spiketimes(spikeTimes, eventOnsetTimes, timeRange)
-
-# Raster Plot
-timeRange = [-0.3,0.5]
-pRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnset,indexLimitsEachTrial,timeRange)
-
-plt.show()
-
-sys.exit()
-
-#Differentiating cells by frequency.
-timeRange = [-0.3,0.5]
+    ephysData, bdata = oneCell.load('pureTones')
 
 
-frequencies_each_trial = bdata['currentFreq']
-array_of_frequencies = np.unique(bdata['currentFreq'])
-trialsEachCond = behavioranalysis.find_trials_each_type(frequencies_each_trial, array_of_frequencies)
-fRaster = extraplots.raster_plot(spikeTimesFromEventOnset, indexLimitsEachTrial, timeRange, trialsEachCond)
+    spikeTimes = ephysData['spikeTimes']
+    eventOnsetTimes = ephysData['events']['stimOn']
+    timeRange = [-0.5, 1]  # In seconds
 
-#plt.imshow(trialsEachCond,aspect='auto')
+    (spikeTimesFromEventOnset,trialIndexForEachSpike,indexLimitsEachTrial) = spikesanalysis.eventlocked_spiketimes(spikeTimes, eventOnsetTimes, timeRange)
+
+    # Raster Plot
+    # timeRange = [-0.3,0.5]
+    # pRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnset,indexLimitsEachTrial,timeRange)
+
+
+    #Differentiating cells by frequency.
+    timeRange = [-0.3,0.5]
+
+
+    frequencies_each_trial = bdata['currentFreq']
+    array_of_frequencies = np.unique(bdata['currentFreq'])
+    trialsEachCond = behavioranalysis.find_trials_each_type(frequencies_each_trial, array_of_frequencies)
+    fRaster = extraplots.raster_plot(spikeTimesFromEventOnset, indexLimitsEachTrial, timeRange, trialsEachCond)
+
+    plt.title(f'Cluster = {dbRow.cluster}, Depth = {dbRow.pdepth}, Date = {dbRow.date}')
+    plt.imshow(trialsEachCond,aspect='auto')
+    plt.show()
+    input("press enter for next cell")
 
 sys.exit()
 
