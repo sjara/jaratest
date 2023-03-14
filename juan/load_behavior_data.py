9e9a368163eecba869361955cb6805d392e2b18f
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-03-14 15:01:51 -0700

    with this code i am checking if mice have peak of interactions during the section time

--- juan/load_behavior_data.py
+++ juan/load_behavior_data.py
@@ -1,16 +1,47 @@
 from jaratoolbox import loadbehavior
 import matplotlib.pyplot as plt
+import pandas as pd
 
-subject = "coop010x011"  # The name of the mouse
+subject = "coop010x011"  
 paradigm = "coop4ports"  # The paradigm name is also part of the data file name
-session = "20230313a"  # The format is usually YYYYMMDD and a short suffix
+session = "20230313a" 
 behavFile = loadbehavior.path_to_behavior_data(subject, paradigm, session)
 bdata = loadbehavior.BehaviorData(behavFile)
 
-##  I WANT TO IDENTIFY IF IT EXISTS A POINT OF TIME WITH THE MAJOR ACTIVITY
-mask = bdata ['outcome'] == 1
-bdata['outcome'][mask]
-bdata['timeTrialStart'] [mask]
+def load_data (subject, session):
+    """_summary_
 
-plt.plot(bdata['timeTrialStart'] [mask], bdata['outcome'][mask], marker='o')
-plt.show()
+    Args:
+        subject (str): The name of the mouse
+        session (str): The format is usually YYYYMMDD and a short suffix
+
+    Returns:
+        dict{str:np.ndarray}: Dictionary containing results of subject and session given to the function. 
+    """
+
+    paradigm = 'coop4ports' # The paradigm name is also part of the data file name
+    behavFile = loadbehavior.path_to_behavior_data(subject,paradigm,session)
+    bdata = loadbehavior.BehaviorData(behavFile)
+    return bdata
+
+subject = 'coop012x013' 
+bdata_solid = load_data(subject, '20230314a') 
+bdata_perforated = load_data(subject, '20230313a')
+
+#print(bdata.keys())
+#print(bdata.__dict__.keys())
+
+# This is done to explore my hunch
+# I need to check using window times, if mice are being more active during certain time of the section
+df_solid = pd.DataFrame({'Outcome':bdata_solid['outcome'], 'Time': bdata_solid['timeTrialStart'], "Active side":bdata_solid['activeSide']})
+df_perforated = pd.DataFrame({'Outcome':bdata_perforated['outcome'], 'Time': bdata_perforated['timeTrialStart'], "Active side":bdata_perforated['activeSide']})
+print (df_solid.head())
+print ("-------")
+print (df_perforated.head())
+
+print ("------")
+print ("Hereunder what matters")
+# Here I am printing the results. The window time is defined by the "bins" in the pandas function pd.cut
+print(df_solid.groupby(pd.cut(df_solid['Time'], bins=3))['Outcome'].count())
+print("-----")
+print(df_perforated.groupby(pd.cut(df_perforated['Time'], bins=3))['Outcome'].count())
