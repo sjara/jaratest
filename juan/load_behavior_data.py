7d961391a0b9b83d3ce441106943fe20b81e82fd
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-03-16 10:08:50 -0700

    I am in step 3: creation of bar plots

--- juan/load_behavior_data.py
+++ juan/load_behavior_data.py
@@ -3,15 +3,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import date, timedelta
 
-# subject = "coop010x011"  
+# subject = "coop010x011"
 # paradigm = "coop4ports"  # The paradigm name is also part of the data file name
-# session = "20230313a" 
+# session = "20230313a"
 # behavFile = loadbehavior.path_to_behavior_data(subject, paradigm, session)
 # bdata = loadbehavior.BehaviorData(behavFile)
 
 # Objective: Make a group of graphs to show the accumulated rewards every 20 min for each pair of mice, date and barrier
 
-def load_data (subject, session):
+
+def load_data(subject, session):
     """_summary_
 
     Args:
@@ -19,56 +20,70 @@ def load_data (subject, session):
         session (str): The format is usually YYYYMMDD and a short suffix
 
     Returns:
-        dict{str:np.ndarray}: Dictionary containing results of subject and session given to the function. 
+        dict{str:np.ndarray}: Dictionary containing results of subject and session given to the function.
     """
 
-    paradigm = 'coop4ports' # The paradigm name is also part of the data file name
-    behavFile = loadbehavior.path_to_behavior_data(subject,paradigm,session)
+    paradigm = "coop4ports"  # The paradigm name is also part of the data file name
+    behavFile = loadbehavior.path_to_behavior_data(subject, paradigm, session)
     bdata = loadbehavior.BehaviorData(behavFile)
     return bdata
 
 
 # Paso 1: I need to accumulate all data taking into account that I started recording the time since 20230313a and
 # only the time of stage 1 can be estimated
-def merge_data (start_subject, number_of_mice, start_date, end_date):
+def merge_data(start_subject, number_of_mice, start_date, end_date):
     # Empty dataframe defining the fields will need
-    df_all_data = pd.DataFrame(columns=['Outcome', 'Time', "BarrierType", "Date", "MiceID"])
-    # Break down the tuple to handle each number separetaly 
-    mouse1,mouse2 = start_subject
+    df_all_data = pd.DataFrame(
+        columns=["Outcome", "Time", "BarrierType", "Date", "MiceID"]
+    )
+    # Break down the tuple to handle each number separetaly
+    mouse1, mouse2 = start_subject
     for _ in range(number_of_mice):
-        mice_id = f'coop0{mouse1}x0{mouse2}'
-        #print(mice_id)
-        for days in range(int((end_date - start_date).days)+1):
+        mice_id = f"coop0{mouse1}x0{mouse2}"
+        # print(mice_id)
+        for days in range(int((end_date - start_date).days) + 1):
             current_date = str(start_date + timedelta(days)).replace("-", "")
-            #print(current_date)
-            bdata = load_data(mice_id, f'{current_date}a')
-            df = pd.DataFrame({'Outcome':bdata['outcome'], 'Time': bdata['timeTrialStart'], "BarrierType":bdata['barrierType'] })
-            df['Date'] = current_date
-            df['MiceID'] = mice_id
-            df_all_data= pd.concat([df, df_all_data],ignore_index=True)
-        mouse1= mouse2 + 1
-        mouse2= mouse1 + 1
-    
-    df_all_data.replace({'BarrierType':bdata.labels['barrierType']}, inplace=True)
+            # print(current_date)
+            bdata = load_data(mice_id, f"{current_date}a")
+            df = pd.DataFrame(
+                {
+                    "Outcome": bdata["outcome"],
+                    "Time": bdata["timeTrialStart"],
+                    "BarrierType": bdata["barrierType"],
+                }
+            )
+            df["Date"] = current_date
+            df["MiceID"] = mice_id
+            df_all_data = pd.concat([df, df_all_data], ignore_index=True)
+        mouse1 = mouse2 + 1
+        mouse2 = mouse1 + 1
+
+    df_all_data.replace({"BarrierType": bdata.labels["barrierType"]}, inplace=True)
     return df_all_data
 
-data = merge_data(start_subject=(10,11), number_of_mice=3, start_date=date(2023, 3, 13), end_date=date(2023, 3, 14))
 
-# Paso 2: I need to group all data by barrier > mice id 
-data.set_index(keys=['BarrierType','MiceID'], inplace=True)
+data = merge_data(
+    start_subject=(10, 11),
+    number_of_mice=3,
+    start_date=date(2023, 3, 13),
+    end_date=date(2023, 3, 14),
+)
 
+# Paso 2: I need to group all data by barrier > mice id
+data.set_index(keys=["BarrierType", "MiceID"], inplace=True)
+data_1 = data[data["Outcome"] == 1]
+data_1 = data_1.groupby(
+    by=[
+        "BarrierType",
+        "MiceID",
+        pd.cut(data_1["Time"], bins=3, labels=["0-20", "20-40", "40-60"]),
+    ]
+)["Outcome"].count()
 
-def f ():
-    
-    # print ("------")
-    # print ("Hereunder what matters")
-    # # Here I am printing the results. The window time is defined by the "bins" in the pandas function pd.cut
-    # print(df_solid.groupby(pd.cut(df_solid['Time'], bins=3))['Outcome'].count())
-    print("-----")
-    print(df_perforated.groupby(pd.cut(df_perforated['Time'], bins=3))['Outcome'].count())
-    df_perforated=df_perforated.groupby(pd.cut(df_perforated['Time'], bins=3))['Outcome'].count()
+fig, ax = plt.subplots(layout="constrained")
+ax.bar(
+    data_1.loc['solid'].reset_index()['MiceID'].values,
+    data_1.loc['solid'].values,
+)
 
-    # Plotting histogram with seaborn
-    plt.bar(df_perforated.index.astype(str), df_perforated.values)
-
-    plt.show()
+plt.show()
