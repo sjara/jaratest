6481658407fd00061b3bccbd4cc0a21fba3f078f
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-03-22 16:15:11 -0700

    function to plot accumulated rewards fixed

--- juan/plot_accum_rewards.py
+++ juan/plot_accum_rewards.py
@@ -4,34 +4,37 @@ import pandas as pd
 from datetime import date
 from load_behavior_data import collect_data
 
-# Objective: Make a group of graphs to show the accumulated rewards every 20 min for each pair of mice, date and barrier
+''' 
+Objective: 
+    Make a group of graphs to show the accumulated rewards 
+    every n min for each pair of mice in order to assess
+    how mice are performing through time.
+'''
 
 # Paso 1: I need to accumulate all data taking into account that I started recording the time since 20230313a and
 # only the time of stage 1 can be estimated
-
 data = collect_data(
     start_subject=(10, 11),
     number_of_mice=3,
-    start_date=date(2023, 3, 13),
-    end_date=date(2023, 3, 16),
+    start_date=date(2023, 3, 19),
+    end_date=date(2023, 3, 20),
 )
-# data2 = collect_data(
-#     start_subject=(14, 15),
-#     number_of_mice=1,
-#     start_date=date(2023, 3, 5),
-#     end_date=date(2023, 3, 7),
-# )
-# data = pd.concat([data,data2])
-
-# Paso 2: I need to group all data by barrier > mice id
+# Paso 2: I need to filter and group all data by barrier > mice id
 data.set_index(keys=["BarrierType", "MiceID"], inplace=True)
 data_only_successful = data[data["Outcome"] == 1]
+bins = 4
 data_only_successful_grouped = data_only_successful.groupby(
     by=[
         "BarrierType",
         "MiceID",
-        pd.cut(data_only_successful["Time"], bins=3, #labels=["0-20","20-40","40-60"]
-            )
+        pd.cut(
+            data_only_successful["Time"],
+            bins=bins,
+            labels=[
+                f"{int((60/bins*i)-(60/bins))}-{int(60/bins*i)}"
+                for i in range(1, bins + 1)
+            ],
+        ),
     ]
 )["Outcome"].count()
 
@@ -48,16 +51,18 @@ def barplot_accu_rewards_time(data):
         data (pandas.dataframe): Dataframe containing minimun
     """
 
-    fig, ax = plt.subplots( nrows=2,figsize=(10, 5) )
-    fig.suptitle(f"Accumulated rewards on time per mice for stage 1")
+    fig, ax = plt.subplots(nrows=2, figsize=(10, 5))
+    fig.suptitle(f"Accumulated rewards on time per mice for stage 4")
     times = data.index.levels[2]
-    x_pos = np.arange(len(times))
     mice_ids = data.index.levels[1]
-    width = 0.25
-    multiplier = 0
+    x_pos = np.arange(len(mice_ids))
+    print(mice_ids)
+    width = 0.8/len(times)
+    offset = 0
+
     for index, barrier in enumerate(data.index.levels[0]):
         for time in times:
-            offset = width * multiplier
+            # print(data.loc[f"{barrier}", :, time].values)
             bars = ax[index].bar(
                 x_pos + offset,
                 data.loc[f"{barrier}", :, time].values,
@@ -65,16 +70,18 @@ def barplot_accu_rewards_time(data):
                 label=time,
             )
             ax[index].bar_label(bars, padding=3)
-            multiplier += 1
-        multiplier = 0
+            offset += width
+        offset = 0
 
         # Add some text for labels, title and custom x-axis tick labels, etc.
         ax[index].set_title(f"{barrier} barrier")
         ax[index].set_ylabel("Rewards count")
-        ax[index].set_xticks(x_pos + width, mice_ids)
-        ax[index].legend(loc="upper left", ncols=3)
+        ax[index].set_xticks(x_pos + (width*len(times)-width)/2, mice_ids)
+        ax[index].legend(loc="upper left", ncols=len(times)/2)
         ax[index].set_ylim(0, 350)
+
 
 barplot_accu_rewards_time(data_only_successful_grouped)
 plt.tight_layout()
-plt.savefig('rewards_on_time_stage4.jpg')
+plt.show()
+# plt.savefig('rewards_on_time_stage4.jpg')
