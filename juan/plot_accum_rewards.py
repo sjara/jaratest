9caa3c5bc24f4948871bf4ca1139747658c2b6f8
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-03-26 10:18:56 -0700

    small change to adjust the y-axis to the maximum value in the data

--- juan/plot_accum_rewards.py
+++ juan/plot_accum_rewards.py
@@ -6,16 +6,16 @@ from load_behavior_data import collect_data
 
 
 def filter_and_group(bins, data) -> pd.DataFrame:
-    
+
     """_summary_:
-    This function is used to filter the data by the outcome of a trial 
+    This function is used to filter the data by the outcome of a trial
     and to group the data by the number of segmentation in time desired (bins)
 
     Returns:
-        data_filtered_grouped: Dataframe with the data filtered and grouped into 
+        data_filtered_grouped: Dataframe with the data filtered and grouped into
         as many segments as chose by the user
     """
-    
+
     data.set_index(keys=["BarrierType", "MiceID"], inplace=True)
     data_filtered = data[data["Outcome"] == 1]
     data_filtered_grouped = data_filtered.groupby(
@@ -52,12 +52,12 @@ def barplot_accu_rewards_time(data):
     mice_ids = data.index.levels[1]
     x_pos = np.arange(len(mice_ids))
     print(mice_ids)
-    width = 0.8/len(times)
+    width = 0.8 / len(times)
     offset = 0
 
     for index, barrier in enumerate(data.index.levels[0]):
+        max_value = 0
         for time in times:
-            # print(data.loc[f"{barrier}", :, time].values)
             bars = ax[index].bar(
                 x_pos + offset,
                 data.loc[f"{barrier}", :, time].values,
@@ -66,22 +66,28 @@ def barplot_accu_rewards_time(data):
             )
             ax[index].bar_label(bars, padding=3)
             offset += width
+            max_value = (
+                max(data.loc[f"{barrier}", :, time].values)
+                if max(data.loc[f"{barrier}", :, time].values) > max_value
+                else max_value
+            )
+
         offset = 0
 
         # Add some text for labels, title and custom x-axis tick labels, etc.
         ax[index].set_title(f"{barrier} barrier")
         ax[index].set_ylabel("Rewards count")
-        ax[index].set_xticks(x_pos + (width*len(times)-width)/2, mice_ids)
-        ax[index].legend(loc="upper left", ncols=len(times)/2)
-        ax[index].set_ylim(0, 350)
+        ax[index].set_xticks(x_pos + (width * len(times) - width) / 2, mice_ids)
+        ax[index].legend(loc="upper left", ncols=len(times) / 2)
+        ax[index].set_ylim(0, max_value+100)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    '''
-    Objective: 
-    
-        Make a group of graphs to show the accumulated rewards 
+    """
+    Objective:
+
+        Make a group of graphs to show the accumulated rewards
         every n min for each pair of mice in order to assess
         how mice are performing through time.
 
@@ -91,15 +97,15 @@ if __name__ == '__main__':
     # Paso 2: I need to filter and group all data by barrier > mice id.
     # Paso 3: Creation of bar charts.
     # paso 4: Compile all in a function to plot both barriers.
-    '''
+    """
     ## DATA COLLECTION
     data = collect_data(
         start_subject=(10, 11),
         number_of_mice=3,
         start_date=date(2023, 3, 19),
-        end_date=date(2023, 3, 20),
+        end_date=date(2023, 3, 25),
     )
-    
+
     ## RUN
     data_filtered_grouped = filter_and_group(bins=4, data=data)
     barplot_accu_rewards_time(data_filtered_grouped)
