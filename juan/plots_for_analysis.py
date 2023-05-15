a461229b83ff7e039a162441121e2094fc0a4388
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-05-15 11:57:23 -0700

    pct_rewarder_trials is working. but I need to change the way it is plotting the barriers

--- juan/plots_for_analysis.py
+++ juan/plots_for_analysis.py
@@ -5,16 +5,40 @@ import numpy as np
 from datetime import date
 from load_behavior_data import collect_behavior_data
 
-data_behavior = collect_behavior_data(
+all_data = pd.DataFrame([])
+data_behavior_comp = collect_behavior_data(
     start_subject=(10, 11),
     number_of_mice=3,
     start_date=date(2023, 3, 21),
     end_date=date(2023, 3, 30),
 )
 
+data_behavior = collect_behavior_data(
+    start_subject=(12, 13),
+    number_of_mice=1,
+    start_date=date(2023, 5, 5),
+    end_date=date(2023, 5, 12),
+)
+data_behavior_2 = collect_behavior_data(
+    start_subject=(14, 15),
+    number_of_mice=1,
+    start_date=date(2023, 5, 11),
+    end_date=date(2023, 5, 14),
+)
+data_behavior_2.loc[data_behavior_2["Date"] == "20230512", "BarrierType"] = "solid"
+
+data_behavior_3 = collect_behavior_data(
+    start_subject=(18, 19),
+    number_of_mice=1,
+    start_date=date(2023, 5, 7),
+    end_date=date(2023, 5, 14),
+)
+all_data = pd.concat(
+    [data_behavior, data_behavior_2, data_behavior_3, all_data], ignore_index=True
+)
 
 
-def barplot_accu_rewards_time(data:pd.DataFrame):
+def barplot_accu_rewards_time(data: pd.DataFrame):
     """_summary_:
     This function will create axes for each barrier,
     each axes will show bars representing the accumulated rewards for each pair of mice on time.
@@ -22,8 +46,8 @@ def barplot_accu_rewards_time(data:pd.DataFrame):
     wich means that for a 60 minute assay, accumulated rewards will be calculated every 20 minutes.
 
     Args:
-        data (pandas.dataframe): Dataframe grouped by 3 columns: 
-            BarrierType, MiceID and ranges of numerical values (time). 
+        data (pandas.dataframe): Dataframe grouped by 3 columns:
+            BarrierType, MiceID and ranges of numerical values (time).
         This dataframe is returned by load_behavior_data.py.filter_and_group
     """
 
@@ -57,7 +81,7 @@ def barplot_accu_rewards_time(data:pd.DataFrame):
         ax[index].set_ylabel("Trials count")
         ax[index].set_xticks(x_pos + (width * len(times) - width) / 2, mice_ids)
         ax[index].legend(loc="upper left", ncols=len(times) / 2)
-        ax[index].set_ylim(0, max_value+1000)
+        ax[index].set_ylim(0, max_value + 1000)
 
 
 def pct_rewarded_trials(
@@ -66,23 +90,32 @@ def pct_rewarded_trials(
     """_summary_:
     This categorical scatter plot is for analyze the percentage of regarded trials against barriers
     So, for each pair of mice is going to plot a graph with the percentage of rewarded trials for each barrier.
-    
+
     Args:
         data_behavior (pd.DataFrame): Pandas dataframe with at least 3 columns: BarrierType, Percent rewarded and MiceID
         colors (list[str], optional): List of colors in string type to distinguish points from each barrier. Defaults to ["red", "cyan"].
         width_lines (float, optional): Float type to set the long of the line representing the mean of the points in each barrier. Defaults to 0.1.
     """
+
+    #FIXME: Right now the code plot organizing the dataframes by the barriertype name. This has to be corrected.
+    # It is easy to make wrong plots if the isolated and non-isolated barriers do not follow an alphabetical order where isolated are together and same for non-isolated.
+    
     width_lines = width_lines
-    number_of_mice = len(data_behavior["MiceID"].unique())
+    miceIds = data_behavior["MiceID"].unique()
+    number_of_mice = len(miceIds)
     fig, ax = plt.subplots(1, number_of_mice, sharey=True)
 
     # Depending on the number of mice we will get list of axes of just one ax
     if number_of_mice > 1:
         for i in range(number_of_mice):
+            data_one_pair_mice = data_behavior.loc[
+                (data_behavior["MiceID"] == (miceIds[i]))
+            ]
+            data_one_pair_mice.sort_values(["BarrierType"], inplace=True)
             ax[i].scatter(
-                x=data_behavior["BarrierType"],
-                y=data_behavior["Percent rewarded"],
-                c=(data_behavior["BarrierType"]).apply(
+                x=data_one_pair_mice["BarrierType"],
+                y=data_one_pair_mice["Percent rewarded"],
+                c=(data_one_pair_mice["BarrierType"]).apply(
                     lambda x: colors[0] if x == "solid" else colors[1]
                 ),
             )
@@ -93,26 +126,32 @@ def pct_rewarded_trials(
             ax[i].hlines(
                 y=[
                     int(
-                        data_behavior.loc[
-                            data_behavior["BarrierType"] == "solid", "Percent rewarded"
+                        data_one_pair_mice.loc[
+                            (
+                                data_one_pair_mice["BarrierType"].isin(
+                                    ["perforated", "no_barrier"]
+                                )
+                            ),
+                            "Percent rewarded",
                         ].mean()
                     ),
                     int(
-                        data_behavior.loc[
-                            data_behavior["BarrierType"] == "perforated",
+                        data_one_pair_mice.loc[
+                            (data_one_pair_mice["BarrierType"] == "solid"),
                             "Percent rewarded",
                         ].mean()
                     ),
                 ],
                 xmin=[locs[0][0] - width_lines, locs[0][-1] - width_lines],
                 xmax=[locs[0][0] + width_lines, locs[0][-1] + width_lines],
-                colors=colors,
+                colors=colors[::-1],
             )
 
             ax[i].set_xlabel(data_behavior["MiceID"].unique()[i])
             ax[i].set_ylabel("Percentage of rewarded trials")
 
     else:
+        data_behavior.sort_values(['BarrierType'], inplace=True)
         ax.scatter(
             x=data_behavior["BarrierType"],
             y=data_behavior["Percent rewarded"],
@@ -120,7 +159,7 @@ def pct_rewarded_trials(
                 lambda x: colors[0] if x == "solid" else colors[1]
             ),
         )
-        # This is for getting the positions of the x sticks in order to determine the limites of the lines to plot the mean
+        # This is for getting the positions of the x sticks in order to determine the limits of the lines to plot the mean
         locs = plt.xticks()
 
         # Horizontal line to represent mean of points of both barriers
@@ -133,13 +172,14 @@ def pct_rewarded_trials(
                 ),
                 int(
                     data_behavior.loc[
-                        data_behavior["BarrierType"] == "perforated", "Percent rewarded"
+                        data_behavior["BarrierType"].isin(["perforated", "no_barrier"]),
+                        "Percent rewarded",
                     ].mean()
                 ),
             ],
             xmin=[locs[0][0] - width_lines, locs[0][-1] - width_lines],
             xmax=[locs[0][0] + width_lines, locs[0][-1] + width_lines],
-            colors=colors,
+            colors=colors[::-1],
         )
 
         ax.set_xlabel(data_behavior["MiceID"].unique()[0])
@@ -151,7 +191,9 @@ def pct_rewarded_trials(
     plt.show()
 
 
-def violin_plot_waitTime(data_behavior:pd.DataFrame, outcome:list[int] = [1], figsize:tuple[int]=(15, 5)):
+def violin_plot_waitTime(
+    data_behavior: pd.DataFrame, outcome: list[int] = [1], figsize: tuple[int] = (15, 5)
+):
     """_summary_:
     This violin plot is for analyze the waitTime
     It helps to see the distributions of how long does it take the second mouse to poke after the first mouse poked.
@@ -160,17 +202,17 @@ def violin_plot_waitTime(data_behavior:pd.DataFrame, outcome:list[int] = [1], fi
 
     Args:
         data_behavior (pd.DataFrame): Pandas dataframe with at least 4 columns: BarrierType, TimePoke1, TimePoke2, ActiveSide, Outcome and MiceID
-        outcome (list[int], optional): List of integers to define what outcomes user want to plot, 
-                    since the meaning of successful trial will change with the stage of training, for example, for stage 4 successfull trial 
+        outcome (list[int], optional): List of integers to define what outcomes user want to plot,
+                    since the meaning of successful trial will change with the stage of training, for example, for stage 4 successfull trial
                     is only outcome = [1], but for stage 1 successful trial is outcome = [1,2,3]. Defaults to [1].
         figsize (tuple[int], optional): Tuple with two integer to define the size of the figure containing the plots.
     """
 
-    # Filter only successfull outcomes 
-    success = data_behavior[data_behavior["Outcome"].isin(outcome) ].copy()
+    # Filter only successfull outcomes
+    success = data_behavior[data_behavior["Outcome"].isin(outcome)].copy()
     success.reset_index(inplace=True, drop=True)
-    
-    # Compute the difference in time between first and second poke 
+
+    # Compute the difference in time between first and second poke
     success["timeBetweenPokes"] = abs(success["TimePoke1"] - success["TimePoke2"])
 
     # Get only those trails where the last active side was in the opposite side
@@ -179,25 +221,37 @@ def violin_plot_waitTime(data_behavior:pd.DataFrame, outcome:list[int] = [1], fi
         if success.loc[i, "ActiveSide"] != success.loc[i - 1, "ActiveSide"]:
             indexes.append(i)
     alternate = success.loc[indexes]
-    
+
     # PLOTS
     fig, axes = plt.subplots(ncols=2, figsize=(15, 5))
     # All data
     sns.violinplot(
-        data=success, x="MiceID", y="timeBetweenPokes", hue="BarrierType", split=True, ax=axes[0], cut=0
+        data=success,
+        x="MiceID",
+        y="timeBetweenPokes",
+        hue="BarrierType",
+        split=True,
+        ax=axes[0],
+        cut=0,
     )
     axes[0].set_xlim(-1, 3.5)
     axes[0].set_title("All successful trials")
 
     # Only alternate sides
     sns.violinplot(
-        data=alternate, x="MiceID", y="timeBetweenPokes", hue="BarrierType", split=True, ax=axes[1], cut=0
+        data=alternate,
+        x="MiceID",
+        y="timeBetweenPokes",
+        hue="BarrierType",
+        split=True,
+        ax=axes[1],
+        cut=0,
     )
     axes[1].set_xlim(-1, 3.5)
     axes[1].set_title("Only trials with the previous trial been in the opposite side")
-    
+
     plt.tight_layout()
     plt.show()
 
 
-pct_rewarded_trials(data_behavior)
+pct_rewarded_trials(all_data)
