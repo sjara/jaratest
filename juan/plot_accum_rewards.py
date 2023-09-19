0aaa4b33092424d955940620f12ec6ccb914d4af
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-09-19 10:16:05 -0700

    added to gitignore the vscode and python caches

--- juan/plot_accum_rewards.py
+++ juan/plot_accum_rewards.py
@@ -5,8 +5,7 @@ from datetime import date
 from load_behavior_data import collect_behavior_data
 
 
-def filter_and_group(bins:int, data:pd.DataFrame, sessionLen:int) -> pd.DataFrame:
-
+def filter_and_group(bins: int, data: pd.DataFrame, sessionLen: int) -> pd.DataFrame:
     """_summary_:
     This function is used to filter the data by the outcome of a trial
     and to group the data by the number of segmentation in time desired (bins)
@@ -17,13 +16,13 @@ def filter_and_group(bins:int, data:pd.DataFrame, sessionLen:int) -> pd.DataFram
     """
 
     data.set_index(keys=["Date", "MiceID"], inplace=True)
-    data_filtered = data[data["Outcome"] == 1]
+    data_filtered = data#[data["Outcome"] == 1]
     data_filtered_grouped = data_filtered.groupby(
         by=[
             "Date",
             "MiceID",
             pd.cut(
-                data_filtered[["TimePoke2", 'TimePoke1']].fillna(0).apply(max, axis=1),
+                data_filtered[["TimePoke2", "TimePoke1"]].fillna(0).apply(max, axis=1),
                 bins=bins,
                 labels=[
                     f"{int((sessionLen/bins*i)-(sessionLen/bins))}-{int(sessionLen/bins*i)}"
@@ -35,7 +34,7 @@ def filter_and_group(bins:int, data:pd.DataFrame, sessionLen:int) -> pd.DataFram
     return data_filtered_grouped
 
 
-def barplot_accu_rewards_time(data:pd.DataFrame):
+def barplot_accu_rewards_time(data: pd.DataFrame):
     """_summary_:
     This function is used to create axes for each barrier,
     each axes will show bars representing the accumulated rewards for each pair of mice on time.
@@ -76,14 +75,15 @@ def barplot_accu_rewards_time(data:pd.DataFrame):
         ax[index].set_ylabel("Trials count")
         ax[index].set_xticks(x_pos + (width * len(times) - width) / 2, mice_ids)
         ax[index].legend(loc="upper left", ncols=len(times) / 2)
-        ax[index].set_ylim(0, max_value+100)
-        ax[index].set_yticks(np.arange(0, max_value+1, max_value))
+        ax[index].set_ylim(0, max_value + 100)
+        ax[index].set_yticks(np.arange(0, max_value + 1, max_value))
 
 
 if __name__ == "__main__":
-
     ## DATA COLLECTION
-    data = collect_behavior_data(mice_data={'coop022x023':[('2023-09-04','2023-09-04')] })
+    data = collect_behavior_data(
+        mice_data={"coop026x027": [("2023-09-17", "2023-09-17")]}
+    )
     ## RUN
     data_filtered_grouped = filter_and_group(bins=1, data=data, sessionLen=60)
     barplot_accu_rewards_time(data_filtered_grouped)
@@ -91,4 +91,4 @@ if __name__ == "__main__":
     ## SHOW PLOT
     plt.tight_layout()
     plt.show()
-    #plt.savefig('trials_on_time_stage4.jpg')
+    # plt.savefig('trials_on_time_stage4.jpg')
