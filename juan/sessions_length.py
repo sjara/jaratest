1b99581e5538c5f27d41e1e18087a29055bf3f91
juanjo255 <piconcossio@gmail.com>
2023-10-20 10:27:41 -0700

    adding script for the length of the session in coop project

--- juan/sessions_length.py
+++ juan/sessions_length.py
@@ -9,15 +9,29 @@ This code is to reproduce the same graphs presented in the question
 """
 
 
-## DATA COLLECTION
+## DATA COLLECTION FIRST SPAN DATES
 data = lbd.collect_behavior_data(
-    mice_data={"coop026x027": [("2023-08-15", "2023-08-18")]}
-)
+        mice_data={"coop026x027": [("2023-08-15", "2023-08-18")]}
+    )
 ## RUN
 data_filtered_grouped = lbd.filter_and_group(bins=3, data=data, sessionLen=60)
-print(data)
-pfa.barplot_accu_rewards_time(data_filtered_grouped, len(data['Date'].unique().tolist()))
+    
+pfa.barplot_accu_rewards_time(data_filtered_grouped, len(data.index.get_level_values(0).unique().tolist()))
+
+# space between plots
+plt.tight_layout()
+
+## DATA COLLECTION SECOND SPAN DATES
+data_2 = lbd.collect_behavior_data(
+        mice_data={"coop026x027": [("2023-08-11", "2023-08-14")]}
+    )
+## RUN
+data_filtered_grouped_2 = lbd.filter_and_group(bins=3, data=data_2, sessionLen=60)
+    
+pfa.barplot_accu_rewards_time(data_filtered_grouped, len(data_2.index.get_level_values(0).unique().tolist()))
+
+# space between plots
+plt.tight_layout()
 
 ## SHOW PLOT
-# plt.tight_layout()
-# plt.show()
+plt.show()
