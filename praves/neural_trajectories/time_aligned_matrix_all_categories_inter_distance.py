e495ada5bfb4ac3805e8a8bcd7174e0e501b62f3
“Praves-Lamichhane” <praveslamichhane@gmail.com>
2024-05-07 09:22:43 -0400

    added legends to PCA plots

--- praves/neural_trajectories/time_aligned_matrix_all_categories_inter_distance.py
+++ praves/neural_trajectories/time_aligned_matrix_all_categories_inter_distance.py
@@ -50,24 +50,6 @@ eventOnsetTimes = ephysData["events"]["stimOn"][:nTrials]
 timeRange = [-1.5, 5.5]
 bin_size = 0.5
 
-## get the stimulus start time for the time range and bin size
-def get_stim_start_time(time_range=[-1.5, 5.5], bin_size=0.5, stim_start=0):
-    num_sec_each_bin = (time_range[1] - time_range[0]) / bin_size
-    start_time = time_range[0]
-    end_time = start_time + num_sec_each_bin
-    curr_bin = 0
-    while True:
-        if stim_start <= time_range[0] and stim_start >= time_range[1]:
-           raise ValueError("Stimulus start time is outside the time range")  
-        else:
-            start_time = time_range[0]
-            end_time = start_time + num_sec_each_bin
-            if start_time <= stim_start <= end_time:
-                return curr_bin
-            start_time = end_time
-            curr_bin += 1 
-
-
 ## get spiketimes, trial index for each spike and index limits for each trial for each cell in the ensemble
 ## NOTE: eventlocked_spiketimes method returns a list of arrays for each cell in the ensemble
 spikeTimesFromEventOnsetAll, trialIndexForEachSpikeAll, indexLimitsEachTrialAll = ensemble.eventlocked_spiketimes(eventOnsetTimes, timeRange)
@@ -135,37 +117,25 @@ for uniq_stim, instance_id in enumerate(possibleStims):
 time_aligned_matrix_all_instances = np.array(time_aligned_matrix_all_instances)
 print("Shape of time aligned matrix: ", time_aligned_matrix_all_instances.shape)
 
-## use PCA to reduce dimensionality of the time_aligned_matrix_all_instances
-DIMS = 2
-num_instances_each_category = len(possibleStims) // TOTAL_CATEGORIES
+## calculate the distance between the time_aligned_matrix for each instance
+## the distance is calculated using the euclidean distance between the time_aligned_matrix for each instance as a function of time
+## distance (matrix A, matrix B, time) = |vector A - vector B| for each time point
 
-## reshape the time_aligned_matrix_all_instances to 2D array so that rows = cells and columns = bins from all instances stacked from 0 to 19
-time_aligned_matrix_all_instances_transposed = time_aligned_matrix_all_instances.transpose(1, 0, 2)
-final_shape = (time_aligned_matrix_all_instances_transposed.shape[0], -1)
-time_aligned_matrix_all_instances_reshaped = time_aligned_matrix_all_instances_transposed.reshape(final_shape)
+## create a dictionary to store the distance between the time_aligned_matrix for each instance
+distance_all_instances = {} ## key: instance_id_1, instance_id_2, value: [distance array for each time point for two instances]
+for instance_id, instance_value in enumerate(time_aligned_matrix_all_instances):
+    for other_instance_id, other_instance_value in enumerate(time_aligned_matrix_all_instances):
+        if instance_id != other_instance_id:
+            time_array = np.arange(0, instance_value.shape[1], 1)
+            distance_all_time_points = []
+            for time_point in time_array:
+                distance_vector = np.linalg.norm(instance_value[:, time_point] - other_instance_value[:, time_point])
+                distance_all_time_points.append(distance_vector)
+            distance_all_instances[(instance_id, other_instance_id)] = distance_all_time_points
 
-# ## test whether the reshaping is correct - we expect the first 13 columns to be the same as the first instance
-# print(time_aligned_matrix_all_instances[0] == time_aligned_matrix_all_instances_reshaped[:, 0:13])
-# print(time_aligned_matrix_all_instances[1] == time_aligned_matrix_all_instances_reshaped[:, 13:26])
-
-## perform PCA on the reshaped 2D array 
-pca = PCA(n_components=DIMS)
-pca_matrix_all_instances = pca.fit_transform(time_aligned_matrix_all_instances_reshaped.T)
-
-pca_matrix_all_instances = pca_matrix_all_instances.T
-
-## now reshape the pca_matrix_all_instances to 3D array reflecting the original matrix 
-num_trials = possibleStims.shape[0]
-num_cells_pca = DIMS 
-num_bins_per_trial = time_aligned_matrix_all_instances.shape[2] 
-
-pca_matrix_all_instances_reshaped_original = pca_matrix_all_instances.reshape(num_cells_pca, num_trials, num_bins_per_trial)
-
-## transpose the matrix so that the dimension information is preserved
-pca_matrix_all_instances_original = pca_matrix_all_instances_reshaped_original.transpose(1, 0, 2)
-
-# ## check if the reshaping is correct
-# print(pca_matrix_all_instances_original[0] == pca_matrix_all_instances[:, 0:13])
+## print the distance all instances dictionary
+# for key, value in distance_all_instances.items():
+#     print(key, len(value))
 
 ## create a category instance dictionary to store instances for each category
 category_instance_dict = {} ## key: category_id, value: [instance_ids]
@@ -182,47 +152,32 @@ for instance_id, instance_value in enumerate(time_aligned_matrix_all_instances):
 print(category_instance_dict)
 
 
-## PCA plots
-## plot the PCA transformed matrix
 
-fig, axs = plt.subplots(2, 3, figsize=(15, 10))
-axs = axs.flatten()
+# ## plot the distance between the time_aligned_matrix for each instance
+# fig, axs = plt.subplots(2, 3)
+# axs = axs.flatten()
+# for category_id, instance_ids in category_instance_dict.items():
+#     for key, value in distance_all_instances.items():
+#         if key[0] in instance_ids and key[1] in instance_ids:
+#             if key[0] < key[1]:
+#                 # print(key)
+#                 # ax = axs[category_id]
+#                 axs.plot(value, label = key, color = colors_categories[category_id])
+#                 ax.legend()
+#     plt.title(f"Distance as a function of time for Category {category_id}")
+#     plt.show()
 
-## plot the PCA transformed matrix all instances
-for instance_id, instance_values in enumerate(pca_matrix_all_instances_original):
-    curr_color = colors_categories[instance_id // num_instances_each_category]
-    # print(instance_values.shape)
-    ax = axs[0]
-    plot_pca = ax.plot(instance_values[0, :], instance_values[1, :], '.-', color=curr_color)
-    ## plot only the first point as a circle
-    ax.plot(instance_values[0, 0], instance_values[1, 0], 'ko', markersize=10, label='start')
-    ax.plot(instance_values[0, -1], instance_values[1, -1], 'yo', markersize=10, label='end')
-    ax.plot(instance_values[0, get_stim_start_time()], instance_values[1, get_stim_start_time()], 'rx', markersize=10, label='Stimulus ON')
+fig, axs = plt.subplots(2, 3)
+axs = axs.flatten()  # Flatten the array of axes for easier indexing
 
-ax.set_xlabel('Principal Component 1')
-ax.set_ylabel('Principal Component 2')
-ax.title.set_text('2D PCA of Spike Data all instances')
+for category_id, instance_ids in category_instance_dict.items():
+    ax = axs[category_id]  # Get the specific axis for this category
+    for key, value in distance_all_instances.items():
+        if key[0] in instance_ids and key[1] in instance_ids:
+            if key[0] < key[1]:
+                ax.plot(value, label=str(key), color=colors_categories[category_id])  # Plot on the specific subplot
+    ax.set_title(f"Distance as a function of time for Category {category_id}")
+    
+    ax.legend()  # Show legend on the specific subplot
 
-
-
-## plot the PCA for each category separately
-for key, values in category_instance_dict.items():
-    plt.title('2D PCA of Spike Data per category')
-    for instance_id in values:
-        instance_values = pca_matrix_all_instances_original[instance_id]
-        print(instance_values.shape)
-        ax = axs[(instance_id // num_instances_each_category) + 1]
-        ax.plot(instance_values[0, :], instance_values[1, :], '.-')
-        # plot only the first point as a circle
-        ax.plot(instance_values[0, 0], instance_values[1, 0], 'ko', markersize=10, label='start')
-        ax.plot(instance_values[0, -1], instance_values[1, -1], 'yo', markersize=10, label='end')
-        ax.plot(instance_values[0, get_stim_start_time()], instance_values[1, get_stim_start_time()], 'rx', markersize=10, label='Stimulus ON')
-    ax.set_xlim(-70, 200)
-    ax.set_ylim(-100, 200)
-    ax.set_xlabel('Principal Component 1')
-    ax.set_ylabel('Principal Component 2')
-    ax.title.set_text('Category: {}'.format(key))
-
-fig.legend(loc='upper right')
-plt.tight_layout()
-plt.show()
+plt.show()  # Display all plots
