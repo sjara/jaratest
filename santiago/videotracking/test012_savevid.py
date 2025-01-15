9399f965005f93b57900a10a262f92c7a277f43b
sjara <sjara@uoregon.edu>
2025-01-15 09:15:02 -0800

    Changed parameters for saving video.

--- santiago/videotracking/test012_savevid.py
+++ santiago/videotracking/test012_savevid.py
@@ -6,7 +6,8 @@ img3 = cv2.imread('1.jpg')
 
 height , width , layers =  img1.shape
 
-video = cv2.VideoWriter('video.avi',-1,1,(width,height))
+#video = cv2.VideoWriter('video.avi',-1,1,(width,height))
+video = cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc('M','J','P','G'),1,(width,height))
 
 video.write(img1)
 video.write(img2)
