a096bc7253e475ab01a6da51e17e2cd2e7e09f73
jguenzama <jguenzama@gmail.com>
2025-01-29 20:12:54 -0800

    Fully updated the CSD creation script with figures included.

--- jeremy/jeremy_csd.py
+++ jeremy/jeremy_csd.py
@@ -3,6 +3,10 @@
 This is where the aggregated CSD creation .py script will go.
 '''
 
+'''
+Load in neurpixels LFP data.
+'''
+
 import os
 import json
 import numpy as np
@@ -29,3 +33,77 @@ bitVolts = contData.bitVolts
 plt.figure(figsize=(15,20))
 plt.imshow(contData.data[:1000,:].T)
 plt.title("Heatmap of the LFP data across all acquisition channels from n=0 to n=1000 | yellow = high, purple = low")
+plt.show()
+
+nSamplesToProcess = 1 * sampleRate  # 4 sec
+traceToProcess = rawdata[:nSamplesToProcess, :20] * bitVolts  # In uV 
+tvec = np.arange(nSamplesToProcess) / sampleRate
+
+events = loadneuropix.RawEvents(rawDataPath, dataStream)
+eventOnsetTimes = events.get_onset_times() # Stimulus onset times ... 331 in total
+
+'''
+Load in behavior data corresponding to the LFP data.
+'''
+
+from jaratoolbox import loadbehavior, behavioranalysis
+
+subject = 'feat018'
+paradigm = 'am_tuning_curve'
+session = '20240614a'
+behavFile = loadbehavior.path_to_behavior_data(subject, paradigm, session)
+bdata = loadbehavior.BehaviorData(behavFile)
+
+'''
+Create uncollapsed PSTH given 
+'''
+
+import jeremy_psth as jpsth
+import jeremy_utils as jutils
+reload(jpsth)
+reload(jutils)
+
+eventOnsetTimes = jpsth.fix_stimuli_number_mismatch(bdata, 'currentFreq', eventOnsetTimes)
+
+frequencies_each_trial = bdata['currentFreq']
+array_of_frequencies = np.unique(bdata['currentFreq'])
+trialsEachCond = behavioranalysis.find_trials_each_type(frequencies_each_trial, array_of_frequencies)
+
+psth_upper = 1000 # Ex: Collect 100 datapoints BEYOND stimulus onset
+psth_lower = 100 # Ex: Collect 100 datapoints BEFORE stimulus onset
+
+## Choose via binary operation whichever trials are of interest.
+trials = trialsEachCond[:,6] + trialsEachCond[:,7] + trialsEachCond[:,8]
+
+psth_uncollapsed, collection_outcome_list = jpsth.get_psth_uncollapsed(contData.data, contData.timestamps, eventOnsetTimes, trials, psth_upper, psth_lower)
+
+plt.figure(figsize=(15,20))
+plt.imshow(psth_uncollapsed.mean(axis=0).T)
+plt.axvline(x=psth_lower, c='r')
+plt.title("Heatmap of average LFP data for a given stimuli. | yellow = high, purple = low, red line = stimulus onset.")
+plt.xlabel("Time evolution (left to right)")
+plt.ylabel("Unorded channels")
+plt.show()
+
+'''
+Estimate CSD
+'''
+
+from elephant.current_source_density import estimate_csd
+from neo import AnalogSignal
+import quantities as pq
+
+coords = [(elem,) for elem in range(psth_uncollapsed[:,:,:].shape[-1])]
+neo_lfp_personal = AnalogSignal(psth_uncollapsed[:,:,:].mean(axis=0)*bitVolts, units="uV", sampling_rate = sampleRate*pq.Hz)
+neo_lfp_personal.annotate(coordinates = coords * pq.mm)
+
+csd_not_np = estimate_csd(neo_lfp_personal, method="KCSD1D")
+csd = np.array(csd_not_np)
+
+plt.figure(figsize=(15,20))
+plt.imshow(csd.T)
+plt.axvline(x=psth_lower, c='r')
+plt.title("Heatmap of estimated CSD for a given stimuli. | yellow = high, purple = low, red line = stimulus onset.")
+plt.xlabel("Time evolution.")
+plt.ylabel("Unorded channels.")
+plt.show()
