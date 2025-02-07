39859ef0dcae1d81a50020317da5c3d2919142ca
jguenzama <jguenzama@gmail.com>
2025-02-07 15:28:22 -0800

    Fixed the plotting script to display one frequency with appropriate labeling and formatting.

--- jeremy/jeremy_preprocess_for_CSD.py
+++ jeremy/jeremy_preprocess_for_CSD.py
@@ -10,7 +10,6 @@ from jaratoolbox import loadbehavior, behavioranalysis
 from jaratoolbox import settings
 import matplotlib.pyplot as plt
 import jeremy_psth as jpsth
-import jeremy_utils as jutils
 import numpy as np
 import os
 
@@ -25,6 +24,8 @@ eventOnsetTimes = events.get_onset_times() # Stimulus onset times ... 331 in tot
 
 contData = loadneuropix.Continuous(rawDataPath, dataStream)
 sampleRate = contData.sampleRate
+nChannels = contData.nChannels
+bitVolts = contData.bitVolts
 
 ## Load in behavior data corresponding to the LFP data.
 subject = 'feat018'
@@ -39,8 +40,10 @@ frequencies_each_trial = bdata['currentFreq']
 array_of_frequencies = np.unique(bdata['currentFreq'])
 trialsEachCond = behavioranalysis.find_trials_each_type(frequencies_each_trial, array_of_frequencies)
 
-psth_upper = jpsth.time_to_indices(3, sampleRate, 's') # Ex: Collect 3 seconds of datapoints BEYOND stimulus onset
-psth_lower = jpsth.time_to_indices(3, sampleRate, 's') # Ex: Collect 3 seconds of datapoints BEFORE stimulus onset
+time_upper = 0.2
+time_lower = 0.2
+psth_upper = jpsth.time_to_indices(time_upper, sampleRate, 's') # Ex: Collect 3 seconds of datapoints BEYOND stimulus onset
+psth_lower = jpsth.time_to_indices(time_lower, sampleRate, 's') # Ex: Collect 3 seconds of datapoints BEFORE stimulus onset
 print(f"Indices below: {psth_lower}\nIndices above: {psth_upper}")
 
 # Grab your trials of interest
@@ -49,10 +52,13 @@ trials = trialsEachCond[:,6]
 psth_uncollapsed, collection_outcome_list = jpsth.get_psth_uncollapsed(contData.data, contData.timestamps, eventOnsetTimes, trials, psth_upper, psth_lower)
 
 print(f"Uncollapsed psth shape: {psth_uncollapsed.shape}")
-plt.figure(figsize=(15,20))
-plt.imshow(psth_uncollapsed.mean(axis=0).T)
-plt.axvline(x=psth_lower, c='r')
-plt.title("Heatmap of average LFP data for a given stimuli. | yellow = high, purple = low, red line = stimulus onset.")
-plt.xlabel("Time evolution (left to right)")
-plt.ylabel("Unorded channels")
+plt.figure(figsize=(10,5))
+plt.imshow(psth_uncollapsed.mean(axis=0).T * bitVolts, aspect='auto',
+           extent=[-time_lower, time_upper, 0, nChannels])
+plt.colorbar(label='Amplitude (uV)')
+plt.axvline(x=1/sampleRate, c='r', label='Stimulus onset')
+plt.legend(loc='upper left')
+plt.title("Heatmap of average LFP data for a given stimuli.")
+plt.xlabel("Time evolution (s)")
+plt.ylabel("Channel index")
 plt.show()
