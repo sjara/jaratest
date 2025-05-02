eb429b6f548e295672b7d26cc153578319fe8de4
ramzymulla <rza@uoregon.edu>
2025-05-02 13:07:11 -0700

    20250502

--- ramzy/minidisplayer.py
+++ ramzy/minidisplayer.py
@@ -2,26 +2,33 @@ import pygame as pg
 import multiprocessing
 from screeninfo import get_monitors
 
+MINIDISPLAY_DIMENSIONS = (640,480)
 
 # Initialize Pygame
 pg.init()
 clock = pg.time.Clock()
 
-# Get HDMI (second) monitor resolution
+# Get monitor resolution
 monitors = get_monitors()
-if len(monitors) < 2:
+
+# find and use minidisplay
+for i in range(len(monitors)):
+    disp = monitors[i]  
+    width, height = disp.width, disp.height
+    if (width,height) == MINIDISPLAY_DIMENSIONS:
+        x, y = disp.x, disp.y
+        window = pg.display.set_mode((width, height), pg.FULLSCREEN, display = 1)
+        pg.display.set_caption('minidisplay')
+        break
+
+# if minidisplay not present, show 640x480p rectangle on primary monitor
+if (width,height) != MINIDISPLAY_DIMENSIONS:
+    print("minidisplay not detected!")
     disp = monitors[0]
-    width,height = 680,480
+    width,height = MINIDISPLAY_DIMENSIONS
     x,y = disp.x,disp.y
     window = pg.display.set_mode((width,height),pg.NOFRAME,display = 0)
     pg.display.set_caption('main')
-
-else:
-    disp = monitors[1]  # Assuming HDMI is second
-    width, height = disp.width, disp.height
-    x, y = disp.x, disp.y
-    window = pg.display.set_mode((width, height), pg.FULLSCREEN, display = 1)
-    pg.display.set_caption('minidisplay')
 
 screen = pg.display.get_surface()
 
@@ -42,7 +49,8 @@ for i in range(1000):
     if i%2:
         screen.fill((0,0,0))
     else:
-        screen.fill((255,255,255))
+        # screen.fill((255,255,255))
+        pg.draw.rect(screen, (255,255,255), (width//2 - 50, height//2 - 50, 100,100))
 
     pg.display.flip()
     clock.tick(pg.display.get_current_refresh_rate())
