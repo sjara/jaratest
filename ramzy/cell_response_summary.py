821b9734f3b34de603c2e13774321ab5a2f6586c
ramzymulla <rza@uoregon.edu>
2025-06-12 09:20:16 -0700

    handling for optoTuning and soundLocalization

--- ramzy/cell_response_summary.py
+++ ramzy/cell_response_summary.py
@@ -43,13 +43,19 @@ def get_args():
 stim_types = {
     "AM" : "currentFreq",
     "Freq" : "currentFreq",
-    "naturalSound" : "soundID"
+    "optoFreq" : "currentFreq",
+    "optoAM" : "currentFreq",
+    "naturalSound" : "soundID",
+    "L2R3_L2R1_L1R2_L3R2" : "soundLocation"
 }
 
 ylabs = {
     "AM" : "AM rate (Hz)",
     "Freq" : "Tone Frequency (Hz)",
-    "naturalSound" : "Sound ID"
+    "optoAM" : "AM rate (Hz)",
+    "optoFreq" : "Tone Frequency (Hz)",
+    "naturalSound" : "Sound ID",
+    "L2R3_L2R1_L1R2_L3R2" : "Sound Location"
 }
 
 # get args
@@ -65,16 +71,14 @@ if cellsToPlot != '':
 
 
 
-# get data
+### Load cell database, or create/save new database if needed
 dbPath = os.path.join(settings.DATABASE_PATH, studyName, f'celldb_{subject}.h5')
 
-if os.path.exists(dbPath):
+if os.path.exists(dbPath): 
     celldb = celldatabase.load_hdf(dbPath)
-
-    if not celldb['date'].isin([sessionDate]).any():
+    if not celldb['date'].isin([sessionDate]).any() :
         celldb = celldatabase.generate_cell_database(os.path.join(settings.INFOREC_PATH,
                                                               f"{subject}_inforec.py"))
-        
         celldatabase.save_hdf(celldb, dbPath)
         
 else:
@@ -83,10 +87,19 @@ else:
     celldatabase.save_hdf(celldb, dbPath)
 
 
-# subset data
+celldb = celldb.rename(columns={'info' : 'extra'})
+celldb['soundLoc'] = [i[1] for i in celldb.extra]
+
+### Subset data
+
 celldbSubset = celldb[(celldb.date==sessionDate) \
-                      & (celldb.pdepth==probeDepth) \
+                    & (celldb.pdepth==probeDepth) \
                         & (celldb.cluster_label=='good')]
+
+
+if len(celldbSubset) == 0:
+    raise Exception(f"error, no cells matching {sessionDate}, {probeDepth}um")
+
 firstCell = celldbSubset.iloc[0].name
 
 ### for doing cellsToPlot as cluster #s instead of good cell #s ---v
@@ -108,6 +121,7 @@ if not cellsToPlot:
 
 ### ---^
 
+### load ensemble
 ensemble = ephyscore.CellEnsemble(celldbSubset)
 
 sessionTimes = {}
@@ -138,8 +152,15 @@ if not os.path.exists(sessionFolder):
 if not os.path.exists(siteFolder):
     os.mkdir(siteFolder)                    # folder for this site
 
+### Determine figure/page parameters
 if cellsToPlot:
-    cellsPerPage = 4
+    cellsPerPage = NROW*NCOL//len(paradigms)
+    numcells = len(cellInds)
+    numpages = int(np.ceil(numcells/cellsPerPage))
+    numdigits = len(str(numpages))
+
+elif "optoFreq" in paradigms or "optoAM" in paradigms:
+    cellsPerPage = NROW*NCOL
     numcells = len(cellInds)
     numpages = int(np.ceil(numcells/cellsPerPage))
     numdigits = len(str(numpages))
@@ -150,9 +171,13 @@ else:
     numpages = int(np.ceil(numcells/cellsPerPage))
     numdigits = len(str(numpages))
     
-spikeTimesFromEventOnsetAll, trialIndexForEachSpikeAll, indexLimitsEachTrialAll = {},{},{}
-possibleStim,trialsEachCond,rasterTimeRange,stimDur = {},{},{},{}
 
+### Allocate data dicts
+spikeTimesFromEventOnsetAll, trialIndexForEachSpikeAll, indexLimitsEachTrialAll = {},{},{}
+possibleStim,trialsEachCond,possibleLaser,trialsEachComb,rasterTimeRange,stimDur = {},{},{},{},{},{}
+possibleYticks = {}
+
+### Extract data
 for paradigm,stim in zip(paradigms,stims):
     # load in the data
     ephysData,bdata = ensemble.load(paradigm)
@@ -173,10 +198,30 @@ for paradigm,stim in zip(paradigms,stims):
     spikeTimesFromEventOnsetAll[paradigm], trialIndexForEachSpikeAll[paradigm], indexLimitsEachTrialAll[paradigm] = \
         ensemble.eventlocked_spiketimes(eventOnsetTimes, rasterTimeRange[paradigm])
     
-    # get trials
     currentStim = bdata[stim]
+    currentLaser = bdata['laserTrial']
+        
     possibleStim[paradigm] = np.unique(currentStim)
-    trialsEachCond[paradigm] = behavioranalysis.find_trials_each_type(currentStim, possibleStim[paradigm])
+    possibleLaser = np.unique(currentLaser)
+
+    trialsEachCond[paradigm] = \
+        behavioranalysis.find_trials_each_type(currentStim, possibleStim[paradigm])
+    
+    trialsEachComb[paradigm] = \
+        behavioranalysis.find_trials_each_combination(currentStim,possibleStim[paradigm],
+                                                      currentLaser,possibleLaser)
+    
+
+    if 'Freq' in paradigm:
+        possibleYticks[paradigm] = np.round(possibleStim[paradigm]/1000,1)
+        ylabs[paradigm] = ylabs[paradigm].replace('Hz','kHz')
+
+    else:
+        possibleYticks[paradigm] = possibleStim[paradigm]
+
+    if 'opto' in paradigm:
+        possibleYticks[paradigm] = list(possibleStim[paradigm])+list(possibleStim[paradigm])
+    
 
 # for plotting a subset of cells
 if cellsToPlot: 
@@ -186,7 +231,7 @@ if cellsToPlot:
 
     # draw raster plots
     for page in range(numpages):
-        fig = plt.figure(1,figsize=(len(paradigms)*8, 12))
+        fig = plt.figure(1,figsize=(6*NCOL, 6*NROW))
         pageCells = cellsToPlot.split(',')[page*cellsPerPage:(page+1)*cellsPerPage]
         cellString = pageCells[0]
         for cell in pageCells[1:]:
@@ -199,20 +244,73 @@ if cellsToPlot:
             figname = f"{subject} {sessionDate} {probeDepth}"
 
         # plot cells for current page
-        for row,paradigm in enumerate(paradigms):
-            for count, indcell in enumerate(cellInds[page*cellsPerPage:(page+1)*cellsPerPage]):
-                plt.subplot(len(paradigms), NCOL, count+row*4+1)
-                pRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnsetAll[paradigm][indcell], indexLimitsEachTrialAll[paradigm][indcell],
-                                                        rasterTimeRange[paradigm], trialsEachCond[paradigm], labels=possibleStim[paradigm])
-                plt.setp(pRaster, ms=2)
-                plt.axvline(stimDur[paradigm], color='0.75', zorder=-10)
-                plt.xlabel('Time (s)')
-                plt.ylabel(ylabs[paradigm])
-                curr_loc = celldbSubset.iloc[indcell]
-                curr_shank = pmap.channelShank[curr_loc.bestChannel]+1
-                curr_depth = curr_loc.maxDepth - pmap.ypos[curr_loc.bestChannel]
-                if row==0:
-                    plt.title(f"Good Cell #{curr_loc.name - firstCell}\n (KS Unit #{curr_loc.cluster}, best channel: {curr_loc.bestChannel} (Shank #{curr_shank}, {curr_depth} {r'$\mu$'}m))")
+
+        for count, indcell in enumerate(cellInds[page*cellsPerPage:(page+1)*cellsPerPage]):
+            if len(paradigms) ==2:
+                for row,paradigm in enumerate(paradigms):
+                    nTrials = trialsEachCond[paradigm].shape[0]
+                    plt.subplot(NROW, NCOL, count*2+row+1)
+                    pRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnsetAll[paradigm][indcell], 
+                                                                   indexLimitsEachTrialAll[paradigm][indcell],
+                                                                   rasterTimeRange[paradigm], trialsEachComb[paradigm], 
+                                                                   labels=possibleYticks[paradigm])
+                    plt.setp(pRaster, ms=2)
+                    plt.axvline(stimDur[paradigm], color='0.75', zorder=-10)
+                    plt.xlabel('Time (s)')
+                    plt.ylabel(ylabs[paradigm])
+                    if 'opto' in paradigm:
+                        midline = sum(1-np.nonzero(trialsEachComb[paradigm])[2])
+                        plt.axhline(midline, color='r', zorder=-10)
+                        plt.yticks([0.5*midline,1.5*midline],['laser OFF', 'laser ON'],minor=True)
+                        ax = plt.gca()
+                        ax.tick_params(axis='y',which='minor',left=False, right=True, labelleft=False, labelright=True)
+                    curr_loc = celldbSubset.iloc[indcell]
+                    curr_shank = pmap.channelShank[curr_loc.bestChannel]+1
+                    curr_depth = curr_loc.maxDepth - pmap.ypos[curr_loc.bestChannel]
+                    plt.title(f"Good Cell #{curr_loc.name - firstCell} {paradigm} \n (KS Unit #{curr_loc.cluster}, best channel: {curr_loc.bestChannel} (Shank #{curr_shank}, {curr_depth} {r'$\mu$'}m))")
+
+            elif len(paradigms)==3:
+                for row,paradigm in enumerate(paradigms):
+
+                    plt.subplot(NROW, NCOL, count+row*4+1)
+                    pRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnsetAll[paradigm][indcell], indexLimitsEachTrialAll[paradigm][indcell],
+                                                            rasterTimeRange[paradigm], trialsEachCond[paradigm], labels=possibleYticks[paradigm])
+                    plt.setp(pRaster, ms=2)
+                    plt.axvline(stimDur[paradigm], color='0.75', zorder=-10)
+                    plt.xlabel('Time (s)')
+                    plt.ylabel(ylabs[paradigm])
+                    if 'opto' in paradigm:
+                        midline = sum(1-np.nonzero(trialsEachComb[paradigm])[2])
+                        plt.axhline(midline, color='r', zorder=-10)
+                        plt.yticks([0.5*midline,1.5*midline],['laser OFF', 'laser ON'],minor=True)
+                        ax = plt.gca()
+                        ax.tick_params(axis='y',which='minor',left=False, right=True, labelleft=False, labelright=True)
+                    curr_loc = celldbSubset.iloc[indcell]
+                    curr_shank = pmap.channelShank[curr_loc.bestChannel]+1
+                    curr_depth = curr_loc.maxDepth - pmap.ypos[curr_loc.bestChannel]
+                    if row==0:
+                        plt.title(f"Good Cell #{curr_loc.name - firstCell}\n (KS Unit #{curr_loc.cluster}, best channel: {curr_loc.bestChannel} (Shank #{curr_shank}, {curr_depth} {r'$\mu$'}m))")
+
+            elif len(paradigms)==1:
+                for row,paradigm in enumerate(paradigms):
+                    plt.subplot(NROW, NCOL, count+row+1)
+                    pRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnsetAll[paradigm][indcell], indexLimitsEachTrialAll[paradigm][indcell],
+                                                            rasterTimeRange[paradigm], trialsEachCond[paradigm], labels=possibleYticks[paradigm])
+                    plt.setp(pRaster, ms=2)
+                    plt.axvline(stimDur[paradigm], color='0.75', zorder=-10)
+                    plt.xlabel('Time (s)')
+                    plt.ylabel(ylabs[paradigm])
+                    if 'opto' in paradigm:
+                        midline = sum(1-np.nonzero(trialsEachComb[paradigm])[2])
+                        plt.axhline(midline, color='r', zorder=-10)
+                        plt.yticks([0.5*midline,1.5*midline],['laser OFF', 'laser ON'],minor=True)
+                        ax = plt.gca()
+                        ax.tick_params(axis='y',which='minor',left=False, right=True, labelleft=False, labelright=True)
+                    curr_loc = celldbSubset.iloc[indcell]
+                    curr_shank = pmap.channelShank[curr_loc.bestChannel]+1
+                    curr_depth = curr_loc.maxDepth - pmap.ypos[curr_loc.bestChannel]
+                    if row==0:
+                        plt.title(f"Good Cell #{curr_loc.name - firstCell}\n (KS Unit #{curr_loc.cluster}, best channel: {curr_loc.bestChannel} (Shank #{curr_shank}, {curr_depth} {r'$\mu$'}m))")
         
         plt.suptitle(figname)
         plt.tight_layout()
@@ -221,16 +319,17 @@ if cellsToPlot:
         plt.savefig(os.path.join(outFolder,filename))
         plt.close(1)
 
-# for plotting all cells
-else:
+elif "optoFreq" in paradigms or "optoAM" in paradigms:
     for paradigm in paradigms:
         outFolder = os.path.join(siteFolder,paradigm)
         if not os.path.exists(outFolder):
             os.mkdir(outFolder)                # output folder
 
+        midline = sum(1-np.nonzero(trialsEachComb[paradigm])[2])
+
         # draw raster plots
         for page in range(numpages):
-            fig = plt.figure(1,figsize=(24, 12))
+            fig = plt.figure(1,figsize=(6*NCOL, 6*NROW))
             filename = f"{subject}_{sessionDate}_{probeDepth}_{paradigm}_{(page+1):02d}.png"
             
             if numpages > 1:
@@ -242,7 +341,51 @@ else:
             for count, indcell in enumerate(cellInds[page*cellsPerPage:(page+1)*cellsPerPage]):
                 plt.subplot(NROW, NCOL, count+1)
                 pRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnsetAll[paradigm][indcell], indexLimitsEachTrialAll[paradigm][indcell],
-                                                            rasterTimeRange[paradigm], trialsEachCond[paradigm], labels=possibleStim[paradigm])
+                                                            rasterTimeRange[paradigm], trialsEachComb[paradigm],labels=possibleYticks[paradigm])
+                plt.setp(pRaster, ms=2)
+                plt.axvline(stimDur[paradigm], color='0.75', zorder=-10)
+                plt.axhline(midline, color='r', zorder=-10)
+                plt.xlabel('Time (s)')
+                plt.ylabel(ylabs[paradigm])
+                plt.axhline(midline, color='r', zorder=-10)
+                plt.yticks([0.5*midline,1.5*midline],['laser OFF', 'laser ON'],minor=True)
+                ax = plt.gca()
+                ax.tick_params(axis='y',which='minor',left=False, right=True, labelleft=False, labelright=True)
+                curr_loc = celldbSubset.iloc[indcell]
+                curr_shank = pmap.channelShank[curr_loc.bestChannel]+1
+                curr_depth = curr_loc.maxDepth - pmap.ypos[curr_loc.bestChannel]
+                plt.title(f"Good Cell #{curr_loc.name - firstCell}\n (KS Unit #{curr_loc.cluster}, best channel: {curr_loc.bestChannel} (Shank #{curr_shank}, {curr_depth} {r'$\mu$'}m))")
+
+                
+            plt.suptitle(figname)
+            plt.tight_layout()
+
+            # save and close current page/fig
+            plt.savefig(os.path.join(outFolder,filename))
+            plt.close(1)
+
+# for plotting all cells
+else:
+    for paradigm in paradigms:
+        outFolder = os.path.join(siteFolder,paradigm)
+        if not os.path.exists(outFolder):
+            os.mkdir(outFolder)                # output folder
+
+        # draw raster plots
+        for page in range(numpages):
+            fig = plt.figure(1,figsize=(6*NCOL, 4*NROW))
+            filename = f"{subject}_{sessionDate}_{probeDepth}_{paradigm}_{(page+1):02d}.png"
+            
+            if numpages > 1:
+                figname = f"{subject} {sessionDate} {probeDepth} {paradigm} {page+1}/{numpages}"
+            else:
+                figname = f"{subject} {sessionDate} {probeDepth} {paradigm}"
+
+            # plot cells for current page
+            for count, indcell in enumerate(cellInds[page*cellsPerPage:(page+1)*cellsPerPage]):
+                plt.subplot(NROW, NCOL, count+1)
+                pRaster, hcond, zline = extraplots.raster_plot(spikeTimesFromEventOnsetAll[paradigm][indcell], indexLimitsEachTrialAll[paradigm][indcell],
+                                                            rasterTimeRange[paradigm], trialsEachCond[paradigm], labels=possibleYticks[paradigm])
                 plt.setp(pRaster, ms=2)
                 plt.axvline(stimDur[paradigm], color='0.75', zorder=-10)
                 plt.xlabel('Time (s)')
@@ -251,7 +394,8 @@ else:
                 curr_shank = pmap.channelShank[curr_loc.bestChannel]+1
                 curr_depth = curr_loc.maxDepth - pmap.ypos[curr_loc.bestChannel]
                 plt.title(f"Good Cell #{curr_loc.name - firstCell}\n (KS Unit #{curr_loc.cluster}, best channel: {curr_loc.bestChannel} (Shank #{curr_shank}, {curr_depth} {r'$\mu$'}m))")
-            
+
+                
             plt.suptitle(figname)
             plt.tight_layout()
 
